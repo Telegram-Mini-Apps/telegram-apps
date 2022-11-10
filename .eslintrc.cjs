20c34ba677e1b8197036e1db5da04bc2ee07f613
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-11-10 17:45:54 +0500

    chore(eslint): add __tests__ to ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  ignorePatterns: ['src/**/*.test.ts'],
+  ignorePatterns: ['**/*.test.ts', '**/__tests__/**'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
   },
