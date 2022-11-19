46aa4be9145f165c8467ba45f4f8eb6cc45e325d
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-11-19 19:44:59 +0500

    chore(tests): move tests to other folder

--- packages/init-data/src/__tests__/parsing.test.ts
+++ packages/init-data/src/__tests__/parsing.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it} from '@jest/globals';
-import {extractInitDataFromSearchParams} from './parsing';
+import {extractInitDataFromSearchParams} from '../parsing';
 
 describe('parsing', () => {
   describe('extractInitDataFromSearchParams', () => {
