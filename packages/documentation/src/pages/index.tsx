0e22cf98c6721e13112ca78c13fd9c3c2c550267
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-11-25 18:13:49 +0300

    fix(pages): index page now redirects to Docs

--- packages/documentation/src/pages/index.tsx
+++ packages/documentation/src/pages/index.tsx
@@ -1,34 +1,16 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import {Redirect} from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
+      noFooter={true}
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <Redirect to={'docs/from-author'}/>
     </Layout>
   );
 }
