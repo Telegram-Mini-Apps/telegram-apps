813e1921ad1fd882e03937c42ab33ba3c1287012
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-11-30 19:52:15 +0500

    ci(docs deployment): specify separate branches for docs

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'telegram-web-apps', // Usually your GitHub org/user name.
   projectName: 'twa', // Usually your repo name.
   trailingSlash: false,
-  deploymentBranch: 'gh-docs',
+  deploymentBranch: 'gh-pages-docs',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
