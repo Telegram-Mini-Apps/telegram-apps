9bef9d84824f1e2d273c3f999e762c71a22ae73a
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-12-03 20:05:02 +0500

    fix(build): fix render of index page

--- packages/documentation/src/pages/index.tsx
+++ packages/documentation/src/pages/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {useIsWebAppEnv} from '@site/src/hooks';
 import {DefaultLayout, WebAppLayout} from '@site/src/components/home';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import {
+  isBrowserEnv,
+  isDesktopOrMobileEnv,
+  isWindowsPhoneEnv,
+} from 'twa-bridge';
 
 /**
  * Index entry for whole documentation.
  * @constructor
  */
 export default function Home(): JSX.Element {
-  return useIsWebAppEnv() ? <WebAppLayout/> : <DefaultLayout/>;
+  return ExecutionEnvironment.canUseDOM && (
+    (isBrowserEnv() && document.referrer.startsWith('https://web.telegram.org/'))
+    || isWindowsPhoneEnv(window)
+    || isDesktopOrMobileEnv(window)
+  )
+    // For Web Apps, we render separate layout.
+    ? <WebAppLayout/>
+    : <DefaultLayout/>;
 }
