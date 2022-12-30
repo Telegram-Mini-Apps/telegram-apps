ba1dd135f6aea315ba7cc3c5c3ab5708786fc2d9
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-12-31 00:11:44 +0500

    feat(events support): add support check for v6.4

--- packages/bridge/src/support.ts
+++ packages/bridge/src/support.ts
@@ -17,6 +17,10 @@ export function supports(method: PostEventName, inVersion: Version): boolean {
       return compareVersions('6.1', inVersion) <= 0;
     case 'web_app_open_popup':
       return compareVersions('6.2', inVersion) <= 0;
+    case 'web_app_close_scan_qr_popup':
+    case 'web_app_open_scan_qr_popup':
+    case 'web_app_read_text_from_clipboard':
+      return compareVersions('6.4', inVersion) <= 0;
     default:
       return true;
   }
