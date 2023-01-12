9b5da86663071433190cc4ba5a35c41db8586413
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-12 06:00:37 +0500

    chore(playground): add playground

--- packages/sdk-react/playground/rollup.config.js
+++ packages/sdk-react/playground/rollup.config.js
@@ -1,22 +1,28 @@
 import typescript from '@rollup/plugin-typescript';
 import serve from 'rollup-plugin-serve';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
 import {defineConfig} from 'rollup';
+import {fileURLToPath, URL} from 'node:url';
 
 export default defineConfig({
-  input: 'playground/src/index.tsx',
-  output: {file: 'playground/bundle.js', format: 'umd'},
+  input: fileURLToPath(new URL('index.tsx', import.meta.url)),
+  output: {
+    file: fileURLToPath(new URL('bundle.js', import.meta.url)),
+    format: 'iife'
+  },
   plugins: [
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
-    typescript({tsconfig: './tsconfig.dev.json'}),
+    typescript({
+      tsconfig: fileURLToPath(new URL('tsconfig.json', import.meta.url))
+    }),
     nodeResolve(),
     commonjs(),
-    serve({contentBase: 'playground', port: '3000'}),
+    serve({contentBase: 'playground'}),
   ],
   watch: {clearScreen: true},
 });
