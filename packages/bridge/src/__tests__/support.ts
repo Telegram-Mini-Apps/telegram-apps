2d605320e699cb1f8f87415f6efdeaaf31fdaa24
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-12 17:30:58 +0500

    test(6.4): add tests for 6.4 methods

--- packages/bridge/src/__tests__/support.ts
+++ packages/bridge/src/__tests__/support.ts
@@ -10,6 +10,10 @@ describe('support', () => {
       'web_app_trigger_haptic_feedback',
     ];
     const methods62: PostEventName[] = ['web_app_open_popup'];
+    const methods64: PostEventName[] = [
+      'web_app_read_text_from_clipboard', 'web_app_close_scan_qr_popup',
+      'web_app_close_scan_qr_popup',
+    ];
     const methodsAny: PostEventName[] = [
       'iframe_ready', 'web_app_close', 'web_app_data_send', 'web_app_expand',
       'web_app_open_link', 'web_app_ready', 'web_app_request_theme',
@@ -36,6 +40,17 @@ describe('support', () => {
 
       it(`should return false in case, passed method is "${method}" and version is lower than 6.2`, () => {
         expect(supports(method, '6.0')).toBe(false);
+      });
+    });
+
+    methods64.forEach(method => {
+      it(`should return true in case, passed method is "${method}" and version is 6.4 or more`, () => {
+        expect(supports(method, '6.4')).toBe(true);
+        expect(supports(method, '6.5')).toBe(true);
+      });
+
+      it(`should return false in case, passed method is "${method}" and version is lower than 6.4`, () => {
+        expect(supports(method, '6.3')).toBe(false);
       });
     });
 
