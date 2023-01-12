e00046263bacdb28dde2d68855aec2dc51f63b02
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-12 19:21:14 +0500

    test(test): fix error case test

--- packages/theme-params/src/__tests__/utils.test.ts
+++ packages/theme-params/src/__tests__/utils.test.ts
@@ -8,7 +8,7 @@ describe('utils', () => {
       'contain tgWebAppThemeParams parameter', () => {
         window.location.hash = '#tgWebAppData=query_id%3DAAHdF6IQAAAAAN0XohChJGOt%26user%3D%257B%2522id%2522%253A279058397%252C%2522first_name%2522%253A%2522Vladislav%2522%252C%2522last_name%2522%253A%2522Kibenko%2522%252C%2522username%2522%253A%2522vdkfrost%2522%252C%2522language_code%2522%253A%2522ru%2522%252C%2522is_premium%2522%253Atrue%252C%2522photo_url%2522%253A%2522https%253A%255C%252F%255C%252Ft.me%255C%252Fi%255C%252Fuserpic%255C%252F320%255C%252F4FPEE4tmP3ATHa57u6MqTDih13LTOiMoKoLDRG4PnSA.svg%2522%257D%26receiver%3D%257B%2522id%2522%253A1985737506%252C%2522is_bot%2522%253Atrue%252C%2522first_name%2522%253A%2522Wallet%2522%252C%2522last_name%2522%253A%2522%2522%252C%2522username%2522%253A%2522wallet%2522%252C%2522photo_url%2522%253A%2522https%253A%255C%252F%255C%252Ft.me%255C%252Fi%255C%252Fuserpic%255C%252F320%255C%252FfySBGKdwVHbIbghGmjAUcMU5KdJhE4ub_Hr51uOE5pE.svg%2522%257D%26auth_date%3D1667535190%26hash%3D6e9439dc0270cdf9a7f1d716b5980d0cd9a618afe20340b0eee2d7bb3063a35b&tgWebAppVersion=6.2&tgWebAppPlatform=web';
 
-        expect(() => extractThemeFromLocation()).toThrowError('Parameter "tgWebAppThemeParams" missing.');
+        expect(() => extractThemeFromLocation()).toThrowError('Parameter "tgWebAppThemeParams" is missing.');
       },
     );
 
