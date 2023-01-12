1c018fb13c59eaf38e175e1f4bd4fb4f1556652b
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-12 17:34:36 +0500

    refactor(utils): improve versioning utils. Remove unused functions

--- packages/sdk/src/utils/version.ts
+++ packages/sdk/src/utils/version.ts
@@ -1,9 +1,8 @@
 import {PostEventName, supports} from 'twa-bridge';
 import {Version} from 'twa-core';
 
-export type SupportChecker<Method extends string> = (
+export type SupportsFunc<Method extends string> = (
   method: Method,
-  version: Version,
 ) => boolean;
 
 /**
@@ -11,24 +10,12 @@ export type SupportChecker<Method extends string> = (
  * and values as bridge post event name or names. It returns function which
  * validates if specified method name is supported assuming, it uses specified
  * bridge event names.
+ * @param version - Web Apps version.
  * @param schema - validation schema.
  */
-export function createSupportChecker<Method extends string>(
-  schema: Record<Method, PostEventName | PostEventName[]>,
-): SupportChecker<Method> {
-  const formattedSchema = Object
-    .entries(schema)
-    .reduce((acc, [method, deps]) => {
-      (acc as any)[method] = Array.isArray(deps) ? deps : [deps];
-      return acc;
-    }, {} as Record<Method, PostEventName[]>);
-
-  return (method, version) => {
-    for (let i = 0; i < formattedSchema[method].length; i++) {
-      if (!supports(formattedSchema[method][i], version)) {
-        return false;
-      }
-    }
-    return true;
-  };
+export function createSupportsFunc<Method extends string>(
+  version: Version,
+  schema: Record<Method, PostEventName>,
+): SupportsFunc<Method> {
+  return method => supports(schema[method], version);
 }
