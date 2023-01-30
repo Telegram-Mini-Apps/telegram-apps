2cfd0f0af93b392e6fbbfb3c6a7ebd8eba253bbc
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-31 02:26:57 +0500

    chore(playground): add sourcemaps to rollup. Add it also to ignore

--- packages/bridge/playground/rollup.config.js
+++ packages/bridge/playground/rollup.config.js
@@ -3,17 +3,20 @@ import serve from 'rollup-plugin-serve';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import {defineConfig} from 'rollup';
 import {fileURLToPath, URL} from 'node:url';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default defineConfig({
   input: fileURLToPath(new URL('index.ts', import.meta.url)),
   output: {
     file: fileURLToPath(new URL('bundle.js', import.meta.url)),
-    format: 'iife'
+    format: 'iife',
+    sourcemap: true,
   },
   plugins: [
     typescript({
-      tsconfig: fileURLToPath(new URL('tsconfig.json', import.meta.url))
+      tsconfig: fileURLToPath(new URL('tsconfig.json', import.meta.url)),
     }),
+    sourcemaps(),
     nodeResolve(),
     serve({contentBase: 'playground'}),
   ],
