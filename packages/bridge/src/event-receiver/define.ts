5d0a99f46f1f8aa185325cfb65d0f90de194fde2
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-31 01:17:57 +0500

    fix(event receiver): make defineEventReceiver pure. Move emitting function to separate declaration

--- packages/bridge/src/event-receiver/define.ts
+++ packages/bridge/src/event-receiver/define.ts
@@ -1,16 +1,22 @@
 /**
- * States, that receiver was defined before.
+ * Emits event sent from Telegram native application like it was sent in
+ * default web environment between 2 iframes. It dispatches new MessageEvent
+ * and expects it to be handled via `window.addEventListener('message', ...)`
+ * as developer would do it to handle messages sent from parent iframe.
+ * @param eventType
+ * @param eventData
  */
-let receiverDefined = false;
+function emitTelegramEvent(eventType: string, eventData: unknown): void {
+  window.dispatchEvent(new MessageEvent('message', {
+    data: JSON.stringify({eventType, eventData}),
+  }));
+}
 
 /**
  * Defines global event receiver, which handles events sent from native
  * Telegram application. As a result, this handler emits "message" event.
  */
-function defineEventReceiver(force = false): void {
-  if (receiverDefined && !force) {
-    return;
-  }
+function defineEventReceiver(): void {
   const wnd = window as any;
 
   // Iterate over each path, where "receiveEvent" function should be
@@ -27,16 +33,7 @@ function defineEventReceiver(force = false): void {
     path.forEach((item, idx, arr) => {
       // We are on the last iteration, where function property name is passed.
       if (idx === arr.length - 1) {
-        // Here we define our custom receive event function which just emits
-        // new custom event in window. We assume, that there is some event
-        // listener which uses classic way of handling events from one iframe
-        // to another - `window.postEvent` to send
-        // and `window.addEventListener('message')` to receive.
-        pointer[item] = (eventType: string, eventData: unknown): void => {
-          window.dispatchEvent(
-            new CustomEvent('message', {detail: {eventType, eventData}}),
-          );
-        };
+        pointer[item] = emitTelegramEvent;
         return;
       }
 
@@ -46,9 +43,6 @@ function defineEventReceiver(force = false): void {
       pointer = pointer[item];
     });
   });
-
-  // To prevent another receiver installation, set flag.
-  receiverDefined = true;
 }
 
 export {defineEventReceiver};
