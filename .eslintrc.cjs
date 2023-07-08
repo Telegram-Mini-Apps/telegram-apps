d2e9ae8569f941097559fe5cad38a4c633466f0f
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-08 05:36:42 +0300

    chore(eslint): update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  ignorePatterns: ['**/*.test.ts', '**/__tests__/**'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
   root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ["custom"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/"],
+    },
+  },
 };
