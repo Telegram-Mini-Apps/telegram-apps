6d8df5534722d5c378414f9e4c776497ff1f0ff3
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-08 12:47:33 +0500

    chore(eslint): configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,9 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ['custom'],
   settings: {
     next: {
-      rootDir: ["apps/*/"],
+      rootDir: ['apps/*/'],
     },
   },
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,4 +1,12 @@
 module.exports = {
-  extends: ["turbo"],
-  // TODO: Should probably extend airbnb here.
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // We don't use default exports anywhere.
+    'import/prefer-default-export': 0,
+    // Usage of "continue" is normal and prevents code from nesting.
+    'no-continue': 0,
+  }
 };
