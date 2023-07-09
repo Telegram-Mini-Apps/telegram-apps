7873e151e8dd7d6411563c74f97e7151a2deaab9
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-09 08:00:09 +0500

    build(rollup): replace rollup-plugin-delete with rimraf

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -4,7 +4,6 @@ import terser from '@rollup/plugin-terser';
 import { defineConfig } from 'rollup';
 import { createRequire } from 'node:module';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import del from 'rollup-plugin-delete';
 
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
@@ -16,5 +15,5 @@ export default defineConfig({
     { file: pkg.browser, format: 'iife', name: 'TwaUtils', sourcemap: true },
     { file: pkg.module, format: 'esm', sourcemap: true },
   ],
-  plugins: [del({ targets: ['dist/*'] }), typescript(), sourcemaps(), terser()],
+  plugins: [typescript(), sourcemaps(), terser()],
 });
