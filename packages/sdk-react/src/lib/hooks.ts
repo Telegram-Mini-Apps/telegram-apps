73fed4c0ec68c97dbba5a7ea49513b12a3679341
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-11 14:39:17 +0500

    fix(hooks): fix deps of useEventsTracking

--- packages/sdk-react/src/lib/hooks.ts
+++ packages/sdk-react/src/lib/hooks.ts
@@ -1,4 +1,4 @@
-import {useCallback, useState, useEffect} from 'react';
+import { useCallback, useState, useEffect } from 'react';
 
 interface Trackable {
   on: (event: any, ...args: any[]) => void;
@@ -8,6 +8,15 @@ interface Trackable {
 type EventName<T extends Trackable> = T extends {
   on(event: infer E, ...args: any[]): any
 } ? E : never;
+
+/**
+ * The hook which returns function used for force update.
+ */
+export function useForceUpdate(): (() => void) {
+  const [, setHash] = useState(0);
+  // TODO: Maybe just assign new symbol?
+  return useCallback(() => setHash((hash) => (hash + 1) % 1_000_000), []);
+}
 
 /**
  * The hook which listens to the specified object events and calls force update
@@ -24,17 +33,9 @@ export function useEventsTracking<T extends Trackable>(
 
   useEffect(() => {
     // Start listening to each event.
-    events.forEach(event => trackable.on(event, forceUpdate));
+    events.forEach((event) => trackable.on(event, forceUpdate));
 
     // On cleanup remove event listeners.
-    return () => events.forEach(event => trackable.off(event, forceUpdate));
-  }, [forceUpdate]);
-}
-
-/**
- * The hook which returns function used for force update.
- */
-export function useForceUpdate(): (() => void) {
-  const [, setHash] = useState(0);
-  return useCallback(() => setHash(hash => (hash + 1) % 1_000_000), []);
+    return () => events.forEach((event) => trackable.off(event, forceUpdate));
+  }, [trackable, forceUpdate, events]);
 }
