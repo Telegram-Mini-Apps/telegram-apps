2cee654eb9a7a2257394da6a136c2d88ced3a29b
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-16 16:17:06 +0500

    tests(jest): fix tests and jest config environment

--- packages/init-data-node/__tests__/validation.ts
+++ packages/init-data-node/__tests__/validation.ts
@@ -1,3 +1,4 @@
+import { URLSearchParams } from 'url';
 import { validate } from '../src';
 
 const sp = 'query_id=AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2';
