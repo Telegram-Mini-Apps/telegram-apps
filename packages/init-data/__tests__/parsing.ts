e44958c33700fb9b094a02ed3ff1527917f52a86
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-07-29 12:27:53 +0300

    fix(imports): fix imports from @twa.js/init-data

--- packages/init-data/__tests__/parsing.ts
+++ packages/init-data/__tests__/parsing.ts
@@ -1,14 +1,14 @@
-import { parseInitData } from '../src';
+import { initData } from '../src';
 
 describe('parsing', () => {
   describe('parseInitData', () => {
     it('should throw an error in case, passed value does not match required schema', () => {
-      expect(() => parseInitData('hash=someHash')).toThrow();
+      expect(() => initData('hash=someHash')).toThrow();
     });
 
     it('should return result in case, passed value matches schema', () => {
-      const initData = 'auth_date=1672418130&hash=68570a4968ca87b8bde6fae0e6a2e30486af899bf80982105d16dc42fe89b45d&query_id=AAHdF6IQAAAAAN0XohASX1Fh&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D';
-      expect(parseInitData(initData)).toStrictEqual({
+      const initDataString = 'auth_date=1672418130&hash=68570a4968ca87b8bde6fae0e6a2e30486af899bf80982105d16dc42fe89b45d&query_id=AAHdF6IQAAAAAN0XohASX1Fh&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D';
+      expect(initData(initDataString)).toStrictEqual({
         hash: '68570a4968ca87b8bde6fae0e6a2e30486af899bf80982105d16dc42fe89b45d',
         authDate: new Date(1672418130000),
         queryId: 'AAHdF6IQAAAAAN0XohASX1Fh',
