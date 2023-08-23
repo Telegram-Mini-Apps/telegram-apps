d1dcf877893d001bf9d5d6fa9ee3f3fda3567c73
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-08-23 21:50:32 +0300

    feat(utils): add isTWA util. Export more utilities and types

---
+++ packages/sdk/src/utils/env.ts
@@ -0,0 +1,17 @@
+import { retrieveLaunchParams } from './launch-params.js';
+
+/**
+ * Returns true in case, current environment is Telegram Web Apps.
+ *
+ * `isTWA` utilizes such function as `retrieveLaunchParams`, which attempts to retrieve
+ * launch parameters from the current environment.
+ * @see retrieveLaunchParams
+ */
+export function isTWA(): boolean {
+  try {
+    retrieveLaunchParams();
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
--- packages/sdk/src/utils/index.ts
+++ packages/sdk/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './colors.js';
+export * from './env.js';
 export * from './launch-params.js';
 export * from './theme-params.js';
 export * from './url.js';
