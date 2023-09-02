c436f2a09a33f0e035b152ee4a24aa26d6180ffb
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-09-02 20:45:54 +0300

    feat(init): improve cssVars option. Add CSS bindings to viewport. Refactor function

--- packages/sdk/src/init/css.ts
+++ packages/sdk/src/init/css.ts
@@ -1,20 +1,42 @@
 import type { RGB } from '@twa.js/utils';
 
-import type { ThemeParams, WebApp } from '../components/index.js';
+import type { ThemeParams, WebApp, Viewport } from '../components/index.js';
 
 /**
- * Sets new CSS variable in case, its value is not null.
+ * Sets CSS variable.
+ * @param name - variable name.
+ * @param value - variable value.
+ */
+function setVariable(name: string, value: string): void {
+  document.documentElement.style.setProperty(name, value);
+}
+
+/**
+ * Sets new CSS color variable in case, its value is not null.
  * @param name - variable name.
  * @param color - variable value.
  */
-function setCssVariable(name: string, color: RGB | null): void {
+function setColorVariable(name: string, color: RGB | null): void {
   if (color === null) {
     return;
   }
-  document.documentElement.style.setProperty(name, color);
+  setVariable(name, color);
 }
 
-function applyTheme(themeParams: ThemeParams): void {
+/**
+ * Sets new CSS variable which value is amount of pixels.
+ * @param name - variable name.
+ * @param size - variable value.
+ */
+function setSizeVariable(name: string, size: number) {
+  setVariable(name, `${size}px`);
+}
+
+/**
+ * Creates CSS variables based on theme parameters.
+ * @param themeParams - ThemeParams instance.
+ */
+function createThemeVariables(themeParams: ThemeParams): void {
   const {
     backgroundColor,
     buttonTextColor,
@@ -25,32 +47,33 @@ function applyTheme(themeParams: ThemeParams): void {
     textColor,
   } = themeParams;
 
-  setCssVariable('--tg-theme-bg-color', backgroundColor);
-  setCssVariable('--tg-theme-button-color', buttonColor);
-  setCssVariable('--tg-theme-button-text-color', buttonTextColor);
-  setCssVariable('--tg-theme-hint-color', hintColor);
-  setCssVariable('--tg-theme-link-color', linkColor);
-  setCssVariable('--tg-theme-secondary-bg-color', secondaryBackgroundColor);
-  setCssVariable('--tg-theme-text-color', textColor);
-}
-
-function applyWebApp(webApp: WebApp, themeParams: ThemeParams) {
-  const { backgroundColor, secondaryBackgroundColor } = themeParams;
-  const { backgroundColor: webAppBackgroundColor, headerColor } = webApp;
-
-  setCssVariable('--tg-bg-color', webAppBackgroundColor);
-  setCssVariable('--tg-header-color', headerColor === 'bg_color' ? backgroundColor : secondaryBackgroundColor);
+  setColorVariable('--tg-theme-bg-color', backgroundColor);
+  setColorVariable('--tg-theme-button-color', buttonColor);
+  setColorVariable('--tg-theme-button-text-color', buttonTextColor);
+  setColorVariable('--tg-theme-hint-color', hintColor);
+  setColorVariable('--tg-theme-link-color', linkColor);
+  setColorVariable('--tg-theme-secondary-bg-color', secondaryBackgroundColor);
+  setColorVariable('--tg-theme-text-color', textColor);
 }
 
 /**
- * Accepts Web App and Theme Params instance extracting properties connected
- * with colors and creates global CSS variables.
+ * Creates CSS variables based on Web App background and header colors with
+ * theme parameters.
+ * @param webApp - WebApp instance.
+ * @param themeParams - theme parameters.
+ */
+function createWebAppVariables(webApp: WebApp, themeParams: ThemeParams): void {
+  const { backgroundColor, secondaryBackgroundColor } = themeParams;
+  const { backgroundColor: webAppBackgroundColor, headerColor } = webApp;
+
+  setColorVariable('--tg-bg-color', webAppBackgroundColor);
+  setColorVariable('--tg-header-color', headerColor === 'bg_color' ? backgroundColor : secondaryBackgroundColor);
+}
+
+/**
+ * Creates CSS variables connected with theme parameters.
  *
- * WebApp variables:
- * - `--tg-bg-color`
- * - `--tg-header-color`
- *
- * ThemeParams variables:
+ * Created variables:
  * - `--tg-theme-bg-color`
  * - `--tg-theme-button-color`
  * - `--tg-theme-button-text-color`
@@ -60,20 +83,70 @@ function applyWebApp(webApp: WebApp, themeParams: ThemeParams) {
  * - `--tg-theme-text-color`
  *
  * Variables are being automatically updated in case, corresponding properties
- * updated in passed Web App and Theme Params instances.
+ * updated in passed ThemeParams instance.
  *
- * @param webApp
- * @param themeParams
+ * @param themeParams - ThemeParams instance.
  */
-export function bindCSSVariables(webApp: WebApp, themeParams: ThemeParams): void {
-  const actualizeAll = () => {
-    applyTheme(themeParams);
-    applyWebApp(webApp, themeParams);
+export function bindThemeCSSVariables(themeParams: ThemeParams): void {
+  const actualize = () => createThemeVariables(themeParams);
+
+  themeParams.on('changed', actualize);
+
+  actualize();
+}
+
+/**
+ * Creates CSS variables connected with WebApp background and header colors based on
+ * passed WebApp and ThemeParams instances.
+ *
+ * Created variables:
+ * - `--tg-bg-color`
+ * - `--tg-header-color`
+ *
+ * Variables are being automatically updated in case, corresponding properties are updating.
+ *
+ * @param webApp - WebApp instance.
+ * @param themeParams - ThemeParams instance.
+ */
+export function bindWebAppVariables(webApp: WebApp, themeParams: ThemeParams): void {
+  const actualize = () => createWebAppVariables(webApp, themeParams);
+
+  themeParams.on('changed', actualize);
+  webApp.on('backgroundColorChanged', actualize);
+  webApp.on('headerColorChanged', actualize);
+
+  actualize();
+}
+
+/**
+ * Accepts Viewport instance and sets CSS variables connected with viewport
+ * sizes.
+ *
+ * Be careful using this function as long as it can impact application
+ * performance. Viewport size is changing rather often, this makes CSS
+ * variables update, which leads to possible layout redraw.
+ *
+ * Variables:
+ * - `--tg-viewport-height`
+ * - `--tg-viewport-stable-height`
+ *
+ * Variables are being automatically updated in case, corresponding properties
+ * updated in passed Viewport instance.
+ *
+ * @param viewport - Viewport instance.
+ */
+export function bindViewportCSSVariables(viewport: Viewport): void {
+  const setHeight = () => {
+    setSizeVariable('--tg-viewport-height', viewport.height);
   };
 
-  themeParams.on('changed', actualizeAll);
-  webApp.on('backgroundColorChanged', () => applyWebApp(webApp, themeParams));
-  webApp.on('headerColorChanged', () => applyWebApp(webApp, themeParams));
+  const setStableHeight = () => {
+    setSizeVariable('--tg-viewport-stable-height', viewport.stableHeight);
+  };
 
-  actualizeAll();
+  viewport.on('heightChanged', setHeight);
+  viewport.on('stableHeightChanged', setStableHeight);
+
+  setHeight();
+  setStableHeight();
 }
