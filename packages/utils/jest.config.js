56d34955be20170d31e059e5eef2d8896c37c13c
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-09-10 19:32:37 +0300

    chore(tests): remove jsdom from jest config

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -3,7 +3,6 @@ import tsConfig from 'jest-config-custom/ts';
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...tsConfig,
-  testEnvironment: 'jsdom',
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.ts',
