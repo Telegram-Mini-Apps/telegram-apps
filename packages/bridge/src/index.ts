d100ce70356d42835faf3860e0e5953a07538883
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-09-15 22:48:52 +0300

    feat(methods): implement request method

--- packages/bridge/src/index.ts
+++ packages/bridge/src/index.ts
@@ -2,4 +2,5 @@ export * from './events/index.js';
 export * from './methods/index.js';
 export * from './env.js';
 export { setDebug, setTargetOrigin } from './globals.js';
+export * from './request.js';
 export * from './supports.js';
