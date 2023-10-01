514182cbb6bcd1ec01faeb10e1e71708e2ea0a1c
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-10-01 16:19:36 +0300

    chore(playground): fix import path in comments

--- apps/local-playground/index.ts
+++ apps/local-playground/index.ts
@@ -2,7 +2,7 @@
 * You can import any code from the other folders in mono-repo. For example, you could
 * use this code:
 *
-* import { postEvent } from '../../packages/bridge/src/index.ts';
+* import { postEvent } from '../../packages/bridge/src/index.js';
 *
 * And test postEvent function here.
 * */
