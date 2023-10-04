404c30885ad9cbafaa75e618af19314d8ad8c99d
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-10-04 21:41:15 +0300

    refactor(exports): export more utilities and types

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,8 +1,7 @@
 export * from './components/index.js';
-export { init, type InitResult, type InitOptions } from './init/index.js';
-export { MethodNotSupportedError } from './errors/index.js';
+export * from './errors/index.js';
+export * from './init/index.js';
 export * from './env.js';
 export * from './launch-params.js';
-export * from './theme-params.js';
 export * from './types.js';
 export * from './url.js';
