421dd1165d640508603859b4e7f9ff32d645c095
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-10-23 00:12:44 +0300

    feat(packages): implement @tma.js/solid-router-integration

---
+++ packages/solid-router-integration/vite.config.ts
@@ -0,0 +1,9 @@
+import { createViteConfig } from 'build-utils';
+
+import packageJson from './package.json';
+
+export default createViteConfig({
+  packageName: packageJson.name,
+  formats: ['es', 'cjs'],
+  external: ['@tma.js/navigation', '@solidjs/router'],
+});
