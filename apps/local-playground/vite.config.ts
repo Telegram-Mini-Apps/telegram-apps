a3b0b8f084467a249f3efc506fce4554a27d54ec
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-10-23 00:15:04 +0300

    build(server): add an opportunity to launch dev server at HTTPS

--- apps/local-playground/vite.config.ts
+++ apps/local-playground/vite.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite';
+import { readFileSync } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, type ServerOptions } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+// TODO: Add docs link for getServerOptions.
+
+/**
+ * Returns Vite dev server options.
+ */
+function getServerOptions(): ServerOptions {
+  const dir = dirname(fileURLToPath(import.meta.url));
+
+  return {
+    port: 3000,
+    https: {
+      cert: readFileSync(resolve(dir, ' ../../https-cert.pem')),
+      key: readFileSync(resolve(dir, ' ../../https-key.pem')),
+    },
+    host: 'tma.internal',
+  };
+}
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  // Comment this line in case, you would like to run Vite dev server using HTTP. Otherwise,
+  // it would be launched using HTTPS protocol.
+  server: getServerOptions(),
 });
+
