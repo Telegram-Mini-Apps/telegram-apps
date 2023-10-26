815c82e4ac3c0e906f8fc971fc22f5b498f2e4bd
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-10-27 01:48:45 +0300

    feat(boolean): improve boolean parser

--- packages/parsing/tests/parsers/boolean.ts
+++ packages/parsing/tests/parsers/boolean.ts
@@ -9,8 +9,20 @@ describe('parsers', () => {
         expect(boolean().parse(false)).toBe(false);
       });
 
-      it('should throw an error in case, passed value is not of type boolean', () => {
-        expect(() => boolean().parse('true')).toThrow();
+      it('should return true in case, value string representation equals "1" or "true"', () => {
+        expect(boolean().parse(1)).toBe(true);
+        expect(boolean().parse('1')).toBe(true);
+        expect(boolean().parse('true')).toBe(true);
+      });
+
+      it('should return false in case, value string representation equals "0" or "false"', () => {
+        expect(boolean().parse(0)).toBe(false);
+        expect(boolean().parse('0')).toBe(false);
+        expect(boolean().parse('false')).toBe(false);
+      });
+
+      it('should throw an error in case, passed value is not of type boolean and its string representation is not "0", "1", "true" or "false"', () => {
+        expect(() => boolean().parse('true!')).toThrow();
         expect(() => boolean().parse({})).toThrow();
       });
 
