fe19f38eaed2a8224605c575a65a8343d8f32029
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-05 17:21:49 +0300

    feat(keys): add new v6.10 palette keys

--- packages/theme-params/src/themeParams.ts
+++ packages/theme-params/src/themeParams.ts
@@ -4,38 +4,30 @@ import type { ThemeParams } from './types.js';
 
 const rgbOptional = rgb().optional();
 
+function field(from: string) {
+  return {
+    type: rgbOptional,
+    from,
+  };
+}
+
 /**
  * Returns parser used to parse theme parameters.
  */
 export function themeParams() {
   return json<ThemeParams>({
-    backgroundColor: {
-      type: rgbOptional,
-      from: 'bg_color',
-    },
-    buttonColor: {
-      type: rgbOptional,
-      from: 'button_color',
-    },
-    buttonTextColor: {
-      type: rgbOptional,
-      from: 'button_text_color',
-    },
-    hintColor: {
-      type: rgbOptional,
-      from: 'hint_color',
-    },
-    linkColor: {
-      type: rgbOptional,
-      from: 'link_color',
-    },
-    secondaryBackgroundColor: {
-      type: rgbOptional,
-      from: 'secondary_bg_color',
-    },
-    textColor: {
-      type: rgbOptional,
-      from: 'text_color',
-    },
+    accentTextColor: field('accent_text_color'),
+    backgroundColor: field('bg_color'),
+    buttonColor: field('button_color'),
+    buttonTextColor: field('button_text_color'),
+    destructiveTextColor: field('destructive_text_color'),
+    headerBackgroundColor: field('header_bg_color'),
+    hintColor: field('hint_color'),
+    linkColor: field('link_color'),
+    secondaryBackgroundColor: field('secondary_bg_color'),
+    sectionHeaderTextColor: field('section_header_text_color'),
+    sectionBackgroundColor: field('section_bg_color'),
+    subtitleTextColor: field('subtitle_text_color'),
+    textColor: field('text_color'),
   });
 }
--- packages/theme-params/src/types.ts
+++ packages/theme-params/src/types.ts
@@ -1,14 +1,39 @@
 import type { RGB } from '@tma.js/colors';
 
 /**
- * Application theme parameters. Defines palette used by the Telegram application.
+ * Application [theme parameters](https://docs.telegram-mini-apps.com/functionality/theming).
+ * Defines palette used by the Telegram application.
  */
 export interface ThemeParams {
+  /**
+   * @since v6.10
+   */
+  accentTextColor?: RGB;
   backgroundColor?: RGB;
   buttonColor?: RGB;
   buttonTextColor?: RGB;
+  /**
+   * @since v6.10
+   */
+  destructiveTextColor?: RGB;
+  /**
+   * @since v6.10
+   */
+  headerBackgroundColor?: RGB;
   hintColor?: RGB;
   linkColor?: RGB;
   secondaryBackgroundColor?: RGB;
+  /**
+   * @since v6.10
+   */
+  sectionBackgroundColor?: RGB;
+  /**
+   * @since v6.10
+   */
+  sectionHeaderTextColor?: RGB;
+  /**
+   * @since v6.10
+   */
+  subtitleTextColor?: RGB;
   textColor?: RGB;
 }
