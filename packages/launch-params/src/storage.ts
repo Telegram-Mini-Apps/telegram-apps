f3d35aa67dfdd7d418b7c22d2e3b8801d9024ef8
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-10 23:15:44 +0300

    fix(storage): make retrieveFromStorage unsafer. Add methods comments

--- packages/launch-params/src/storage.ts
+++ packages/launch-params/src/storage.ts
@@ -5,25 +5,28 @@ import type { LaunchParams } from './types.js';
 const SESSION_STORAGE_KEY = 'telegram-mini-apps-launch-params';
 
 /**
- * Attempts to extract launch parameters directly from the sessionStorage.
+ * Attempts to extract launch parameters directly from the session storage.
+ * @returns Launch parameters in case, they were stored before or null, if there is no launch
+ * parameters key in the session storage.
+ * @throws {Error} Data stored in the session storage is invalid.
  */
 export function retrieveFromStorage(): LaunchParams | null {
   const raw = sessionStorage.getItem(SESSION_STORAGE_KEY);
-  if (raw) {
-    try {
-      return parse(raw);
-      // eslint-disable-next-line no-empty
-    } catch (e) {
-    }
-  }
 
-  return null;
+  return raw
+    // We are not handling the error on purpose as long as we are waiting for data stored by
+    // this session storage key to contain the valid launch parameters.
+    ? parse(raw)
+    : null;
 }
 
 /**
- * Saves specified launch params to session storage.
+ * Saves specified launch parameters in the session storage.
  * @param value - launch params to save.
  */
 export function saveToStorage(value: LaunchParams): void {
+  // TODO: We probably don't need serialize here. We used it only to correctly serialize Date
+  //  values which being converted strings. To solve the problem we could improve date parser
+  //  to allows parsing such invalid (Dates, converted to strings) values.
   sessionStorage.setItem(SESSION_STORAGE_KEY, serialize(value));
 }
