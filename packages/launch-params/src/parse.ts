e73eba36b1453d2a2f29d84d839d6f10d1d68bbb
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-11 00:21:46 +0300

    chore(parse): add thrown error jsdoc

--- packages/launch-params/src/parse.ts
+++ packages/launch-params/src/parse.ts
@@ -4,6 +4,7 @@ import type { LaunchParams } from './types.js';
 /**
  * Parses incoming value as launch parameters.
  * @param value - value to parse.
+ * @throws {Error} Value contains invalid data.
  */
 export function parse(value: unknown): LaunchParams {
   return launchParams().parse(value);
