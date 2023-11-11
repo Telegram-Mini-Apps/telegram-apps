12a62aee28ece408fcef38f8cc9df813a4e6d7a5
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-11 19:41:32 +0300

    refactor(launch-params): remove launch parameters utilities

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,6 +2,5 @@ export * from './components/index.js';
 export * from './errors/index.js';
 export * from './init/index.js';
 export * from './env.js';
-export * from './launch-params.js';
 export * from './types.js';
 export * from './url.js';
