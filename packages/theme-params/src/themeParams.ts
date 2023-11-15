5e3c5c5b3f6af298ca6562cf3ae8e5d0e29721e2
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-15 19:09:23 +0300

    feat(themeParams): improve ThemeParams type and its parser

--- packages/theme-params/src/themeParams.ts
+++ packages/theme-params/src/themeParams.ts
@@ -1,35 +1,18 @@
-import { json, rgb } from '@tma.js/parsing';
+import { createValueParserGenerator, rgb, toRecord } from '@tma.js/parsing';
 
 import type { ThemeParams } from './types.js';
+import { keyToLocal } from './keys.js';
 
-const rgbOptional = rgb().optional();
+export const themeParams = createValueParserGenerator<ThemeParams>(
+  (value) => {
+    const parser = rgb().optional();
 
-function field(from: string) {
-  return {
-    type: rgbOptional,
-    from,
-  };
-}
-
-/**
- * Returns parser used to parse theme parameters.
- */
-export function themeParams() {
-  return json<ThemeParams>({
-    accentTextColor: field('accent_text_color'),
-    backgroundColor: field('bg_color'),
-    buttonColor: field('button_color'),
-    buttonTextColor: field('button_text_color'),
-    destructiveTextColor: field('destructive_text_color'),
-    headerBackgroundColor: field('header_bg_color'),
-    hintColor: field('hint_color'),
-    linkColor: field('link_color'),
-    secondaryBackgroundColor: field('secondary_bg_color'),
-    sectionHeaderTextColor: field('section_header_text_color'),
-    sectionBackgroundColor: field('section_bg_color'),
-    subtitleTextColor: field('subtitle_text_color'),
-    textColor: field('text_color'),
-  }, {
-    type: 'ThemeParams',
-  });
-}
+    return Object
+      .entries(toRecord(value))
+      .reduce<ThemeParams>((acc, [k, v]) => {
+        acc[keyToLocal(k)] = parser.parse(v);
+        return acc;
+      }, {});
+  },
+  'ThemeParams',
+);
