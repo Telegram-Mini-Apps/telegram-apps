93fde77361f72157bf53b5a139582c02d70ce65b
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-15 19:07:18 +0300

    refactor(json): replace content with toRecord function

--- packages/parsing/src/parsers/json.ts
+++ packages/parsing/src/parsers/json.ts
@@ -1,51 +1,17 @@
 import { ValueParser } from '../ValueParser.js';
-import { isUndefined } from '../isUndefined.js';
-import { unexpectedTypeError } from '../unexpectedTypeError.js';
 import { parseBySchema } from '../parseBySchema.js';
-import type { Schema, IsEmptyFunc } from '../types.js';
-
-interface Options {
-  /**
-   * Type name.
-   */
-  type?: string;
-
-  /**
-   * Should return true if passed value is recognized empty for this parser.
-   * @default Function which returns true if value is undefined or null.
-   */
-  isEmpty?: IsEmptyFunc;
-}
+import { toRecord } from '../toRecord.js';
+import type { Schema } from '../types.js';
 
 /**
  * Creates new Json parser according to passed schema.
  * @param schema - object schema.
- * @param options - additional options.
+ * @param type - parser type name.
  */
-export function json<T>(schema: Schema<T>, options: Options = {}): ValueParser<T, false> {
-  const {
-    type,
-    isEmpty = isUndefined,
-  } = options;
-
+export function json<T>(schema: Schema<T>, type?: string): ValueParser<T, false> {
   return new ValueParser((value) => {
-    let formattedValue: any = value;
+    const record = toRecord(value);
 
-    // Convert value to JSON in case, it is string. We expect value to be
-    // JSON string.
-    if (typeof formattedValue === 'string') {
-      formattedValue = JSON.parse(formattedValue);
-    }
-
-    // We expect json to be usual object.
-    if (
-      typeof formattedValue !== 'object'
-      || formattedValue === null
-      || Array.isArray(formattedValue)
-    ) {
-      throw unexpectedTypeError();
-    }
-
-    return parseBySchema(schema, (field) => formattedValue[field]);
-  }, false, isEmpty, type);
+    return parseBySchema(schema, (field) => record[field]);
+  }, false, type);
 }
