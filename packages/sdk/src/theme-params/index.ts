6a79c6055631aabeed83cd62c901f61cf0e92824
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-26 02:26:10 +0300

    feat(theme-params): implement requestThemeParams function

--- packages/sdk/src/theme-params/index.ts
+++ packages/sdk/src/theme-params/index.ts
@@ -1,4 +1,5 @@
 export * from './parseThemeParams.js';
+export * from './requestThemeParams.js';
 export * from './serializeThemeParams.js';
 export * from './ThemeParams.js';
 export * from './themeParamsParser.js';
