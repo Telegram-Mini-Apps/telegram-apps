892f989b7ca837d03ed68fcd7749df177af220bf
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-26 02:21:44 +0300

    fix(init-data): remove serializeInitData function

--- packages/sdk/src/init-data/index.ts
+++ packages/sdk/src/init-data/index.ts
@@ -2,6 +2,5 @@ export * from './chatParser.js';
 export * from './InitData.js';
 export * from './initDataParser.js';
 export * from './parseInitData.js';
-export * from './serializeInitData.js';
 export * from './types.js';
 export * from './userParser.js';
