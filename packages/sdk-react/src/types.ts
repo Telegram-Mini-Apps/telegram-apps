faffd8ff35baf63edef7e8d9c2d83c3f7885e6f1
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-11-26 23:05:40 +0300

    refactor(types): move types to separate file

---
+++ packages/sdk-react/src/types.ts
@@ -0,0 +1,29 @@
+import type { InitOptions, InitResult } from '@tma.js/sdk';
+
+export { InitOptions, InitResult };
+
+export interface Trackable {
+  on(event: 'change', listener: () => void): () => void;
+}
+
+/**
+ * SDK init result keys, which describe values with trackable changes.
+ */
+export type DynamicInitResultKey = {
+  [K in InitResultKey]-?: InitResultValue<K> extends Trackable ? K : never;
+}[InitResultKey];
+
+/**
+ * SDK init result keys, which describe static values.
+ */
+export type StaticInitResultKey = Exclude<InitResultKey, DynamicInitResultKey>;
+
+/**
+ * SDK initialization result key.
+ */
+export type InitResultKey = keyof InitResult;
+
+/**
+ * Returns SDK init result value type by its key.
+ */
+export type InitResultValue<K extends InitResultKey> = InitResult[K];
