c2a676232964eb823b39144597fe24ba0c569a78
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-12-14 21:35:59 +0300

    chore(local-playground): actualize

--- apps/local-playground/index.ts
+++ apps/local-playground/index.ts
@@ -2,12 +2,12 @@
  * You can import any code from other packages here. There are currently 2 shortcuts:
  *
  * 1. "@packages/*". Provides access to "packages" directory:
- * import { postEvent } from '@packages/bridge/src/index.js';
+ * import { postEvent } from '@packages/sdk/src/index.js';
  *
  * 2. "@/*". Provides easy access to packages' index files:
- * import { postEvent } from '@/bridge';
+ * import { postEvent } from '@/sdk';
  */
 
-import { postEvent } from '@/bridge';
+import { postEvent } from '@/sdk';
 
 postEvent('web_app_expand');
