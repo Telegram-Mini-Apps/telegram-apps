f198185bf806838df0053c5f36563da70a1b3146
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-12-15 13:38:02 +0300

    feat(bridge): implement invokeCustomMethod function

--- packages/sdk/src/bridge/index.ts
+++ packages/sdk/src/bridge/index.ts
@@ -2,4 +2,5 @@ export * from './env/index.js';
 export * from './errors/index.js';
 export * from './events/index.js';
 export * from './methods/index.js';
+export * from './invoke-custom-method.js';
 export * from './request.js';
