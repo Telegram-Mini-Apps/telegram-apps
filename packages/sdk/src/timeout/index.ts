67af11f9ecce895525515ca674b5727193380545
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-12-15 13:37:36 +0300

    refactor(withTimeout): refactor withTimeout function and accept only function which returns promise

--- packages/sdk/src/timeout/index.ts
+++ packages/sdk/src/timeout/index.ts
@@ -1,3 +1,4 @@
 export * from './isTimeoutError.js';
+export * from './sleep.js';
 export * from './TimeoutError.js';
 export * from './withTimeout.js';
--- packages/sdk/src/timeout/withTimeout.ts
+++ packages/sdk/src/timeout/withTimeout.ts
@@ -1,36 +1,24 @@
 import { TimeoutError } from './TimeoutError.js';
 
-type AnyAsyncFunction = (...args: any[]) => Promise<any>;
-
 /**
  * Creates promise which rejects after timeout milliseconds.
  * @param timeout - timeout in milliseconds.
  */
-function createTimeoutPromise(timeout: number): Promise<void> {
+function createTimeoutPromise(timeout: number): Promise<never> {
   return new Promise((_, rej) => {
     setTimeout(rej, timeout, new TimeoutError(timeout));
   });
 }
 
 /**
- * Rejects specified promise in case, it is processed more than timeout seconds.
- * @param promise - wrapped promise.
- * @param timeout - timeout in milliseconds.
+ * Accepts specified function and instantly executes. It waits for timeout milliseconds for
+ * it to complete and throws an error in case, deadline was reached.
+ * @param func - function to execute.
+ * @param timeout - completion timeout.
  */
-export function withTimeout<P extends Promise<any>>(promise: P, timeout: number): P;
-/**
- * Wraps async function in function using timeout.
- * @param func - wrapped function.
- * @param timeout - async function timeout.
- */
-export function withTimeout<F extends AnyAsyncFunction>(func: F, timeout: number): F;
-export function withTimeout(funcOrPromise: Promise<any> | AnyAsyncFunction, timeout: number) {
-  if (typeof funcOrPromise === 'function') {
-    return (...args: any[]) => Promise.race([
-      funcOrPromise(...args),
-      createTimeoutPromise(timeout),
-    ]);
-  }
-
-  return Promise.race([funcOrPromise, createTimeoutPromise(timeout)]);
+export function withTimeout<T>(func: () => Promise<T>, timeout: number): Promise<T> {
+  return Promise.race([
+    func(),
+    createTimeoutPromise(timeout),
+  ]);
 }
