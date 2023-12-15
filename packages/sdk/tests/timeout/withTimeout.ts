96d6c565c39a4db10e005562ccd92e1b1aa08e31
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-12-15 13:42:33 +0300

    tests(withTimeout): fix withTimeout tests

--- packages/sdk/tests/timeout/withTimeout.ts
+++ packages/sdk/tests/timeout/withTimeout.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { afterAll, beforeAll, expect, it, vi } from 'vitest';
 
 import { withTimeout, TimeoutError } from '~/timeout/index.js';
 
@@ -10,39 +10,19 @@ afterAll(() => {
   vi.useRealTimers();
 });
 
-describe('wrapped value is function', async () => {
-  it('should throw an error in case timeout reached', () => {
-    const wrapped = withTimeout(() => new Promise((res) => {
-      setTimeout(res, 500);
-    }), 100);
+it('should throw an error in case timeout reached', () => {
+  const promise = withTimeout(() => new Promise((res) => {
+    setTimeout(res, 500);
+  }), 100);
 
-    Promise.resolve().then(() => vi.advanceTimersByTime(500));
-    expect(wrapped()).rejects.toStrictEqual(new TimeoutError(100));
-  }, 1000);
+  Promise.resolve().then(() => vi.advanceTimersByTime(500));
+  expect(promise).rejects.toStrictEqual(new TimeoutError(100));
+}, 1000);
 
-  it('should return resolved value by wrapped function', () => {
-    const wrapped = withTimeout(() => new Promise((res) => {
-      res('I am fine');
-    }), 100);
+it('should return resolved value by wrapped function', () => {
+  const promise = withTimeout(() => new Promise((res) => {
+    res('I am fine');
+  }), 100);
 
-    expect(wrapped()).resolves.toBe('I am fine');
-  }, 1000);
-});
-
-describe('wrapped value is promise', () => {
-  it('should throw an error in case timeout reached', () => {
-    Promise.resolve().then(() => vi.advanceTimersByTime(500));
-
-    const wrapped = withTimeout(new Promise((res) => {
-      setTimeout(res, 500);
-    }), 100);
-
-    expect(wrapped).rejects.toStrictEqual(new TimeoutError(100));
-  }, 1000);
-
-  it('should return resolved value by wrapped function', () => {
-    const wrapped = withTimeout(Promise.resolve('I am fine'), 100);
-
-    expect(wrapped).resolves.toBe('I am fine');
-  }, 1000);
-});
+  expect(promise).resolves.toBe('I am fine');
+}, 1000);
