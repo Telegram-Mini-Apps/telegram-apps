bedb903ddd64a59935dc213e2aef24109334338c
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-01-09 02:25:31 +0300

    chore(tests): add coverage

--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -4,6 +4,10 @@ import { fileURLToPath } from 'node:url';
 
 import packageJson from './package.json';
 
+function prefix(name: string): string {
+  return `src/${name}`;
+}
+
 export default createViteConfig({
   packageName: packageJson.name,
   formats: ['es', 'cjs'],
@@ -12,6 +16,34 @@ export default createViteConfig({
   },
   test: createVitestConfig({
     environment: 'happy-dom',
+    coverage: {
+      // TODO: As long as currently we are not covering all code in the project, we exclude some
+      //  of the folders or files from the coverage. To be done in the future.
+      exclude: [
+        prefix('**/index.ts'),
+        prefix('back-button/BackButton.ts'),
+        prefix('bridge/invoke-custom-method.ts'),
+        prefix('closing-behavior/ClosingBehavior.ts'),
+        prefix('init-data/InitData.ts'),
+        prefix('launch-params'),
+        prefix('popup/preparePopupParams.ts'),
+        prefix('state'),
+        prefix('supports/createSupportsParamFunc.ts'),
+        prefix('main-button/MainButton.ts'),
+        prefix('mini-app/MiniApp.ts'),
+        prefix('misc/isTMA.ts'),
+        prefix('navigation/**'),
+        prefix('theme-params/ThemeParams.ts'),
+        prefix('theme-params/requestThemeParams.ts'),
+        prefix('timeout/sleep.ts'),
+        prefix('viewport/Viewport.ts'),
+        prefix('viewport/requestViewport.ts'),
+      ],
+      branches: 100,
+      functions: 100,
+      statements: 100,
+      lines: 100,
+    },
   }),
   tsconfigPath: './tsconfig.build.json',
 });
