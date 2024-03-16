e04c33d52520f4ededff3cdaa9dd57d8019a1a6e
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-16 20:59:32 +0300

    feat(slotToComponents): enhance the utility

--- packages/ui-solid/src/helpers/slotToComponent.ts
+++ packages/ui-solid/src/helpers/slotToComponent.ts
@@ -6,8 +6,8 @@ import type { ComponentSlot } from '~/types/components.js';
  * Converts component slot to component.
  * @param value - value to convert.
  */
-export function slotToComponent<P>(value: ComponentSlot<P>): Component<P> {
-  return typeof value === 'function'
-    ? value
-    : () => value;
+export function slotToComponent<P>(value: ComponentSlot<P> | undefined): Component<P> | undefined {
+  return value
+    ? typeof value === 'function' ? value : () => value
+    : undefined;
 }
