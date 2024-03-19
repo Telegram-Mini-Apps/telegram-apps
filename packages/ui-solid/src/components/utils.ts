28c82655d10ce5c0a8373375a7fe7bda7286606d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-19 22:34:21 +0300

    chore(comments): add some func comments

--- packages/ui-solid/src/components/utils.ts
+++ packages/ui-solid/src/components/utils.ts
@@ -3,6 +3,10 @@ import { mergeProps } from 'solid-js';
 
 import type { ColorScheme, Platform } from '~/types/known.js';
 
+/**
+ * Merges specified properties prepending default color scheme and platform.
+ * @param sources - properties to merge.
+ */
 export function mergeWithConfigDefaults<T extends unknown[]>(
   ...sources: T
 ): MergeProps<[{ colorScheme: ColorScheme, platform: Platform }, ...T]> {
