07cb7b6c730055280563b652eaa653f6b3b3cfee
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-23 22:48:20 +0300

    chore(eslint): better import settings for ui-solid

--- packages/ui-solid/.eslintrc.cjs
+++ packages/ui-solid/.eslintrc.cjs
@@ -3,5 +3,40 @@ module.exports = {
   rules: {
     // TODO: For some reason, eslint can't resolve tsconfig aliases.
     'import/no-unresolved': 0,
+    // Simple sort.
+    // https://github.com/lydell/eslint-plugin-simple-import-sort?tab=readme-ov-file#custom-grouping
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        // Node.js builtins prefixed with `node:`.
+        // node:fs
+        [/^node:/.source],
+
+        // Packages.
+        // solid-js
+        [/^@?\w/.source],
+
+        // Tsconfig alias.
+        // ~/styles
+        [/^~\/(?!components|icons)/.source],
+
+        // Components and icons.
+        // ~/components
+        // ~/icons
+        [/^~\/components/.source, /^~\/icons/.source],
+
+        // Parent imports.
+        // ../Typography.js
+        [/^\.\.\//.source],
+
+        // Current folder imports.
+        // ./utils.js
+        [/\.\/.+\.(?!s?css)/.source],
+
+        // Styles.
+        // ./Typography.scss
+        // ./Typography.css
+        [/\.s?css$/.source],
+      ],
+    }],
   },
 };
