cef9c819fcac1b16e1fcecf2ab0443c4cb0ab195
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-23 23:03:01 +0300

    chore(eslint): return some import rules

--- packages/eslint-config-custom/rules.js
+++ packages/eslint-config-custom/rules.js
@@ -21,6 +21,21 @@ module.exports = {
   'no-void': 0,
   'object-curly-newline': ['error', { consistent: true }],
 
+  // We use special module resolution, that's why we need extensions.
+  'import/extensions': 0,
+  'import/first': 'error',
+  'import/newline-after-import': 'error',
+  'import/no-duplicates': 'error',
+  'import/no-empty-named-blocks': 'error',
+  // We have extraneous deps, because we have workspace.
+  // 'import/no-extraneous-dependencies': 0,
+  'import/no-mutable-exports': 0,
+  'import/no-self-import': 'error',
+  'import/order': 0,
+
+  // We don't use default exports anywhere.
+  'import/prefer-default-export': 0,
+
   // Simple sort.
   // https://github.com/lydell/eslint-plugin-simple-import-sort?tab=readme-ov-file#custom-grouping
   'simple-import-sort/imports': ['error', {
