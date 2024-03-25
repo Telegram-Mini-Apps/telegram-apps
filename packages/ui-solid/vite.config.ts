81aaacf948f02d13da9a44e7285bda4678142a47
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-26 01:19:25 +0300

    build(config): fix invalid and scripts

--- packages/ui-solid/vite.config.ts
+++ packages/ui-solid/vite.config.ts
@@ -1,12 +1,16 @@
 import { createViteConfig } from 'build-utils';
 import solidPlugin from 'vite-plugin-solid';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import packageJson from './package.json';
 
 export default createViteConfig({
   packageName: packageJson.name,
   formats: ['es', 'cjs'],
-  external: ['solid-js'],
-  plugins: [solidPlugin(), tsconfigPaths()],
+  external: ['@tma.js/sdk', 'solid-js'],
+  plugins: [solidPlugin()],
+  alias: {
+    '~': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+  },
 });
