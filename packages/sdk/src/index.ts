50d250a2453ed1bafc8ebe3d1c8b170e1d67c851
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-04-26 22:11:32 +0300

    feat(entries): actualize main entry

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,23 +1,24 @@
 /**
  * Bridge.
  */
-export { hasExternalNotify } from '@/bridge/env/hasExternalNotify.js';
-export { hasWebviewProxy } from '@/bridge/env/hasWebviewProxy.js';
-export { createMiniAppsEmitter } from '@/bridge/events/event-emitter/createMiniAppsEmitter.js';
-// export { once } from '@/bridge/events/miniAppsEmitterSingleton.js';
-// export { off } from '@/bridge/events/off.js';
-// export { on } from '@/bridge/events/on.js';
-// export { once } from '@/bridge/events/once.js';
-// export { once } from '@/bridge/events/onTelegramEvent.js';
-export { subscribe } from '@/bridge/events/subscribe.js';
-export * from '@/bridge/events/types/index.js';
-export { unsubscribe } from '@/bridge/events/unsubscribe.js';
+export { off } from '@/bridge/events/listening/off.js';
+export { on } from '@/bridge/events/listening/on.js';
+export { subscribe } from '@/bridge/events/listening/subscribe.js';
+export { unsubscribe } from '@/bridge/events/listening/unsubscribe.js';
+export * from '@/bridge/events/types.js';
+export { createPostEvent } from '@/bridge/methods/createPostEvent.js';
+export { type PostEvent, postEvent } from '@/bridge/methods/postEvent.js';
+export * from '@/bridge/methods/types/index.js';
+export { setTargetOrigin, targetOrigin } from '@/bridge/target-origin.js';
+export { captureSameReq } from '@/bridge/utils/captureSameReq.js';
+export { invokeCustomMethod } from '@/bridge/utils/invokeCustomMethod.js';
+export { request, type RequestOptions } from '@/bridge/utils/request.js';
 
 /**
  * Classnames.
  */
 export { classNames } from '@/classnames/classNames.js';
-export { mergeClassNames } from '@/classnames/mergeClassNames.js';
+export { type MergeClassNames, mergeClassNames } from '@/classnames/mergeClassNames.js';
 
 /**
  * Colors.
@@ -26,7 +27,7 @@ export { isColorDark } from '@/colors/isColorDark.js';
 export { isRGB } from '@/colors/isRGB.js';
 export { isRGBShort } from '@/colors/isRGBShort.js';
 export { toRGB } from '@/colors/toRGB.js';
-export type { RGB, RGBShort } from '@/colors/types.js';
+export * from '@/colors/types.js';
 
 /**
  * Components.
@@ -35,7 +36,10 @@ export type { RGB, RGBShort } from '@/colors/types.js';
 // Back button.
 export { BackButton } from '@/components/back-button/BackButton.js';
 export { initBackButton } from '@/components/back-button/initBackButton.js';
+export { offBackButtonClick } from '@/components/back-button/offBackButtonClick.js';
+export { onBackButtonClick } from '@/components/back-button/onBackButtonClick.js';
 export type {
+  BackButtonClickListener,
   BackButtonEventListener,
   BackButtonEventName,
   BackButtonEvents,
@@ -43,6 +47,7 @@ export type {
 
 // Closing behavior
 export { ClosingBehavior } from '@/components/closing-behavior/ClosingBehavior.js';
+export { initClosingBehavior } from '@/components/closing-behavior/initClosingBehavior.js';
 export type {
   ClosingBehaviorEventListener,
   ClosingBehaviorEventName,
@@ -51,30 +56,33 @@ export type {
 
 // Cloud storage.
 export { CloudStorage } from '@/components/cloud-storage/CloudStorage.js';
+export { initCloudStorage } from '@/components/cloud-storage/initCloudStorage.js';
 
 // HapticFeedback.
 export { HapticFeedback } from '@/components/haptic-feedback/HapticFeedback.js';
+export { initHapticFeedback } from '@/components/haptic-feedback/initHapticFeedback.js';
 
 // Init data.
 export { InitData } from '@/components/init-data/InitData.js';
 export { parseInitData } from '@/components/init-data/parseInitData.js';
-export { chat } from '@/components/init-data/parsers/chat.js';
-export { initData } from '@/components/init-data/parsers/initData.js';
-export { user } from '@/components/init-data/parsers/user.js';
-export type { Chat, ChatType, InitDataParsed, User } from '@/components/init-data/types.js';
+export * from '@/components/init-data/types.js';
 
 // Invoice.
+export { initInvoice } from '@/components/invoice/initInvoice.js';
 export { Invoice } from '@/components/invoice/Invoice.js';
 export type {
   InvoiceEventListener,
   InvoiceEventName,
   InvoiceEvents,
-  InvoiceState,
 } from '@/components/invoice/types.js';
 
 // Main button.
+export { initMainButton } from '@/components/main-button/initMainButton.js';
 export { MainButton } from '@/components/main-button/MainButton.js';
+export { offMainButtonClick } from '@/components/main-button/offMainButtonClick.js';
+export { onMainButtonClick } from '@/components/main-button/onMainButtonClick.js';
 export type {
+  MainButtonClickListener,
   MainButtonEventListener,
   MainButtonEventName,
   MainButtonEvents,
@@ -83,6 +91,8 @@ export type {
 } from '@/components/main-button/types.js';
 
 // Mini app.
+export { bindMiniAppCSSVars } from '@/components/mini-app/bindMiniAppCSSVars.js';
+export { initMiniApp } from '@/components/mini-app/initMiniApp.js';
 export { MiniApp } from '@/components/mini-app/MiniApp.js';
 export type {
   MiniAppEventListener,
@@ -90,9 +100,11 @@ export type {
   MiniAppEvents,
   MiniAppHeaderColor,
   MiniAppProps,
+  RequestedContact,
 } from '@/components/mini-app/types.js';
 
 // Popup.
+export { initPopup } from '@/components/popup/initPopup.js';
 export { Popup } from '@/components/popup/Popup.js';
 export type {
   OpenPopupOptions,
@@ -103,6 +115,7 @@ export type {
 } from '@/components/popup/types.js';
 
 // QR scanner.
+export { initQRScanner } from '@/components/qr-scanner/initQRScanner.js';
 export { QRScanner } from '@/components/qr-scanner/QRScanner.js';
 export type {
   QRScannerEventListener,
@@ -111,17 +124,22 @@ export type {
 } from '@/components/qr-scanner/types.js';
 
 // Settings button.
+export { initSettingsButton } from '@/components/settings-button/initSettingsButton.js';
+export { offSettingsButtonClick } from '@/components/settings-button/offSettingsButtonClick.js';
+export { onSettingsButtonClick } from '@/components/settings-button/onSettingsButtonClick.js';
 export { SettingsButton } from '@/components/settings-button/SettingsButton.js';
 export type {
+  SettingsButtonClickListener,
   SettingsButtonEventListener,
   SettingsButtonEventName,
   SettingsButtonEvents,
 } from '@/components/settings-button/types.js';
 
 // Theme params.
+export { bindThemeParamsCSSVars } from '@/components/theme-params/bindThemeParamsCSSVars.js';
+export { initThemeParams } from '@/components/theme-params/initThemeParams.js';
 export { parseThemeParams } from '@/components/theme-params/parsing/parseThemeParams.js';
 export { serializeThemeParams } from '@/components/theme-params/parsing/serializeThemeParams.js';
-export { themeParams } from '@/components/theme-params/parsing/themeParams.js';
 export { requestThemeParams } from '@/components/theme-params/requestThemeParams.js';
 export { ThemeParams } from '@/components/theme-params/ThemeParams.js';
 export type {
@@ -133,10 +151,12 @@ export type {
 } from '@/components/theme-params/types.js';
 
 // Utils.
+export { initUtils } from '@/components/utils/initUtils.js';
 export { Utils } from '@/components/utils/Utils.js';
 
 // Viewport.
-export { isStableViewportPlatform } from '@/components/viewport/isStableViewportPlatform.js';
+export { bindViewportCSSVars } from '@/components/viewport/bindViewportCSSVars.js';
+export { initViewport } from '@/components/viewport/initViewport.js';
 export {
   requestViewport,
   type RequestViewportResult,
@@ -150,62 +170,65 @@ export type {
 export { Viewport } from '@/components/viewport/Viewport.js';
 
 /**
- * CSS.
+ * CSS Vars.
  */
-export { bindMiniAppCSSVars } from '@/components/mini-app/bindMiniAppCSSVars.js';
-export { bindThemeParamsCSSVars } from '@/components/theme-params/bindThemeParamsCSSVars.js';
-export { bindViewportCSSVars } from '@/components/viewport/bindViewportCSSVars.js';
 export { setCSSVar } from '@/css-vars/setCSSVar.js';
+
+/**
+ * Debug.
+ */
+export { setDebug } from '@/debug/debug.js';
+
+/**
+ * Env.
+ */
+export { isIframe } from '@/env/isIframe.js';
+export { isTMA } from '@/env/isTMA.js';
+
+/**
+ * Errors.
+ */
+export * from '@/errors/errors.js';
+export { isSDKError } from '@/errors/isSDKError.js';
+export { isSDKErrorOfType } from '@/errors/isSDKErrorOfType.js';
+export { SDKError } from '@/errors/SDKError.js';
+
+/**
+ * Events.
+ */
+export { EventEmitter } from '@/events/event-emitter/EventEmitter.js';
+export * from '@/events/event-emitter/types.js';
+export * from '@/events/types.js';
 
 /**
  * Init.
  */
-export { init } from '@/init/init.js';
-export type { InitOptions, InitResult } from '@/init/types.js';
+export { init, type InitOptions } from '@/init/init.js';
 
 /**
  * Launch params.
  */
 export { parseLaunchParams } from '@/launch-params/parseLaunchParams.js';
-export { launchParams } from '@/launch-params/parsers/launchParams.js';
-export { retrieveLaunchData } from '@/launch-params/retrieveLaunchData.js';
 export { retrieveLaunchParams } from '@/launch-params/retrieveLaunchParams.js';
 export { serializeLaunchParams } from '@/launch-params/serializeLaunchParams.js';
-export type { LaunchParams } from '@/launch-params/types.js';
-export type { LaunchData } from '@/launch-params/types.js';
-
-/**
- * Misc.
- */
-export { setDebug } from '@/debug/debug.js';
-export { isIframe } from '@/env/isIframe.js';
-export { isTMA } from '@/env/isTMA.js';
-export { setTargetOrigin } from '@/misc/globals.js';
-export { isRecord } from '@/misc/isRecord.js';
-export { isPageReload } from '@/navigation/isPageReload.js';
+export * from '@/launch-params/types.js';
 
 /**
  * Navigation.
  */
-export { getHash } from '@/navigation/getHash.js';
-export { HashNavigator } from '@/navigation/HashNavigator/HashNavigator.js';
-export type {
-  HashNavigatorEventListener,
-  HashNavigatorEventName,
-  HashNavigatorEventsMap,
-  HashNavigatorOptions,
-} from '@/navigation/HashNavigator/types.js';
-export { Navigator } from '@/navigation/Navigator/Navigator.js';
-export type {
-  NavigationEntry,
-  NavigatorConEntry,
-  NavigatorOptions,
-} from '@/navigation/Navigator/types.js';
+export { BasicNavigator } from '@/navigation/navigators/BasicNavigator/BasicNavigator.js';
+export * from '@/navigation/navigators/BasicNavigator/types.js';
+export { BrowserNavigator } from '@/navigation/navigators/BrowserNavigator/BrowserNavigator.js';
+export * from '@/navigation/navigators/BrowserNavigator/types.js';
+export {
+  createBrowserNavigatorFromLocation,
+} from '@/navigation/navigators/BrowserNavigator/utils/createBrowserNavigatorFromLocation.js';
+export { getHash } from '@/navigation/utils/getHash.js';
+export { isPageReload } from '@/navigation/utils/isPageReload.js';
 
 /**
  * Parsing.
  */
-export { ParseError } from '@/parsing/ParseError.js';
 export { array } from '@/parsing/parsers/array.js';
 export { boolean } from '@/parsing/parsers/boolean.js';
 export { date } from '@/parsing/parsers/date.js';
@@ -214,7 +237,11 @@ export { number } from '@/parsing/parsers/number.js';
 export { rgb } from '@/parsing/parsers/rgb.js';
 export { searchParams } from '@/parsing/parsers/searchParams.js';
 export { string } from '@/parsing/parsers/string.js';
-export { ParseSchemaFieldError } from '@/parsing/ParseSchemaFieldError.js';
+
+/**
+ * Request ID
+ */
+export type { RequestId } from '@/request-id/types.js';
 
 /**
  * Supports.
@@ -224,14 +251,13 @@ export { supports } from '@/supports/supports.js';
 /**
  * Timeout.
  */
-export { isTimeoutError } from '@/timeout/isTimeoutError.js';
-export { TimeoutError } from '@/timeout/TimeoutError.js';
 export { withTimeout } from '@/timeout/withTimeout.js';
 
 /**
  * Types.
  */
-export type { CreateRequestIdFn, RequestId } from '@/request-id/types.js';
+export * from '@/types/methods.js';
+export * from '@/types/platform.js';
 
 /**
  * Version.
