01f429952a2df588beef8fb0dcf0fd4b22d90164
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-04-28 22:53:21 +0300

    chore(eslint): disable no-param-reassign

--- packages/eslint-config-custom/rules.js
+++ packages/eslint-config-custom/rules.js
@@ -89,6 +89,7 @@ module.exports = {
   'no-console': 0,
   'no-continue': 0,
   'no-nested-ternary': 0,
+  'no-param-reassign': 0,
   'no-return-assign': 0,
   'no-underscore-dangle': 0,
   // Sometimes we need to write "void promise".
