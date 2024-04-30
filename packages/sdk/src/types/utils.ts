b8da76238e5669f36737cff33255b332151827ab
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-04-30 23:59:38 +0300

    feat(types): add Maybe type

--- packages/sdk/src/types/utils.ts
+++ packages/sdk/src/types/utils.ts
@@ -22,3 +22,8 @@ export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
  * Marks specified properties as required.
  */
 export type RequiredBy<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+
+/**
+ * Appends `null` and `undefined`.
+ */
+export type Maybe<T> = T | null | undefined;
