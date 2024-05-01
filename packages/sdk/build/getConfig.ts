40ac3455b1e085eb7716f08edaa7b59a84e38602
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-01 17:52:18 +0300

    build(config): remove coverage excludes. Reduce coverage requirements

--- packages/sdk/build/getConfig.ts
+++ packages/sdk/build/getConfig.ts
@@ -3,11 +3,6 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
-import { createVitestConfig } from 'build-utils';
-
-function prefix(name: string): string {
-  return `../src/${name}`;
-}
 
 interface Options {
   input: string;
@@ -19,7 +14,8 @@ interface Options {
 export function getConfig({ filename, input, formats, declarations }: Options): UserConfigFn {
   return defineConfig((config) => {
     const dir = dirname(fileURLToPath(import.meta.url));
-    const tsconfigPath = config.mode === 'test'
+    const test = config.mode === 'test';
+    const tsconfigPath = test
       ? resolve(dir, '../tsconfig.json')
       : resolve(dir, '../tsconfig.build.json');
 
@@ -54,46 +50,19 @@ export function getConfig({ filename, input, formats, declarations }: Options): 
           },
         },
       },
-      test: createVitestConfig({
+      test: {
         environment: 'happy-dom',
         coverage: {
-          // TODO: As long as currently we are not covering all code in the project, we exclude some
-          //  of the folders or files from the coverage. To be done in the future.
-          exclude: [
-            prefix('**/index.ts'),
-            prefix('**/__tests__'),
-            prefix('bridge/methods/invokeCustomMethod.ts'),
-            prefix('components/BackButton'),
-            prefix('components/ClosingBehavior/ClosingBehavior.ts'),
-            prefix('components/InitData/InitData.ts'),
-            prefix('components/Popup/preparePopupParams.ts'),
-            prefix('components/main-button/MainButton.ts'),
-            prefix('components/mini-app/MiniApp.ts'),
-            prefix('components/theme-params/ThemeParams.ts'),
-            prefix('components/theme-params/requestThemeParams.ts'),
-            prefix('components/viewport/Viewport.ts'),
-            prefix('components/viewport/requestViewport.ts'),
-            prefix('init/creators/createRequestIdGenerator.ts'),
-            prefix('launch-params/launchParamsParser.ts'),
-            prefix('launch-params/parseLaunchParams.ts'),
-            prefix('launch-params/retrieveFromLocation.ts'),
-            prefix('launch-params/retrieveFromPerformance.ts'),
-            prefix('launch-params/retrieveLaunchData.ts'),
-            prefix('launch-params/retrieveLaunchParams.ts'),
-            prefix('launch-params/serializeLaunchParams.ts'),
-            prefix('launch-params/storage.ts'),
-            prefix('state'),
-            prefix('supports/createSupportsParamFunc.ts'),
-            prefix('misc/isTMA.ts'),
-            prefix('navigation/**'),
-            prefix('timeout/sleep.ts'),
-          ],
-          branches: 100,
-          functions: 100,
-          statements: 100,
-          lines: 100,
+          enabled: true,
+          provider: 'v8',
+          include: ['src/**/*.ts'],
+          exclude: ['src/**/*.test.ts'],
+          branches: 80,
+          functions: 80,
+          statements: 80,
+          lines: 80,
         },
-      }),
+      },
     };
   });
 }
