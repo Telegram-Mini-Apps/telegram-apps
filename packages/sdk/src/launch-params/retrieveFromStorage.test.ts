94a966465a02aeae6bf512a356ddab3b36d2e6cf
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-01 14:02:05 +0300

    tests(launch-params): retrieve* functions tests

---
+++ packages/sdk/src/launch-params/retrieveFromStorage.test.ts
@@ -0,0 +1,28 @@
+import { expect, it, vi, afterEach } from 'vitest';
+
+import { retrieveFromStorage } from './retrieveFromStorage.js';
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+it('should return launch parameters from the session storage tma.js/launch-params key. If data is missing or invalid, throw an error', () => {
+  const spy = vi
+    .spyOn(sessionStorage, 'getItem')
+    .mockImplementationOnce(() => '');
+  expect(() => retrieveFromStorage()).toThrow();
+
+  spy.mockClear();
+  spy.mockImplementationOnce(() => {
+    return '"tgWebAppPlatform=android&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%7D&tgWebAppVersion=7.0"';
+  });
+  expect(retrieveFromStorage()).toStrictEqual({
+    version: '7.0',
+    platform: 'android',
+    themeParams: {
+      bgColor: '#ffffff',
+    },
+  });
+  expect(spy).toHaveBeenCalledOnce();
+  expect(spy).toHaveBeenCalledWith('tma.js/launch-params');
+});
