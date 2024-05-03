49cdbfb8b9e71300774a24b146808e562cea6af7
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-03 21:50:08 +0300

    feat(solid-router-integration): actualize the update

--- packages/solid-router-integration/vite.config.ts
+++ packages/solid-router-integration/vite.config.ts
@@ -1,9 +1,27 @@
-import { createViteConfig } from 'build-utils';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-import packageJson from './package.json';
+// import packageJson from './package.json';
 
-export default createViteConfig({
-  packageName: packageJson.name,
-  formats: ['es', 'cjs'],
-  external: ['@tma.js/navigation', '@solidjs/router'],
-});
+// export default createViteConfig({
+//   packageName: packageJson.name,
+//   formats: ['es', 'cjs'],
+//   external: ['@tma.js/navigation', '@solidjs/router'],
+// });
+
+export default defineConfig({
+  plugins: [
+    dts({ outDir: 'dist/dts' }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: { external: ['@solidjs/router', '@tma.js/sdk-solid'] },
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+  },
+})
