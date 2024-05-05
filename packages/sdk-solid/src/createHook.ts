cd29ba03abcd5ee75febfe0b12b4ea1589338d36
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-05 16:46:49 +0300

    feat(sdk-solid): better errors handling

--- packages/sdk-solid/src/createHook.ts
+++ packages/sdk-solid/src/createHook.ts
@@ -7,12 +7,24 @@ interface AnyFn {
   (...args: any): any;
 }
 
+/**
+ * Hook execution result. Represents weak implementation of Solid Resource.
+ */
+export interface HookResource<Factory extends AnyFn> {
+  /**
+   * Returns factory execution result.
+   * @throws An error, if it occurred during factory call. Use the "error" property before
+   * to check if something went wrong.
+   */
+  (): ReturnType<Factory> extends PromiseLike<infer R> ? R | undefined : ReturnType<Factory>;
+  /**
+   * Error occurred during factory call.
+   */
+  error?: any;
+}
+
 export interface Hook<Factory extends AnyFn> {
-  (...args: Parameters<Factory>): Accessor<
-    ReturnType<Factory> extends PromiseLike<infer R>
-      ? R | undefined
-      : ReturnType<Factory>
-  >;
+  (...args: Parameters<Factory>): HookResource<Factory>;
 }
 
 /**
@@ -23,27 +35,46 @@ export interface Hook<Factory extends AnyFn> {
  */
 export function createHook<Fn extends AnyFn>(fn: Fn): Hook<Fn> {
   return (...args) => {
-    const sdk = useSDK();
+    const cache = useSDK();
 
     // SDK context already has this value.
-    if (sdk.has(fn)) {
-      return sdk.get(fn)!;
+    if (cache.has(fn)) {
+      return cache.get(fn)!;
     }
 
     // We are creating a resource as long as factory can sometimes return promise. For example,
     // this may happen in case, factory initializes BiometryManager or Viewport.
-    const [resource] = createResource(() => fn(...args as any));
+    const [resource] = createResource(() => {
+      try {
+        return fn(...args as any);
+      } catch (e) {
+        // Solid can't normally handle errors in a sync way. We need an error to be handled in
+        // the first frame and this was the only 1 way of doing it I found.
+        return [e];
+      }
+    });
 
-    // Create signal, which will be used externally.
-    const getResult = createMemo(() => {
+    // Signal, which returns computed resource value.
+    const getResource = createMemo(() => {
+      // Something went wrong, return an error.
+      if (resource.error) {
+        return [resource.error];
+      }
+
+      // The resource is loading, return nothing. This usually happens when the factory is async.
       if (resource.state !== 'ready') {
         return;
       }
 
-      // Factory may return undefined if it creates the InitData component, for example.
-      // It also may not allow tracking its changes. It means, it cannot be reactive, we just
-      // this values as is.
+      // Get a value, stored in the resource and check if it ended up with an error.
       const value = resource();
+      if (Array.isArray(value)) {
+        return value;
+      }
+
+      // Factory may return undefined if it creates the InitData component, for example.
+      // The returned component may also not allow tracking its changes. It means, it cannot
+      // be reactive, we just this values as is.
       if (!value || !('on' in value)) {
         return value;
       }
@@ -60,27 +91,44 @@ export function createHook<Fn extends AnyFn>(fn: Fn): Hook<Fn> {
       const prototype = Object.getPrototypeOf(value);
 
       // Cache with already defined signals.
-      const cache: Record<string | symbol, Accessor<any>> = {};
+      const propCache: Record<string | symbol, Accessor<any>> = {};
 
       return new Proxy(value, {
         get(target: any, property: string | symbol): any {
-          if (!(property in cache)) {
+          if (!(property in propCache)) {
             const descriptor = Reflect.getOwnPropertyDescriptor(prototype, property);
 
             // We receive descriptor, describing this property and check if it has getter. In
             // case it does, we must make it reactive. Otherwise, we return real value behind
             // this property.
-            cache[property] = descriptor && ('get' in descriptor)
+            propCache[property] = descriptor && ('get' in descriptor)
               ? createMemo(() => (get() as any)[property])
               : () => Reflect.get(target, property);
           }
-          return cache[property]();
+          return propCache[property]();
         },
       });
     });
 
-    sdk.set(fn, getResult);
+    // Define accessor.
+    function read() {
+      const v = getResource();
+      if (Array.isArray(v)) {
+        throw v[0];
+      }
+      return v;
+    }
 
-    return getResult;
+    // Define accessor "error" property.
+    Object.defineProperty(read, 'error', {
+      get: () => {
+        const v = getResource();
+        return Array.isArray(v) ? v[0] : undefined;
+      },
+    });
+
+    cache.set(fn, read);
+
+    return read;
   };
 }
