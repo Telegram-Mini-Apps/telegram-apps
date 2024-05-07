3898924451fe66b7a2fed4925b411a7fc2981bd1
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-07 16:52:10 +0300

    feat(solid-router-integration): actualize package

--- packages/solid-router-integration/src/createNavigator.ts
+++ packages/solid-router-integration/src/createNavigator.ts
@@ -1,13 +1,12 @@
-import {
-  BrowserNavigator,
+import { BrowserNavigator,
   type BrowserNavigatorHashMode,
   createBrowserNavigatorFromLocation,
-  isPageReload,
-} from '@tma.js/sdk-solid';
+  isPageReload  } from '@tma.js/sdk-solid';
+import type {BrowserNavigatorConOptions } from '@tma.js/sdk-solid';
 
 function instantiate<State>(
   sessionStorageKey: string,
-  hashMode?: BrowserNavigatorHashMode,
+  options?: BrowserNavigatorConOptions,
 ): BrowserNavigator<State> {
   // If page was reloaded, we assume that navigator had to previously save
   // its state in the session storage.
@@ -16,7 +15,7 @@ function instantiate<State>(
     if (stateRaw) {
       try {
         const { cursor, entries } = JSON.parse(stateRaw);
-        return new BrowserNavigator<State>(entries, cursor, hashMode);
+        return new BrowserNavigator(entries, cursor, options);
       } catch (e) {
         console.error('Unable to restore hash navigator state.', e);
       }
@@ -25,21 +24,22 @@ function instantiate<State>(
 
   // In case, we could not restore its state, or it is the fresh start, we
   // can create an empty navigator.
+  // We are creating BrowserNavigator from the window.location.
   return window.location.hash.includes('?')
-    ? createBrowserNavigatorFromLocation<State>(hashMode)
-    : new BrowserNavigator(['/'], 0, hashMode);
+    ? createBrowserNavigatorFromLocation(options)
+    : new BrowserNavigator(['/'], 0, options);
 }
 
 /**
  * Creates Mini Apps navigator.
- * @param hashMode - navigator hash mode. Omit, if non-hash navigation is required.
  * @param sessionStorageKey - session storage key, containing this navigator state.
+ * @param options - additional BrowserNavigator options.
  */
 export function createNavigator<State>(
   sessionStorageKey: string,
-  hashMode?: BrowserNavigatorHashMode,
+  options?: BrowserNavigatorConOptions,
 ): BrowserNavigator<State> {
-  const navigator = instantiate<State>(sessionStorageKey, hashMode);
+  const navigator = instantiate<State>(sessionStorageKey, options);
 
   const saveState = () => sessionStorage.setItem(sessionStorageKey, JSON.stringify({
     cursor: navigator.cursor,
