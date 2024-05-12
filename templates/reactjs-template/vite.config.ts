c072d4e423cc06f260c41840ed2b1d6a6d0717b9
qbnk <wolfram.deus@gmail.com>
2024-05-12 20:36:58 +0500

    chore(repo): move templates to a separate folder

--- templates/reactjs-template/vite.config.ts
+++ templates/reactjs-template/vite.config.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { readFileSync } from 'node:fs';
+// import { dirname, resolve } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
