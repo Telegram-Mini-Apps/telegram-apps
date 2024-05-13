b425cf40d9b21f5d918fd01bc7f6738aad6716b0
qbnk <wolfram.deus@gmail.com>
2024-05-14 00:38:29 +0500

    fix(sdk): fix incorrect isTMA behavior in native apps

--- packages/sdk/src/env/isTMA.ts
+++ packages/sdk/src/env/isTMA.ts
@@ -1,9 +1,13 @@
 import { request } from '@/bridge/utils/request.js';
+import { hasWebviewProxy } from '@/env/hasWebviewProxy.js';
 
 /**
  * Returns true in case, current environment is Telegram Mini Apps.
  */
 export async function isTMA(): Promise<boolean> {
+  if (hasWebviewProxy(window)) {
+    return true;
+  }
   try {
     await request({ method: 'web_app_request_theme', event: 'theme_changed', timeout: 100 });
     return true;
