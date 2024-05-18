86a838cf79b6938522bcb9d9371793d1930b68b1
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-18 15:39:04 +0300

    refactor(sdk-solid): rework according to changes done in the sdk

--- packages/sdk-solid/src/SDKProvider/SDKProvider.types.ts
+++ packages/sdk-solid/src/SDKProvider/SDKProvider.types.ts
@@ -1,4 +1,4 @@
-import type { AnyFn } from '@tma.js/sdk';
+import type { AnyFn, CleanupFn } from '@tma.js/sdk';
 import type { ParentProps } from 'solid-js';
 
 export interface SDKProviderProps extends ParentProps {
@@ -15,25 +15,48 @@ export interface SDKProviderProps extends ParentProps {
   debug?: boolean;
 }
 
-export interface SDKContextItem<Factory extends AnyFn> {
+type ExtractResult<T> = T extends [result: infer R, cleanup: CleanupFn]
+  ? ExtractResult<R>
+  : T extends Promise<infer U>
+    ? U | undefined
+    : T;
+
+interface FactorySignalFn<Factory extends AnyFn> {
   /**
    * Returns factory execution result.
    * @throws An error, if it occurred during factory call. Use the "error" property before
    * to check if something went wrong.
    */
-  (): ReturnType<Factory> extends PromiseLike<infer T> ? T | undefined : ReturnType<Factory>;
+  (): ExtractResult<ReturnType<Factory>>;
+}
+
+export interface FactorySignal<Factory extends AnyFn> extends FactorySignalFn<Factory> {
   /**
    * Error occurred during factory call.
    */
   error?: unknown;
 }
 
+export interface SDKContextItem<Factory extends AnyFn> {
+  /**
+   * Function, to cleanup this item.
+   */
+  cleanup?: CleanupFn;
+  /**
+   * Error occurred during factory call.
+   */
+  error?: unknown;
+  /**
+   * Signal to retrieve the result.
+   */
+  signal: FactorySignalFn<Factory>;
+}
+
 export interface SDKContextType {
   /**
    * Uses specified factory with the passed arguments. In case, this factory was called
-   * previously, a cached result will be returned.
+   * previously, its previous result will be returned.
    * @param factory - factory function.
    * @param args - factory arguments.
-   */
-  <Fn extends AnyFn>(factory: Fn, ...args: Parameters<Fn>): SDKContextItem<Fn>;
+   */<Fn extends AnyFn>(factory: Fn, ...args: Parameters<Fn>): SDKContextItem<Fn>;
 }
--- packages/sdk-solid/src/createHook.ts
+++ packages/sdk-solid/src/createHook.ts
@@ -1,18 +1,28 @@
 import { AnyFn } from '@tma.js/sdk';
 
 import { useSDK } from './SDKProvider/SDKContext.js';
-import { SDKContextItem } from './SDKProvider/SDKProvider.types.js';
+import type { FactorySignal } from './SDKProvider/SDKProvider.types.js';
 
 export interface Hook<Factory extends AnyFn> {
-  (...args: Parameters<Factory>): SDKContextItem<Factory>;
+  (...args: Parameters<Factory>): FactorySignal<Factory>;
 }
 
 /**
  * Creates new hook, which firstly attempts to extract a value from the SDK context. In case,
  * value is missing, it uses the passed component factory, creates a component, subscribes
  * to its changes if required, and returns its reactive copy.
- * @param fn
+ * @param factory - component factory.
  */
-export function createHook<Fn extends AnyFn>(fn: Fn): Hook<Fn> {
-  return (...args) => useSDK()(fn, ...args);
+export function createHook<Factory extends AnyFn>(factory: Factory): Hook<Factory> {
+  return (...args) => {
+    const contextItem = useSDK()(factory, ...args);
+    const signal: FactorySignal<Factory> = () => {
+      return contextItem.signal();
+    };
+    Object.defineProperty(signal, 'error', () => {
+      return contextItem.error;
+    });
+
+    return signal;
+  };
 }
