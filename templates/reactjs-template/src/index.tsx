8c146fd2e94f2d432e10ddccae234528d115d269
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-18 20:09:09 +0300

    chore(reactjs-template): get rid of the strict mode

--- templates/reactjs-template/src/index.tsx
+++ templates/reactjs-template/src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Root } from '@/components/Root';
 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
