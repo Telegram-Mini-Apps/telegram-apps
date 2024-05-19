08c8f76b5f99f2457c02b511fbb2dda17890c0c9
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-19 15:48:51 +0300

    refactor(docs): refactor config creation

--- apps/docs/.vitepress/packages.ts
+++ apps/docs/.vitepress/packages.ts
@@ -1,153 +1,69 @@
-function prefixed(value: string): string {
-  return `/packages${value}`;
-}
+import { sectionGen } from './utils';
+
+const PREFIX = '/packages';
+
+const section = sectionGen(PREFIX);
 
 export const packagesNavItem = {
   text: 'Packages',
-  link: prefixed('/tma-js-sdk'),
+  link: `${PREFIX}/tma-js-sdk`,
 };
 
 export const packagesSidebar = {
-  [prefixed('/')]: [
-    {
-      text: 'CLI',
-      items: [
-        { text: '@tma.js/create-mini-app', link: prefixed('/tma-js-create-mini-app') },
-      ]
-    },
-
-    {
-      text: 'TypeScript',
-      items: [
+  [PREFIX]: [
+    section('CLI', {
+      '@tma.js/create-mini-app': 'tma-js-create-mini-app',
+    }),
+    section('TypeScript', {
+      '@tma.js/sdk': [
+        'tma-js-sdk',
         {
-          text: '@tma.js/sdk',
-          link: prefixed('/tma-js-sdk'),
-          items: [
+          'Initialization': 'init',
+          'Methods and events': 'methods-and-events',
+          'Launch parameters': 'launch-parameters',
+          'Theme parameters': 'theme-parameters',
+          'Init data': [
+            'init-data',
             {
-              text: 'Initialization',
-              link: prefixed('/tma-js-sdk/init'),
+              'InitData': 'init-data',
+              'Chat': 'chat',
+              'User': 'user',
             },
+            true,
+          ],
+          'Navigation': 'navigation',
+          'Components': [
+            'components',
             {
-              text: 'Methods and events',
-              link: prefixed('/tma-js-sdk/methods-and-events'),
+              'BackButton': 'back-button',
+              'ClosingBehavior': 'closing-behavior',
+              'CloudStorage': 'cloud-storage',
+              'HapticFeedback': 'haptic-feedback',
+              'InitData': 'init-data',
+              'Invoice': 'invoice',
+              'MainButton': 'main-button',
+              'MiniApp': 'mini-app',
+              'Popup': 'popup',
+              'QRScanner': 'qr-scanner',
+              'SettingsButton': 'settings-button',
+              'ThemeParams': 'theme-params',
+              'Utils': 'utils',
+              'Viewport': 'viewport',
             },
-            {
-              text: 'Launch parameters',
-              link: prefixed('/tma-js-sdk/launch-parameters'),
-            },
-            {
-              text: 'Theme parameters',
-              link: prefixed('/tma-js-sdk/theme-parameters'),
-            },
-            {
-              text: 'Init data',
-              link: prefixed('/tma-js-sdk/init-data'),
-              collapsed: true,
-              items: [
-                {
-                  text: 'InitData',
-                  link: prefixed('/tma-js-sdk/init-data/init-data'),
-                },
-                {
-                  text: 'Chat',
-                  link: prefixed('/tma-js-sdk/init-data/chat'),
-                },
-                {
-                  text: 'User',
-                  link: prefixed('/tma-js-sdk/init-data/user'),
-                },
-              ],
-            },
-            {
-              text: 'Navigation',
-              link: prefixed('/tma-js-sdk/navigation'),
-            },
-            {
-              text: 'Components',
-              collapsed: true,
-              link: prefixed('/tma-js-sdk/components'),
-              items: [
-                {
-                  text: 'BackButton',
-                  link: prefixed('/tma-js-sdk/components/back-button'),
-                },
-                {
-                  text: 'ClosingBehavior',
-                  link: prefixed('/tma-js-sdk/components/closing-behavior'),
-                },
-                {
-                  text: 'CloudStorage',
-                  link: prefixed('/tma-js-sdk/components/cloud-storage'),
-                },
-                {
-                  text: 'HapticFeedback',
-                  link: prefixed('/tma-js-sdk/components/haptic-feedback'),
-                },
-                {
-                  text: 'InitData',
-                  link: prefixed('/tma-js-sdk/components/init-data'),
-                },
-                {
-                  text: 'Invoice',
-                  link: prefixed('/tma-js-sdk/components/invoice'),
-                },
-                {
-                  text: 'MainButton',
-                  link: prefixed('/tma-js-sdk/components/main-button'),
-                },
-                {
-                  text: 'MiniApp',
-                  link: prefixed('/tma-js-sdk/components/mini-app'),
-                },
-                {
-                  text: 'Popup',
-                  link: prefixed('/tma-js-sdk/components/popup'),
-                },
-                {
-                  text: 'QRScanner',
-                  link: prefixed('/tma-js-sdk/components/qr-scanner'),
-                },
-                {
-                  text: 'SettingsButton',
-                  link: prefixed('/tma-js-sdk/components/settings-button'),
-                },
-                {
-                  text: 'ThemeParams',
-                  link: prefixed('/tma-js-sdk/components/theme-params'),
-                },
-                {
-                  text: 'Utils',
-                  link: prefixed('/tma-js-sdk/components/utils'),
-                },
-                {
-                  text: 'Viewport',
-                  link: prefixed('/tma-js-sdk/components/viewport'),
-                },
-              ],
-            },
+            true,
           ],
         },
-        { text: '@tma.js/sdk-react', link: prefixed('/tma-js-sdk-react') },
-        { text: '@tma.js/sdk-solid', link: prefixed('/tma-js-sdk-solid') },
-        {
-          text: '@tma.js/solid-router-integration',
-          link: prefixed('/tma-js-solid-router-integration'),
-        },
       ],
-    },
-
-    {
-      text: 'Node',
-      items: [
-        { text: '@tma.js/init-data-node', link: prefixed('/tma-js-init-data-node') },
-      ],
-    },
-
-    {
-      text: 'GoLang',
-      items: [
-        { text: 'init-data-golang', link: prefixed('/init-data-golang') },
-      ],
-    },
+      '@tma.js/sdk-react': '/tma-js-sdk-react',
+      '@tma.js/sdk-solid': '/tma-js-sdk-solid',
+      '@tma.js/solid-router-integration': '/tma-js-solid-router-integration',
+      '@tma.js/react-router-integration': '/tma-js-react-router-integration',
+    }),
+    section('Node', {
+      '@tma.js/init-data-node': 'tma-js-init-data-node',
+    }),
+    section('GoLang', {
+      'init-data-golang': 'init-data-golang',
+    }),
   ],
 };
