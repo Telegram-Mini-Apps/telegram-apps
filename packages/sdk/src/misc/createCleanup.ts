9beb52f69a08103061d72ff0c731c4f6f03b2ed0
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-19 23:56:20 +0300

    feat(sdk): improve createCleanup passing the initial array

--- packages/sdk/src/misc/createCleanup.ts
+++ packages/sdk/src/misc/createCleanup.ts
@@ -4,13 +4,13 @@ import { CleanupFn } from '@/types/index.js';
  * Returns a tuple, containing function to add cleanup, call cleanup, and flag showing whether
  * cleanup was called. Cleanup will not be performed in case, it was done before.
  */
-export function createCleanup(): [
+export function createCleanup(...fns: CleanupFn[]): [
   add: (fn: CleanupFn) => void,
   call: () => void,
   cleanedUp: boolean,
 ] {
   let called = false;
-  const cache: CleanupFn[] = [];
+  const cache: CleanupFn[] = [...fns];
 
   return [
     (fn) => !called && cache.push(fn),
