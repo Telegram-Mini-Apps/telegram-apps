0eaea89079acb72a9e04b75ba3b027797b17faed
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-10 23:12:38 +0300

    refactor(init-data-node): refactor a bit hmac.ts file and rename it

---
+++ packages/init-data-node/src/createHmac.test.ts
@@ -0,0 +1,9 @@
+import { expect, it } from 'vitest';
+
+import { createHmac } from './createHmac';
+
+it('should use HMAC-SHA256 algorithm with key, based on HMAC-SHA256 keyed with the "WebAppData" value, applied to the secret token', async () => {
+  expect(
+    Buffer.from(await createHmac('data123', 'my-secret-key')).toString('hex'),
+  ).toBe('d834d96e53d71451bdc56691d0f01c3f0132fa2d404eb7618593ea0d13252642');
+});
