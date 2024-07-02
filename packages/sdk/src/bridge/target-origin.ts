6068440f65d55339e8b3a840b7711ba973975a05
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-07-03 01:29:23 +0300

    chore(sdk): add resetTargetOrigin function

--- packages/sdk/src/bridge/target-origin.ts
+++ packages/sdk/src/bridge/target-origin.ts
@@ -1,15 +1,23 @@
 let currentTargetOrigin = 'https://web.telegram.org';
 
 /**
- * Sets new global targetOrigin, used by `postEvent` method.
- * Default value is "https://web.telegram.org". You don't need to
- * use this method until you know what you are doing.
+ * Sets a new global targetOrigin, used by the `postEvent` method.
+ * The default value is "https://web.telegram.org".
+ * You don't need to use this method until you know what you are doing.
  *
  * This method could be used for test purposes.
  * @param value - new target origin.
+ * @see postEvent
  */
 export function setTargetOrigin(value: string): void {
   currentTargetOrigin = value;
+}
+
+/**
+ * Sets the initial target origin.
+ */
+export function resetTargetOrigin(): void {
+  setTargetOrigin('https://web.telegram.org');
 }
 
 /**
