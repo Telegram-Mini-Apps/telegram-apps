da25e37dbc3846978d4bcd90dd8766698ed4f832
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-07-29 01:44:38 +0500

    feat(sdk): implement cloud storage

---
+++ packages/sdk/src/cloud-storage/cloudStorage.ts
@@ -0,0 +1,117 @@
+import { invokeCustomMethod } from '@/bridge/invokeCustomMethod.js';
+import { array } from '@/parsing/parsers/array.js';
+import { json } from '@/parsing/parsers/json.js';
+import { string } from '@/parsing/parsers/string.js';
+import {
+  decorateWithSupports,
+  type WithSupports,
+} from '@/components/utilities/decorateWithSupports.js';
+import { createRequestId, postEvent } from '@/components/globals.js';
+import type { ExecuteWithTimeout } from '@/types/methods.js';
+
+const MINI_APPS_METHOD = 'web_app_invoke_custom_method';
+
+// TODO: Usage.
+
+/**
+ * @see API: https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/components/cloud-storage
+ */
+
+/**
+ * Deletes specified key or keys from the cloud storage.
+ * @param keyOrKeys - key or keys to delete.
+ * @param options - request execution options.
+ */
+export const deleteKeys = decorateWithSupports(
+  async (keyOrKeys: string | string[], options: ExecuteWithTimeout = {}): Promise<void> => {
+    const keys = Array.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys];
+    if (keys.length) {
+      await invokeCustomMethod(
+        'deleteStorageValues',
+        { keys },
+        createRequestId()(),
+        { ...options, postEvent: postEvent() },
+      );
+    }
+  },
+  MINI_APPS_METHOD,
+);
+
+/**
+ * Returns list of all keys presented in the cloud storage.
+ * @param options - request execution options.
+ */
+export const getKeys = decorateWithSupports(
+  async (options: ExecuteWithTimeout = {}): Promise<string[]> => {
+    return array().of(string()).parse(
+      await invokeCustomMethod(
+        'getStorageKeys',
+        {},
+        createRequestId()(),
+        { ...options, postEvent: postEvent() },
+      ),
+    );
+  },
+  MINI_APPS_METHOD,
+);
+
+type GetFn = WithSupports<{
+  /**
+   * @param keys - keys list.
+   * @param options - request execution options.
+   * @returns Map, where a key is one of the specified in the `keys` argument, and a value is
+   * a corresponding storage value.
+   */<K extends string>(keys: K[], options?: ExecuteWithTimeout): Promise<Record<K, string>>;
+  /**
+   * @param key - cloud storage key.
+   * @param options - request execution options.
+   * @return Value of the specified key. If the key was not created previously, the function
+   * will return an empty string.
+   */
+  (key: string, options?: ExecuteWithTimeout): Promise<string>;
+}>;
+
+export const get: GetFn = decorateWithSupports(
+  async (
+    keyOrKeys: string | string[],
+    options?: ExecuteWithTimeout,
+  ): Promise<string | Record<string, string>> => {
+    const keys = Array.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys];
+    if (!keys.length) {
+      return typeof keyOrKeys === 'string' ? '' : {};
+    }
+
+    const data = await invokeCustomMethod(
+      'getStorageValues',
+      { keys },
+      createRequestId()(),
+      { ...options || {}, postEvent: postEvent() },
+    );
+
+    const result = json(
+      Object.fromEntries(keys.map((k) => [k, string()])),
+      'CloudStorageData',
+    ).parse(data);
+
+    return Array.isArray(keyOrKeys) ? result : result[keyOrKeys];
+  },
+  MINI_APPS_METHOD,
+) as GetFn;
+
+/**
+ * Saves specified value by key.
+ * @param key - storage key.
+ * @param value - storage value.
+ * @param options - request execution options.
+ */
+export const set = decorateWithSupports(
+  async (key: string, value: string, options: ExecuteWithTimeout = {}): Promise<void> => {
+    await invokeCustomMethod(
+      'saveStorageValue',
+      { key, value },
+      createRequestId()(),
+      { ...options, postEvent: postEvent() },
+    );
+  },
+  MINI_APPS_METHOD,
+);
