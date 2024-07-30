addb295e1d6b69ae7e67e0a6a9f69f3bd38ab01a
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-07-31 01:55:12 +0500

    refactor(sdk): refactor theme params

---
+++ packages/sdk/src/theme-params/themeParams.computed.ts
@@ -0,0 +1,73 @@
+import { computed, Computed } from '@/signals/computed/computed.js';
+import { isColorDark } from '@/colors/isColorDark.js';
+
+import { state as _state, isMounted as _isMounted } from './themeParams.private.js';
+import type { ThemeParams } from './types.js';
+
+function createStateComputed<K extends keyof ThemeParams>(
+  key: K,
+): Computed<ThemeParams[K] | undefined> {
+  return computed(() => _state()[key]);
+}
+
+export const state = computed(_state);
+
+/**
+ * @since v6.10
+ */
+export const accentTextColor = createStateComputed('accentTextColor');
+
+export const backgroundColor = createStateComputed('bgColor');
+
+export const buttonColor = createStateComputed('buttonColor');
+
+export const buttonTextColor = createStateComputed('buttonTextColor');
+
+export const destructiveTextColor = createStateComputed('destructiveTextColor');
+
+/**
+ * @since v6.10
+ */
+export const headerBackgroundColor = createStateComputed('headerBgColor');
+
+export const hintColor = createStateComputed('hintColor');
+
+/**
+ * @returns True if the current color scheme is recognized as dark.
+ * This value is calculated based on the current theme's background color.
+ */
+export const isDark = computed(() => {
+  const { bgColor } = state();
+  return !bgColor || isColorDark(bgColor);
+});
+
+/**
+ * True if the component is currently mounted.
+ */
+export const isMounted = computed(_isMounted);
+
+export const linkColor = createStateComputed('linkColor');
+
+export const secondaryBackgroundColor = createStateComputed('secondaryBgColor');
+
+/**
+ * @since v6.10
+ */
+export const sectionBackgroundColor = createStateComputed('sectionBgColor');
+
+/**
+ * @since v6.10
+ */
+export const sectionHeaderTextColor = createStateComputed('sectionHeaderTextColor');
+
+/**
+ * @since v7.6
+ */
+export const sectionSeparatorColor = createStateComputed('sectionSeparatorColor');
+
+/**
+ * @since v6.10
+ */
+export const subtitleTextColor = createStateComputed('subtitleTextColor');
+
+export const textColor = createStateComputed('textColor');
--- packages/sdk/src/theme-params/themeParams.ts
+++ packages/sdk/src/theme-params/themeParams.ts
@@ -1,80 +1,28 @@
 import { on } from '@/bridge/events/listening/on.js';
 import { off } from '@/bridge/events/listening/off.js';
-import { isColorDark } from '@/colors/isColorDark.js';
 import { retrieveLaunchParams } from '@/launch-params/retrieveLaunchParams.js';
 import { isPageReload } from '@/navigation/isPageReload.js';
 import { getStorageValue, setStorageValue } from '@/storage/storage.js';
-import { signal } from '@/signals/signal/signal.js';
-import { computed, type Computed } from '@/signals/computed/computed.js';
 import type { MiniAppsEventListener } from '@/bridge/events/types.js';
 
 import { parse } from './ThemeParams.static.js';
-import type { ThemeParams } from './types.js';
+import { state as _state, isMounted as _isMounted } from './themeParams.private.js';
 
-/**
+/*
+ * fixme
  * @see Usage: https://docs.telegram-mini-apps.com/platform/theming
  * @see API: https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/components/theme-params
  */
-
-
-function createStateComputed<K extends keyof ThemeParams>(
-  key: K,
-): Computed<ThemeParams[K] | undefined> {
-  return computed(() => state()[key]);
-}
-
-export const state = signal<ThemeParams>({}, {
-  set(s, value) {
-    setStorageValue('themeParams', value);
-    s.set(value);
-  },
-});
-
-/**
- * @since v6.10
- */
-export const accentTextColor = createStateComputed('accentTextColor')
-
-export const backgroundColor = createStateComputed('bgColor')
-
-export const buttonColor = createStateComputed('buttonColor')
-
-export const buttonTextColor = createStateComputed('buttonTextColor')
-
-export const destructiveTextColor = createStateComputed('destructiveTextColor');
-
-/**
- * @since v6.10
- */
-export const headerBackgroundColor = createStateComputed('headerBgColor')
-
-export const hintColor = createStateComputed('hintColor');
-
-/**
- * @returns True if the current color scheme is recognized as dark.
- * This value is calculated based on the current theme's background color.
- */
-export const isDark = computed(() => {
-  const { bgColor } = state();
-  return !bgColor || isColorDark(bgColor);
-});
-
-/**
- * Boolean signal, containing `true` if the component is currently mounted.
- */
-export const isMounted = signal(false);
-
-export const linkColor = createStateComputed('linkColor');
 
 /**
  * Mounts the component restoring its state and adding special event listeners, actualizing
  * the component state.
  */
-export function mount(): void {
-  if (!isMounted()) {
-    state.set(isPageReload() && getStorageValue('themeParams') || retrieveLaunchParams().themeParams);
+function mount(): void {
+  if (!_isMounted()) {
+    _state.set(isPageReload() && getStorageValue('themeParams') || retrieveLaunchParams().themeParams);
     on('theme_changed', onThemeChanged);
-    isMounted.set(true);
+    _isMounted.set(true);
   }
 }
 
@@ -83,39 +31,20 @@ export function mount(): void {
  * @param e - event data.
  */
 const onThemeChanged: MiniAppsEventListener<'theme_changed'> = (e) => {
-  state.set(parse(e.theme_params));
+  const value = parse(e.theme_params);
+  _state.set(value);
+  setStorageValue('themeParams', value);
 };
-
-export const secondaryBackgroundColor = createStateComputed('secondaryBgColor');
-
-/**
- * @since v6.10
- */
-export const sectionBackgroundColor = createStateComputed('sectionBgColor');
-
-/**
- * @since v6.10
- */
-export const sectionHeaderTextColor = createStateComputed('sectionHeaderTextColor')
-
-/**
- * @since v7.6
- */
-export const sectionSeparatorColor = createStateComputed('sectionSeparatorColor');
-
-/**
- * @since v6.10
- */
-export const subtitleTextColor = createStateComputed('subtitleTextColor');
-
-export const textColor = createStateComputed('textColor');
 
 /**
  * Unmounts the component removing all bound event listeners.
  */
-export function unmount(): void {
-  if (isMounted()) {
+function unmount(): void {
+  if (_isMounted()) {
     off('theme_changed', onThemeChanged);
-    isMounted.set(false);
+    _isMounted.set(false);
   }
 }
+
+export { mount, unmount };
+export * from './themeParams.computed.js';
