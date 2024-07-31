dff162f97cde3452a2e611658ad407c80f29b933
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-01 03:26:28 +0500

    refactor(sdk): remove unused types

--- packages/sdk/src/types/logical.ts
+++ packages/sdk/src/types/logical.ts
@@ -1,21 +1,12 @@
 /**
- * Represents classic "if" condition.
+ * Represents a classic "if" condition.
  */
 export type If<Cond extends boolean, True, False> = Cond extends true ? True : False;
 
 /**
- * Represents classic "not" logical operation.
+ * Represents a classic "not" logical operation.
  */
 export type Not<Cond extends boolean> = If<Cond, false, true>;
-
-/**
- * Represents classic "and" logical operation.
- */
-export type And<A extends boolean, B extends boolean> = A extends true
-  ? B extends true
-    ? true
-    : false
-  : false;
 
 /**
  * Represents classic "or" logical operation.
--- packages/sdk/src/types/utils.ts
+++ packages/sdk/src/types/utils.ts
@@ -9,21 +9,6 @@ export type IsNever<T> = [T] extends [never] ? true : false;
 export type IsUndefined<T> = [T] extends [undefined] ? true : false;
 
 /**
- * Returns object string keys.
- */
-export type StringKeys<T extends object> = Extract<keyof T, string>;
-
-/**
- * Marks specified properties as optional.
- */
-export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-
-/**
- * Marks specified properties as required.
- */
-export type RequiredBy<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
-
-/**
  * Appends `null` and `undefined`.
  */
 export type Maybe<T> = T | null | undefined;
