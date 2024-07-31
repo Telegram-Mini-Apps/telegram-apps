206c0a1ad10019dc357e119b942c056a1eb62c34
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-01 03:26:42 +0500

    refactor(sdk): refactor request-id

--- packages/sdk/src/request-id/createRequestIdGenerator.ts
+++ packages/sdk/src/request-id/createRequestIdGenerator.ts
@@ -1,9 +1,7 @@
-import type { CreateRequestIdFn } from './types.js';
-
 /**
  * Creates function which generated request identifiers.
  */
-export function createRequestIdGenerator(): CreateRequestIdFn {
+export function createRequestIdGenerator(): () => string {
   let requestId = 0;
   return () => (requestId += 1).toString();
 }
