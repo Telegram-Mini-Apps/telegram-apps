7e9cbba1d8a71c7bda41eb69509ac1985b41fea9
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-01 03:28:22 +0500

    feat(sdk): extend createCleanup add method

--- packages/sdk/src/misc/createCleanup.ts
+++ packages/sdk/src/misc/createCleanup.ts
@@ -1,11 +1,11 @@
-import { CleanupFn } from '@/types/index.js';
+import type { CleanupFn } from '@/types/index.js';
 
 /**
  * Returns a tuple, containing function to add cleanup, call cleanup, and flag showing whether
  * cleanup was called. Cleanup will not be performed in case, it was done before.
  */
 export function createCleanup(...fns: (CleanupFn | CleanupFn[])[]): [
-  add: (fn: CleanupFn) => void,
+  add: (...fns: CleanupFn[]) => void,
   cleanup: () => void,
   cleanedUp: boolean,
 ] {
@@ -13,7 +13,7 @@ export function createCleanup(...fns: (CleanupFn | CleanupFn[])[]): [
   const cache = fns.flat(1);
 
   return [
-    (fn) => !cleanedUp && cache.push(fn),
+    (...fns) => !cleanedUp && cache.push(...fns),
     () => {
       if (!cleanedUp) {
         cleanedUp = true;
