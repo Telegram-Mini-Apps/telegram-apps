03f53e7bc2262d888c33628c6b018954994cf063
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-01 03:29:46 +0500

    feat(sdk): allow preventing updates in signal

--- packages/sdk/src/signals/signal/signal.ts
+++ packages/sdk/src/signals/signal/signal.ts
@@ -1,8 +1,16 @@
 import { collectSignal } from '../reactive-context.js';
 import type { ListenerFn, UnsubscribeFn } from '../types.js';
 
-export interface SignalOverrides<T> {
-  set?(this: void, signal: Signal<T>, value: T): void;
+export interface SignalOptions<T> {
+  /**
+   * Previous and next values comparator. Should return true if values are the same.
+   *
+   * By default, signals compares values directly using "===".
+   * @param a - current value.
+   * @param b - incoming value.
+   * @returns True if values are considered the same.
+   */
+  equals?(this: void, a: T, b: T): boolean;
 }
 
 export interface Signal<T> {
@@ -14,18 +22,18 @@ export interface Signal<T> {
    * Updates the signal notifying all subscribers about changes.
    * @param value - value to set.
    */
-  set(value: T): void;
+  set(this: void, value: T): void;
   /**
    * Resets the signal value to its initial value.
    */
-  reset(): void;
+  reset(this: void): void;
   /**
    * Adds a new listener, tracking the signal changes.
    * @param fn - event listener.
    * @param options - additional options.
    * @returns A function to remove bound listener.
    */
-  sub(fn: ListenerFn<T>, options?: {
+  sub(this: void, fn: ListenerFn<T>, options?: {
     /**
      * True if the listener was added by some other signal.
      */
@@ -41,16 +49,17 @@ export interface Signal<T> {
    * @param fn - event listener.
    * @param once - was this listener added for a single call. Default: false
    */
-  unsub(fn: ListenerFn<T>, once?: boolean): void;
+  unsub(this: void, fn: ListenerFn<T>, once?: boolean): void;
   /**
    * Removes all signal change listeners, not added by other signals.
    */
-  unsubAll(): void;
+  unsubAll(this: void): void;
 }
 
-// @__NO_SIDE_EFFECTS__
-export function signal<T>(initialValue: T, overrides?: SignalOverrides<T>): Signal<T> {
-  overrides ||= {};
+/*@__NO_SIDE_EFFECTS__*/
+export function signal<T>(initialValue: T, options?: SignalOptions<T>): Signal<T> {
+  options ||= {};
+  const equals = options.equals || ((a, b) => a === b);
 
   let listeners: [
     /**
@@ -68,49 +77,52 @@ export function signal<T>(initialValue: T, overrides?: SignalOverrides<T>): Sign
   ][] = [];
   let value: T = initialValue;
 
+  const set: Signal<T>['set'] = v => {
+    if (!equals(value, v)) {
+      value = v;
+
+      // We are making a copy of listeners as long as they may mutate the listeners' array,
+      // leading to an unexpected behavior.
+      //
+      // We want the setter to make sure that all listeners will be called in predefined
+      // order withing a single update frame.
+      [...listeners].forEach(([fn, once]) => {
+        fn(v);
+
+        // Remove "once" listeners.
+        if (once) {
+          unsub(fn, true);
+        }
+      });
+    }
+  };
+
+  const unsub: Signal<T>['unsub'] = (fn, once) => {
+    const idx = listeners.findIndex(item => {
+      return item[0] === fn && item[1] === !!once;
+    });
+    if (idx >= 0) {
+      listeners.splice(idx, 1);
+    }
+  };
+
   const s = Object.assign(
     function get(): T {
       collectSignal(s);
       return value;
     },
     {
-      set(v) {
-        if (value !== v) {
-          value = v;
-
-          // We are making a copy of listeners as long as they may mutate the listeners' array,
-          // leading to an unexpected behavior.
-          //
-          // We want the setter to make sure that all listeners will be called in predefined
-          // order withing a single update frame.
-          [...listeners].forEach(([fn, once]) => {
-            fn(v);
-
-            // Remove "once" listeners.
-            if (once) {
-              this.unsub(fn, true);
-            }
-          });
-        }
-      },
+      set,
       reset() {
-        this.set(initialValue);
+        set(initialValue);
       },
       sub(fn, options) {
         options ||= {};
         const { once } = options;
         listeners.push([fn, !!once, options.signal]);
-        return () => this.unsub(fn, once);
+        return () => unsub(fn, once);
       },
-      unsub(fn, once): void {
-        once ||= false;
-        const idx = listeners.findIndex(item => {
-          return item[0] === fn && item[1] === once;
-        });
-        if (idx >= 0) {
-          listeners.splice(idx, 1);
-        }
-      },
+      unsub,
       unsubAll() {
         // We are removing only non-signal listeners. This is due to we don't want this method
         // to break computed signals tracking of other signals.
@@ -118,21 +130,6 @@ export function signal<T>(initialValue: T, overrides?: SignalOverrides<T>): Sign
       },
     } satisfies Pick<Signal<T>, 'set' | 'reset' | 'sub' | 'unsub' | 'unsubAll'>,
   );
-
-  const { set: overridesSet } = overrides;
-  if (overridesSet) {
-    return Object.assign(
-      function overridden() {
-        return s();
-      },
-      s,
-      {
-        set(v) {
-          overridesSet(s, v);
-        },
-      } satisfies Pick<Signal<T>, 'set'>,
-    );
-  }
 
   return s;
 }
