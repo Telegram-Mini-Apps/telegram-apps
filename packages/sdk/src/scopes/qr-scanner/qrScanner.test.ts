ae112dd8c58417788f503d7f02becba189b27990
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-07-31 12:03:13 +0500

    feat(sdk): rework QR Scanner

---
+++ packages/sdk/src/scopes/qr-scanner/qrScanner.test.ts
@@ -0,0 +1,148 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { resetGlobals } from '@test-utils/resetGlobals.js';
+import { mockPostEvent } from '@test-utils/mockPostEvent.js';
+import { dispatchWindowMessageEvent } from '@test-utils/dispatchWindowMessageEvent.js';
+
+import { postEvent } from '@/scopes/globals/globals.js';
+
+import * as _ from './qrScanner.private.js';
+import {
+  isOpened,
+  close,
+  open,
+} from './qrScanner.js';
+
+beforeEach(() => {
+  resetGlobals();
+  _.isOpened.reset();
+  _.isOpened.unsubAll();
+  vi.restoreAllMocks();
+  postEvent.set(() => null);
+});
+
+describe('close', () => {
+  it('should set isOpened = false', () => {
+    _.isOpened.set(true);
+    expect(isOpened()).toBe(true);
+    close();
+    expect(isOpened()).toBe(false);
+  });
+
+  it('should call postEvent with "web_app_close_scan_qr_popup"', () => {
+    const spy = vi.fn(() => null);
+    postEvent.set(spy);
+    expect(spy).toHaveBeenCalledTimes(0);
+    close();
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith('web_app_close_scan_qr_popup');
+  });
+});
+
+describe('open', () => {
+  describe('common mode', () => {
+    it('should set isOpened = true', () => {
+      expect(isOpened()).toBe(false);
+      void open({
+        capture() {
+          return true;
+        },
+      });
+      expect(isOpened()).toBe(true);
+    });
+
+    it('should set isOpened = false if QR was scanned', async () => {
+      expect(isOpened()).toBe(false);
+      const promise = open({
+        capture() {
+          return true;
+        },
+      });
+      expect(isOpened()).toBe(true);
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      await promise;
+      expect(isOpened()).toBe(false);
+    });
+
+    it('should call postEvent with "web_app_open_scan_qr_popup" with { text: string } and catch "qr_text_received" event returning event "data" property if "capture" returned true', async () => {
+      const spy = mockPostEvent();
+      const promise = open({
+        text: 'TEXT',
+        capture(qr) {
+          return qr === 'QR2';
+        },
+      });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR2' });
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('web_app_open_scan_qr_popup', { text: 'TEXT' });
+      await expect(promise).resolves.toBe('QR2');
+    });
+
+    it('should call "web_app_close_scan_qr_popup" if QR was scanned', async () => {
+      const spy = mockPostEvent();
+      const promise = open({
+        capture() {
+          return true;
+        },
+      });
+      spy.mockClear();
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      await promise;
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('web_app_close_scan_qr_popup');
+    });
+
+    it('should return promise with null, if "scan_qr_popup_closed" event was received', async () => {
+      const promise = open({
+        capture() {
+          return true;
+        },
+      });
+      dispatchWindowMessageEvent('scan_qr_popup_closed');
+      await expect(promise).resolves.toBeNull();
+    });
+  });
+
+  describe('stream mode', () => {
+    it('should set isOpened = true', () => {
+      expect(isOpened()).toBe(false);
+      open({
+        onCaptured() {
+        },
+      });
+      expect(isOpened()).toBe(true);
+    });
+
+    it('should call onCaptured with QR content each time, "qr_text_received" event was received', () => {
+      const spy = vi.fn();
+      open({ onCaptured: spy });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR2' });
+      expect(spy).toHaveBeenCalledTimes(2);
+      expect(spy).toHaveBeenNthCalledWith(1, 'QR1');
+      expect(spy).toHaveBeenNthCalledWith(2, 'QR2');
+    });
+
+    it('should call postEvent with "web_app_open_scan_qr_popup" with { text: string }', () => {
+      const spy = mockPostEvent();
+      open({
+        text: 'TEXT',
+        onCaptured: vi.fn(),
+      });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('web_app_open_scan_qr_popup', { text: 'TEXT' });
+    });
+
+    it('should not call onCaptured if QR scanner was closed', () => {
+      const spy = vi.fn();
+      open({ onCaptured: spy });
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR1' });
+      close();
+      dispatchWindowMessageEvent('qr_text_received', { data: 'QR2' });
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('QR1');
+    });
+  });
+});
---
+++ packages/sdk/src/scopes/qr-scanner/qrScanner.ts
@@ -0,0 +1,120 @@
+import { computed } from '@/signals/computed/computed.js';
+import { decorateWithIsSupported, type WithIsSupported } from '@/scopes/decorateWithIsSupported.js';
+import { request } from '@/bridge/request.js';
+import { createError } from '@/errors/createError.js';
+import { postEvent } from '@/scopes/globals/globals.js';
+import { on } from '@/bridge/events/listening/on.js';
+import { createCleanup } from '@/misc/createCleanup.js';
+import { ERR_SCANNER_OPENED } from '@/errors/errors.js';
+
+import * as _ from './qrScanner.private.js';
+
+// TODO: Links?
+
+const CLOSE_METHOD = 'web_app_close_scan_qr_popup';
+const OPEN_METHOD = 'web_app_open_scan_qr_popup';
+const CLOSED_EVENT = 'scan_qr_popup_closed';
+const SCANNED_EVENT = 'qr_text_received';
+
+/**
+ * True if the scanner is currently opened.
+ */
+export const isOpened = computed(_.isOpened);
+
+/**
+ * Closes the scanner.
+ */
+export const close: WithIsSupported<() => void> = decorateWithIsSupported(() => {
+  _.isOpened.set(false);
+  postEvent()(CLOSE_METHOD);
+}, CLOSE_METHOD);
+
+type OpenFn = WithIsSupported<{
+  /**
+   * Opens the scanner.
+   *
+   * Whenever a user scans a QR, the passed `capture` function is being called with the QR
+   * content. It should return true if this QR must be captured and promise resolved.
+   * @param options - method options.
+   * @returns A captured QR content or null, if the scanner was closed.
+   */
+  (options: {
+    /**
+     * Title to be displayed in the scanner.
+     */
+    text?: string;
+    /**
+     * Function, which should return true if a scanned QR should be captured and promise resolved.
+     * @param qr - scanned QR content.
+     */
+    capture(this: void, qr: string): boolean;
+  }): Promise<string | null>;
+  /**
+   * Opens the scanner in stream mode.
+   *
+   * Whenever a user scans a QR, the passed `onCaptured` function will be called.
+   * @param options - method options.
+   */
+  (options: {
+    /**
+     * Title to be displayed in the scanner.
+     */
+    text?: string;
+    /**
+     * Function which will be called in case, some QR code was scanned.
+     * @param qr - scanned QR content.
+     */
+    onCaptured(this: void, qr: string): void;
+  }): void;
+}>;
+
+export const open: OpenFn = decorateWithIsSupported((options) => {
+  if (_.isOpened()) {
+    throw createError(ERR_SCANNER_OPENED);
+  }
+  _.isOpened.set(true);
+
+  const { text } = options;
+
+  // Capture single QR mode.
+  if ('capture' in options) {
+    const { capture } = options;
+
+    return request({
+      method: OPEN_METHOD,
+      event: [SCANNED_EVENT, CLOSED_EVENT],
+      postEvent: postEvent(),
+      params: { text },
+      capture(ev) {
+        return ev.event === 'scan_qr_popup_closed' || capture(ev.payload.data);
+      },
+    })
+      .then((result) => {
+        const qr = result ? result.data : null;
+        if (qr !== null) {
+          // Some valid QR was scanned. After this, we can close the scanner.
+          close();
+        }
+        return qr;
+      })
+      .finally(() => {
+        _.isOpened.set(false);
+      });
+  }
+
+  // Stream mode.
+  const [, cleanup] = createCleanup(
+    on('qr_text_received', ({ data }) => {
+      options.onCaptured(data);
+    }),
+    on('scan_qr_popup_closed', () => {
+      _.isOpened.set(false);
+    }),
+  );
+
+  // When the scanner was closed, remember to perform a cleanup, removing all bound event
+  // listeners.
+  _.isOpened.sub(cleanup, { once: true });
+
+  postEvent()(OPEN_METHOD, { text: options.text });
+}, OPEN_METHOD) as OpenFn;
