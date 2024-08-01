4af49c1287aa6046ae7ab827bb3fd3b01b5e3613
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-02 03:05:17 +0500

    refactor(types): move mini apps related types to "mini apps" folder

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,9 +1,9 @@
+export type * from './mini-apps/init-data.js';
+export type * from './mini-apps/launch-params.js';
+export type * from './mini-apps/common.js';
+export type * from './mini-apps/theme-params.js';
 export type * from './colors.js';
-export type * from './init-data.js';
-export type * from './launch-params.js';
 export type * from './logical.js';
-export type * from './mini-apps.js';
 export type * from './misc.js';
-export type * from './theme-params.js';
 export type * from './unions.js';
 export type * from './utils.js';
