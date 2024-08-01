ad769d0753df4c0dbbb6cbb6e9727585d18ac625
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-02 03:18:51 +0500

    feat(bridge): make debug a signal

--- packages/bridge/src/debug.ts
+++ packages/bridge/src/debug.ts
@@ -1,3 +1,5 @@
+import { signal } from '@telegram-apps/signals';
+
 import { subscribe, unsubscribe } from '@/events/listening.js';
 import { createLogger } from '@/utils/createLogger.js';
 import type { SubscribeListener } from '@/events/types.js';
@@ -7,30 +9,29 @@ export const [log, error] = createLogger('Bridge', {
   textColor: 'white',
 });
 
-let debugEnabled = false;
+/**
+ * The package debug mode.
+ *
+ * Enabling debug mode leads to printing additional messages in the console related to the
+ * processes inside the package.
+ */
+export const debug = signal(false, {
+  set(s, v) {
+    if (s() !== v) {
+      v ? subscribe(onEvent) : unsubscribe(onEvent);
+      s.set(v);
+    }
+  },
+});
 
 const onEvent: SubscribeListener = ({ name, payload }) => {
   log('Event received:', payload ? { name, payload } : { name });
 };
 
 /**
- * Sets new debug mode. Enabling debug mode leads to printing additional messages in the console,
- * related to the processes inside the package.
- * @param enable - should debug be enabled.
- */
-export function setDebug(enable: boolean): void {
-  if (debugEnabled !== enable) {
-    debugEnabled = enable;
-    enable ? subscribe(onEvent) : unsubscribe(onEvent);
-  }
-}
-
-/**
  * Logs info message into the console.
  * @param args - additional arguments.
  */
 export function debugLog(...args: any[]): void {
-  if (debugEnabled) {
-    log(...args);
-  }
+  debug() && log(...args);
 }
