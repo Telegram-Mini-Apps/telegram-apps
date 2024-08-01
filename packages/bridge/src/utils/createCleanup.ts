59158dee5ea0ff1a8eae79c778c92536af013f43
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-02 02:28:06 +0500

    refactor(bridge): remove CleanupFn

--- packages/bridge/src/utils/createCleanup.ts
+++ packages/bridge/src/utils/createCleanup.ts
@@ -1,17 +1,21 @@
-/**
- * Function which performs a cleanup.
- */
-export interface CleanupFn {
-  (): void;
-}
+import type { VoidFn } from '@telegram-apps/types';
 
 /**
  * Returns a tuple, containing function to add cleanup, call cleanup, and flag showing whether
  * cleanup was called. Cleanup will not be performed in case, it was done before.
  */
-export function createCleanup(...fns: (CleanupFn | CleanupFn[])[]): [
-  add: (...fns: CleanupFn[]) => void,
+export function createCleanup(...fns: (VoidFn | VoidFn[])[]): [
+  /**
+   * Adds new functions to be called on cleanup.
+   */
+  add: (...fns: VoidFn[]) => void,
+  /**
+   * Performs cleanup.
+   */
   cleanup: () => void,
+  /**
+   * True if cleanup was already performed.
+   */
   cleanedUp: boolean,
 ] {
   let cleanedUp = false;
