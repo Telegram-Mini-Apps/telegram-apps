8f9706261c7650ba859660270a0649f280d72215
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-06 02:47:34 +0500

    refactor(signals): move signals collection to computed

--- packages/signals/src/computed.ts
+++ packages/signals/src/computed.ts
@@ -1,11 +1,46 @@
 import { signal, type Signal, type SignalOptions } from './signal.js';
-import { collectSignals } from './reactive-context.js';
 
 export interface Computed<T> extends Omit<Signal<T>, 'set' | 'reset'> {
   /**
    * @returns An underlying signal value.
    */
   (): T;
+}
+
+/**
+ * Signals collected during the last call of the `collect` method.
+ * @private
+ */
+let collectedSignals: Set<Signal<unknown>> | undefined;
+
+/**
+ * Runs specified function in a reactive context, collecting called signals.
+ * @param fn - function to call.
+ * @returns A tuple, containing a result of the function and collected signals.
+ */
+function collectSignals<T>(fn: () => T): [
+  /**
+   * Function execution result.
+   */
+  result: T,
+  /**
+   * Collected signals.
+   */
+  signals: Set<Signal<unknown>>,
+] {
+  collectedSignals = new Set();
+
+  try {
+    // Call the function and start tracking for all captured reactive units.
+    return [fn(), collectedSignals];
+  } finally {
+    // Remember to untrack the reactive context.
+    collectedSignals = undefined;
+  }
+}
+
+export function collectSignal(signal: Signal<any>): void {
+  collectedSignals && collectedSignals.add(signal);
 }
 
 /*@__NO_SIDE_EFFECTS__*/
