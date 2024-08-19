4eaf6a8494e94d280e9c0e79267aa0f15656db07
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-19 23:05:01 +0500

    feat(packages): implement utils package

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,11 @@
+export { addEventListener } from './addEventListener.js';
+export { createCleanup, type CleanupFn } from './createCleanup.js';
+export {
+  advancedPromise,
+  type PromiseResolveFn,
+  type PromiseRejectFn,
+  type AdvancedPromise,
+  type AdvancedPromiseOptions,
+  type AdvancedPromiseExecutor,
+} from './advancedPromise.js';
+export { camelToKebab, camelToSnake } from './casing.js';
