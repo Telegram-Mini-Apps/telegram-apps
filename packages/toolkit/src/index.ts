d2c5e90767720410714a4d6adf5990af4100c4e8
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-23 01:16:58 +0500

    chore(toolkit): actualize exports

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -1,3 +1,12 @@
+export {
+  AdvancedPromise,
+  type AdvancedPromiseBasicOptions,
+  type AdvancedPromiseCompleteOptions,
+  type PromiseResolveFn,
+  type PromiseRejectFn,
+  type PromiseExecutor,
+} from './async/AdvancedPromise.js';
+export { ERR_TIMED_OUT, withTimeout, isTimeoutError, createTimeoutError } from './async/timeout.js';
 export { TypedError, type TypedErrorOptions } from './errors/TypedError.js';
 export { createTypedError } from './errors/createTypedError.js';
 export { EventEmitter } from './event-emitter/EventEmitter.js';
@@ -17,16 +26,7 @@ export type * from './types/predicates.js';
 export { addEventListener } from './addEventListener.js';
 export { createCbCollector, type CallbackFn } from './createCbCollector.js';
 export { createCleanup, type CleanupFn } from './createCleanup.js';
-export {
-  advancedPromise,
-  type PromiseResolveFn,
-  type PromiseRejectFn,
-  type AdvancedPromise,
-  type AdvancedPromiseOptions,
-  type AdvancedPromiseExecutor,
-} from './advancedPromise.js';
 export { camelToKebab, camelToSnake, snakeToCamel } from './casing.js';
 export { createLogger } from './createLogger.js';
 export { createSingleton } from './createSingleton.js';
-export { sleep } from './sleep.js';
-export { ERR_TIMED_OUT, withTimeout, isTimeoutError } from './timeout.js';
+export { sleep } from './async/sleep.js';
