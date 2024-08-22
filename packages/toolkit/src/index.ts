b4a2481b6d1d2db5f23f13efb195c86f3922ddb8
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-22 22:19:44 +0500

    feat(toolkit): add more exports

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -1,3 +1,5 @@
+export { TypedError, type TypedErrorOptions } from './errors/TypedError.js';
+export { createTypedError } from './errors/createTypedError.js';
 export { EventEmitter } from './event-emitter/EventEmitter.js';
 export type {
   EmptyEventName,
@@ -8,6 +10,10 @@ export type {
   EventListener,
   SubscribeListener,
 } from './event-emitter/types.js';
+export { getStorageValue, setStorageValue } from './storage/storage.js';
+export type * from './types/logical.js';
+export type * from './types/misc.js';
+export type * from './types/predicates.js';
 export { addEventListener } from './addEventListener.js';
 export { createCbCollector, type CallbackFn } from './createCbCollector.js';
 export { createCleanup, type CleanupFn } from './createCleanup.js';
@@ -19,7 +25,8 @@ export {
   type AdvancedPromiseOptions,
   type AdvancedPromiseExecutor,
 } from './advancedPromise.js';
-export { camelToKebab, camelToSnake } from './casing.js';
-export type * from './types/logical.js';
-export type * from './types/misc.js';
-export type * from './types/predicates.js';
+export { camelToKebab, camelToSnake, snakeToCamel } from './casing.js';
+export { createLogger } from './createLogger.js';
+export { createSingleton } from './createSingleton.js';
+export { sleep } from './sleep.js';
+export { ERR_TIMED_OUT, withTimeout, isTimeoutError } from './timeout.js';
