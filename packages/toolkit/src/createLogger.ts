85fecbf5b4de5442600964c2279d55990a20eb95
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-22 21:26:03 +0500

    refactor(toolkit): refactor createLogger a bit

--- packages/toolkit/src/createLogger.ts
+++ packages/toolkit/src/createLogger.ts
@@ -30,20 +30,19 @@ export function createLogger(scope: string, options?: LoggerOptions): [
    * @param args - arguments.
    */
   function print(level: LogLevel, ...args: any[]): void {
-    const now = new Date();
-    const date = Intl
-      .DateTimeFormat('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        fractionalSecondDigits: 3,
-        timeZone: 'UTC',
-      })
-      .format(now);
-
-    const commonCss = 'font-weight: bold;padding: 0 5px;border-radius:5px';
+    const commonCss = 'font-weight:bold;padding:0 5px;border-radius:5px';
     console[level](
-      `%c${date}%c / %c${scope}`,
+      `%c${
+        Intl
+          .DateTimeFormat('en-GB', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            fractionalSecondDigits: 3,
+            timeZone: 'UTC',
+          })
+          .format(new Date())
+      }%c / %c${scope}`,
       `${commonCss};background-color: lightblue;color:black`,
       '',
       `${commonCss};${textColor ? `color:${textColor};` : ''}${bgColor ? `background-color:${bgColor}` : ''}`,
