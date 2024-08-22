c4b931c01632c78218caa4bb69ac5f2ebc6ca7e2
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-22 23:20:22 +0500

    feat(bridge): support modes in createPostEvent

--- packages/bridge/src/methods/createPostEvent.ts
+++ packages/bridge/src/methods/createPostEvent.ts
@@ -1,8 +1,11 @@
+import { isRecord } from '@telegram-apps/transformers';
 import type { Version } from '@telegram-apps/types';
 
-import { createError } from '@/errors/createError.js';
-import { ERR_METHOD_PARAMETER_UNSUPPORTED, ERR_METHOD_UNSUPPORTED } from '@/errors/errors.js';
-import { isRecord } from '@/utils/isRecord.js';
+import {
+  ERR_METHOD_PARAMETER_UNSUPPORTED,
+  ERR_METHOD_UNSUPPORTED,
+  ErrorType,
+} from '@/errors/errors.js';
 import { supports } from '@/methods/supports.js';
 import { type PostEvent, postEvent } from '@/methods/postEvent.js';
 import type {
@@ -10,6 +13,7 @@ import type {
   MethodNameWithVersionedParams,
   MethodVersionedParams,
 } from '@/methods/types/index.js';
+import { BridgeError } from '@/errors/BridgeError.js';
 
 export type OnUnsupportedFn = (
   data: { version: Version } & (
@@ -22,36 +26,51 @@ export type OnUnsupportedFn = (
   }[MethodNameWithVersionedParams]),
 ) => void;
 
+export type CreatePostEventMode = 'strict' | 'non-strict';
+
 /**
  * Creates a function which checks if specified method and parameters are supported.
  *
  * If method or parameters are unsupported, the `onUnsupported` function will be called.
  *
- * By default, the `onUnsupported` function will throw a `BridgeError` error with
- * `ERR_METHOD_UNSUPPORTED` or `ERR_METHOD_PARAMETER_UNSUPPORTED` type.
+ * If `strict` or `non-strict` value was passed as the second argument, the function
+ * will create its own `onUnsupported` function with behavior depending on the value passed.
+ *
+ * - Passing `strict` will make function to throw a `BridgeError` error
+ * with `ERR_METHOD_UNSUPPORTED` or `ERR_METHOD_PARAMETER_UNSUPPORTED` type.
+ * - Passing `non-strict` will just warn you about something being unsupported.
+ *
  * @param version - Telegram Mini Apps version.
- * @param onUnsupported - function which will be called, if a method or parameter is unsupported.
+ * @param onUnsupportedOrMode - function or strict mode. Default: `strict`
  * @see BridgeError
  * @see ERR_METHOD_UNSUPPORTED
  * @see ERR_METHOD_PARAMETER_UNSUPPORTED
  */
 export function createPostEvent(
   version: Version,
-  onUnsupported?: OnUnsupportedFn,
+  onUnsupportedOrMode?: OnUnsupportedFn | CreatePostEventMode,
 ): PostEvent {
-  onUnsupported ||= (data) => {
-    const { method, version } = data;
-    if ('param' in data) {
-      throw createError(
-        ERR_METHOD_PARAMETER_UNSUPPORTED,
-        `Parameter "${data.param}" of "${method}" method is unsupported in Mini Apps version ${version}`,
-      );
-    }
-    throw createError(
-      ERR_METHOD_UNSUPPORTED,
-      `Method "${method}" is unsupported in Mini Apps version ${version}`,
-    );
-  };
+  onUnsupportedOrMode ||= 'strict';
+  const onUnsupported: OnUnsupportedFn = typeof onUnsupportedOrMode === 'function'
+    ? onUnsupportedOrMode
+    : data => {
+      const { method, version } = data;
+      let message: string;
+      let error: ErrorType;
+
+      if ('param' in data) {
+        message = `Parameter "${data.param}" of "${method}" method is unsupported in Mini Apps version ${version}`;
+        error = ERR_METHOD_PARAMETER_UNSUPPORTED;
+      } else {
+        message = `Method "${method}" is unsupported in Mini Apps version ${version}`;
+        error = ERR_METHOD_UNSUPPORTED;
+      }
+
+      if (onUnsupportedOrMode === 'strict') {
+        throw new BridgeError(error, message);
+      }
+      return console.warn(message);
+    };
 
   return (method: any, params: any) => {
     // Firstly, check if a method is supported.
@@ -59,8 +78,7 @@ export function createPostEvent(
       return onUnsupported({ version, method });
     }
 
-    // Method could use parameters, which are supported only in specific
-    // versions of Mini Apps.
+    // Method could use parameters, which are supported only in specific versions of Mini Apps.
     // We are validating only those parameters, which are not backward compatible.
     if (
       isRecord(params)
