5896cc13eb1906f5c2fabed7dcf2d19a0ba385ac
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-25 20:50:31 +0500

    refactor(toolkit): refactor exports

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -6,7 +6,7 @@ export {
   type PromiseRejectFn,
   type PromiseExecutor,
   type PromiseOnRejectedFn,
-  type PromiseOnFulfilledFn
+  type PromiseOnFulfilledFn,
 } from './async/AdvancedPromise.js';
 export {
   createTimeoutError,
@@ -16,9 +16,12 @@ export {
   ERR_TIMED_OUT,
   ERR_ABORTED,
 } from './async/errors.js';
-export { withTimeout } from './async/withTimeout.js';
+export { sleep } from './async/sleep.js';
+export type { AsyncOptions } from './async/types.js';
+
 export { TypedError, type TypedErrorOptions } from './errors/TypedError.js';
 export { createTypedError } from './errors/createTypedError.js';
+
 export { EventEmitter } from './event-emitter/EventEmitter.js';
 export type {
   EmptyEventName,
@@ -29,14 +32,16 @@ export type {
   EventListener,
   SubscribeListener,
 } from './event-emitter/types.js';
+
 export { getStorageValue, setStorageValue } from './storage/storage.js';
+
 export type * from './types/logical.js';
 export type * from './types/misc.js';
 export type * from './types/predicates.js';
+
 export { addEventListener } from './addEventListener.js';
 export { createCbCollector, type CallbackFn } from './createCbCollector.js';
 export { createCleanup, type CleanupFn } from './createCleanup.js';
 export { camelToKebab, camelToSnake, snakeToCamel } from './casing.js';
 export { createLogger } from './createLogger.js';
 export { createSingleton } from './createSingleton.js';
-export { sleep } from './async/sleep.js';
