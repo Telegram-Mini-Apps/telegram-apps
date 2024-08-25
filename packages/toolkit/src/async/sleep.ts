53d8d08eab5a760c365237526ae431c5320e05f2
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-25 20:49:48 +0500

    feat(toolkit): allow aborting sleep

--- packages/toolkit/src/async/sleep.ts
+++ packages/toolkit/src/async/sleep.ts
@@ -1,9 +1,14 @@
+import { AdvancedPromise } from '@/async/AdvancedPromise.js';
+
 /**
  * Awaits for specified amount of time.
  * @param duration - duration in ms to await.
+ * @param abortSignal - signal to stop function execution.
  */
-export function sleep(duration: number): Promise<void> {
-  return new Promise((res) => {
+export function sleep(duration: number, abortSignal?: AbortSignal): AdvancedPromise<void> {
+  return AdvancedPromise.withOptions((res) => {
     setTimeout(res, duration);
+  }, {
+    abortSignal,
   });
 }
