c2a8dea2c4e381153eb3e49e872f360c37c9a18e
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-28 02:02:53 +0500

    feat(bridge): add more information on how isTMA works. Re-use BetterPromise

--- packages/bridge/src/env/isTMA.ts
+++ packages/bridge/src/env/isTMA.ts
@@ -1,17 +1,29 @@
+import { BetterPromise, type AsyncOptions } from '@telegram-apps/toolkit';
+
 import { request } from '@/utils/request.js';
 import { hasWebviewProxy } from '@/env/hasWebviewProxy.js';
 
 /**
- * Returns true in case, current environment is Telegram Mini Apps.
+ * Returns true if the current environment is Telegram Mini Apps.
+ *
+ * First of all, it checks if the current environment contains traits specific to the
+ * Mini Apps environment.
+ * Then, it attempts to call a Mini Apps method and waits for a response to be received.
+ *
+ * In case you have no need to use these strict checks, take a look at the `isTMASimple`
+ * alternative.
+ * @see isTMASimple
  */
-export async function isTMA(): Promise<boolean> {
-  if (hasWebviewProxy(window)) {
-    return true;
-  }
-  try {
-    await request({ method: 'web_app_request_theme', event: 'theme_changed', timeout: 100 });
-    return true;
-  } catch {
-    return false;
-  }
+export function isTMA(options?: AsyncOptions): BetterPromise<boolean> {
+  return BetterPromise.withFn(async () => {
+    if (hasWebviewProxy(window)) {
+      return true;
+    }
+    try {
+      await request('web_app_request_theme', 'theme_changed', { timeout: 100 });
+      return true;
+    } catch {
+      return false;
+    }
+  }, options);
 }
