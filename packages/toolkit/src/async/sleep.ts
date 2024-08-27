8aaf21bb0ff975a5c101de86cd8d8d52ed5932c4
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-28 01:59:10 +0500

    feat(toolkit): rename AdvancedPromise to BetterPromise. Add BetterPromise.withFn

--- packages/toolkit/src/async/sleep.ts
+++ packages/toolkit/src/async/sleep.ts
@@ -1,12 +1,12 @@
-import { AdvancedPromise } from '@/async/AdvancedPromise.js';
+import { BetterPromise } from '@/async/BetterPromise.js';
 
 /**
  * Awaits for specified amount of time.
  * @param duration - duration in ms to await.
  * @param abortSignal - signal to stop function execution.
  */
-export function sleep(duration: number, abortSignal?: AbortSignal): AdvancedPromise<void> {
-  return AdvancedPromise.withOptions((res) => {
+export function sleep(duration: number, abortSignal?: AbortSignal): BetterPromise<void> {
+  return BetterPromise.withOptions((res) => {
     setTimeout(res, duration);
   }, {
     abortSignal,
