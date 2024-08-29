387623d0f2ecfcde6d5a01ea2314676b5cacfbab
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-29 18:51:26 +0500

    feat(bridge): re-implement events listening section

---
+++ packages/bridge/src/events/listening/off.ts
@@ -0,0 +1,18 @@
+import type { SubscribeListenerFn } from '@telegram-apps/signals';
+
+import { signalFor, type SignalPayload } from '@/events/listening/signalFor.js';
+import type { EventName } from '@/events/types/events.js';
+
+/**
+ * Removes the listener from the specified event.
+ * @param event - event to listen.
+ * @param listener - event listener to remove.
+ * @param once - had this listener to be called only once.
+ */
+export function off<E extends EventName>(
+  event: E,
+  listener: SubscribeListenerFn<SignalPayload<E>>,
+  once?: boolean,
+): void {
+  signalFor(event).unsub(listener, once);
+}
