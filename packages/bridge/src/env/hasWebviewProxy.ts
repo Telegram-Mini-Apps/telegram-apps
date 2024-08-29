2db30ab781c83d7e33f698f8e2abda2768923fe8
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-29 18:40:58 +0500

    fix(bridge): switch hasWebviewProxy to transformers

--- packages/bridge/src/env/hasWebviewProxy.ts
+++ packages/bridge/src/env/hasWebviewProxy.ts
@@ -1,20 +1,20 @@
-import { isRecord } from '@telegram-apps/transformers';
+import { object, fn } from '@telegram-apps/transformers';
+
+interface WithWebviewProxy {
+  TelegramWebviewProxy: {
+    postEvent: (...args: unknown[]) => unknown;
+  };
+}
+
+const webViewProxy = object<WithWebviewProxy>({
+  TelegramWebviewProxy: object({ postEvent: fn() })(),
+});
 
 /**
  * Returns true in case, passed value contains path `TelegramWebviewProxy.postEvent` property and
  * `postEvent` is a function.
  * @param value - value to check.
  */
-export function hasWebviewProxy<T extends {}>(value: T): value is (
-  T & {
-  TelegramWebviewProxy: {
-    postEvent: (...args: unknown[]) => unknown;
-  }
-}) {
-  if (isRecord(value)) {
-    const { TelegramWebviewProxy } = value as Record<string, unknown>;
-    return isRecord(TelegramWebviewProxy)
-      && typeof TelegramWebviewProxy.postEvent === 'function';
-  }
-  return false;
+export function hasWebviewProxy<T>(value: T): value is T & WithWebviewProxy {
+  return webViewProxy().isValid(value);
 }
