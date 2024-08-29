bc17fb9dd8c9726ae3c6be51ba6a906ded34e1e5
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-29 18:41:20 +0500

    feat(bridge): allow simple check in isTMA

--- packages/bridge/src/env/isTMA.test.ts
+++ packages/bridge/src/env/isTMA.test.ts
@@ -1,5 +1,6 @@
-import { afterEach, expect, it, vi } from 'vitest';
-import { createWindow } from 'test-utils';
+import { BetterPromise } from '@telegram-apps/toolkit';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { createWindow, mockSessionStorageGetItem, mockWindow } from 'test-utils';
 
 import { request as requestFn } from '@/utils/request.js';
 import { isTMA } from './isTMA.js';
@@ -11,26 +12,53 @@ vi.mock('@/utils/request.js', () => ({
 }));
 
 afterEach(() => {
-  vi.resetAllMocks();
+  vi.restoreAllMocks();
 });
 
-it('should return true if current window contains TelegramWebviewProxy property', async () => {
-  createWindow({
-    TelegramWebviewProxy: {
-      postEvent: 123,
-    },
-  } as any);
-  await expect(isTMA()).resolves.toBe(true);
+describe('async', () => {
+  it('should return true if current window contains TelegramWebviewProxy property', async () => {
+    createWindow({
+      TelegramWebviewProxy: {
+        postEvent: 123,
+      },
+    } as any);
+    await expect(isTMA()).resolves.toBe(true);
+  });
+
+  it('should return promise with true value resolved, if requesting theme parameters was successful', async () => {
+    createWindow();
+    request.mockImplementationOnce(() => BetterPromise.resolve({}));
+    await expect(isTMA()).resolves.toBe(true);
+  });
+
+  it('should return promise with false value resolved, if requesting theme parameters was unsuccessful', async () => {
+    createWindow();
+    request.mockImplementationOnce(() => BetterPromise.reject(new Error('Timed out.')));
+    await expect(isTMA()).resolves.toBe(false);
+  });
 });
 
-it('should return promise with true value resolved, if requesting theme parameters was successful', async () => {
-  createWindow();
-  request.mockImplementationOnce(() => Promise.resolve({}));
-  await expect(isTMA()).resolves.toBe(true);
-});
+describe('sync', () => {
+  beforeEach(() => {
+    mockWindow({
+      location: {
+        href: '',
+      },
+    } as any);
+    mockSessionStorageGetItem();
+  });
 
-it('should return promise with false value resolved, if requesting theme parameters was unsuccessful', async () => {
-  createWindow();
-  request.mockImplementationOnce(() => Promise.reject(new Error('Timed out.')));
-  await expect(isTMA()).resolves.toBe(false);
+  it('should return true if env contains launch params', () => {
+    vi
+      .spyOn(window.location, 'href', 'get')
+      .mockImplementation(() => {
+        return '/abc?tgWebAppStartParam=START#tgWebAppPlatform=tdesktop&tgWebAppVersion=7.0&tgWebAppThemeParams=%7B%7D';
+      });
+
+    expect(isTMA('simple')).toBe(true);
+  });
+
+  it('should return true if env doesnt contain launch params', () => {
+    expect(isTMA('simple')).toBe(false);
+  });
 });
--- packages/bridge/src/env/isTMA.ts
+++ packages/bridge/src/env/isTMA.ts
@@ -2,19 +2,39 @@ import { BetterPromise, type AsyncOptions } from '@telegram-apps/toolkit';
 
 import { request } from '@/utils/request.js';
 import { hasWebviewProxy } from '@/env/hasWebviewProxy.js';
+import { retrieveLaunchParams } from '@/launch-params/retrieveLaunchParams.js';
 
 /**
  * Returns true if the current environment is Telegram Mini Apps.
+ *
+ * It uses the `retrieveLaunchParams` function to determine if the environment contains
+ * launch parameters. In case it does, true will be returned.
+ *
+ * In case you need stricter checks, use async override of this function.
+ */
+export function isTMA(type: 'simple'): boolean;
+
+/**
+ * Returns promise with true if the current environment is Telegram Mini Apps.
  *
  * First of all, it checks if the current environment contains traits specific to the
  * Mini Apps environment.
  * Then, it attempts to call a Mini Apps method and waits for a response to be received.
  *
- * In case you have no need to use these strict checks, take a look at the `isTMASimple`
- * alternative.
- * @see isTMASimple
+ * In case you need less strict checks, use sync override of this function.
  */
-export function isTMA(options?: AsyncOptions): BetterPromise<boolean> {
+export function isTMA(options?: AsyncOptions): BetterPromise<boolean>
+
+export function isTMA(optionsOrType?: AsyncOptions | 'simple'): boolean | BetterPromise<boolean> {
+  if (optionsOrType === 'simple') {
+    try {
+      retrieveLaunchParams();
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
   return BetterPromise.withFn(async () => {
     if (hasWebviewProxy(window)) {
       return true;
@@ -25,5 +45,5 @@ export function isTMA(options?: AsyncOptions): BetterPromise<boolean> {
     } catch {
       return false;
     }
-  }, options);
+  }, optionsOrType);
 }
