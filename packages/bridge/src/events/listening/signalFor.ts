2a09c66f1b8a49dcf45cb2a357ad1ff5b59fa0b2
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 02:35:23 +0500

    fix(bridge): fix invalid signal computation in signalFor

--- packages/bridge/src/events/listening/signalFor.ts
+++ packages/bridge/src/events/listening/signalFor.ts
@@ -24,14 +24,35 @@ export function signalFor<E extends EventName>(event: E): Computed<SignalPayload
   let cached = $eventSignalsCache()[event];
   if (!cached) {
     const lastEvent = lastEventSignal();
-    cached = computed<SignalPayload<E> | undefined>((prev) => {
+
+    // This is the special symbol we use to notify signal, that nothing changed, and the current
+    // value should be preserved.
+    const noop = Symbol();
+
+    cached = computed<SignalPayload<E> | symbol>(() => {
       const ev = lastEvent();
-      return ev && ev[0] === event
-        ? ev[1] as SignalPayload<E>
-        : prev;
+
+      // If any event was received and its name is the same as we are waiting for, return
+      // the payload.
+      //
+      // It is important to mention that some events don't have any payload (undefined, literally),
+      // so returning undefined again and again will not call the signal subscribers, until
+      // some special logic in the "equals" option is written.
+      //
+      // We could solve this problem using this option and not comparing undefined and
+      // undefined and returning true, but then we would call subscribers even when the caught
+      // event was not the target one at all.
+      //
+      // Due to this reason, we have to add some special value notifying the signal about nothing
+      // changed. Then, we use it in the "equals" check.
+      return ev && ev[0] === event ? ev[1] as SignalPayload<E> : noop;
+    }, {
+      equals(_, incoming) {
+        return incoming === noop;
+      },
 
       // Using "as Cache[E]" here we are tricking TypeScript on purpose. The problem here is
-      // that some events can't contain undefined as their payload, but the code above allows
+      // that some events can't contain "noop" value as their payload, but the code above allows
       // this value for them. In case, we would use the signal returned from this function
       // using the call signature (mySignal()), we would surely meet a bug, but as long as
       // we are using only subscription manipulation methods, this bug will not be met.
