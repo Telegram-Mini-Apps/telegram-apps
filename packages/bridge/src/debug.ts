51356ada334dffbf83a3739c33ddb1647fcecb9b
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 02:36:58 +0500

    fix(bridge): simplify debug section

--- packages/bridge/src/debug.ts
+++ packages/bridge/src/debug.ts
@@ -1,16 +1,5 @@
-import { computed, signal, type SubscribeListenerFn } from '@telegram-apps/signals';
-
+import { signal } from '@telegram-apps/signals';
 import { createLogger } from '@telegram-apps/toolkit';
-import { subscribe } from '@/events/listening/subscribe.js';
-import { unsubscribe } from '@/events/listening/unsubscribe.js';
-import type { LastEvent } from '@/events/lastEvent.js';
-
-export const [log, error] = createLogger('Bridge', {
-  bgColor: 'blue',
-  textColor: 'white',
-});
-
-export const $_debug = signal(false);
 
 /**
  * The package debug mode.
@@ -18,30 +7,10 @@ export const $_debug = signal(false);
  * Enabling debug mode leads to printing additional messages in the console related to the
  * processes inside the package.
  */
-export const $debug = computed($_debug);
+export const $debug = signal(false);
 
-const onEvent: SubscribeListenerFn<LastEvent | undefined> = (e) => {
-  if (e) {
-    const [name, payload] = e;
-    log('Event received:', payload ? { name, payload } : { name });
-  }
-};
-
-/**
- * Logs info message into the console.
- * @param args - additional arguments.
- */
-export function debugLog(...args: any[]): void {
-  $_debug() && log(...args);
-}
-
-/**
- * Sets debug mode.
- * @param v - enable debug.
- */
-export function setDebug(v: boolean): void {
-  if ($_debug() !== v) {
-    v ? subscribe(onEvent) : unsubscribe(onEvent);
-    $_debug.set(v);
-  }
-}
+export const [log, error] = createLogger('Bridge', {
+  bgColor: 'blue',
+  textColor: 'white',
+  shouldLog: $debug,
+});
