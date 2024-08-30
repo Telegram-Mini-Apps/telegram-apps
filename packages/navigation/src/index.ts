d6ae943dac95a6d06d57a02c03853714f6c36da4
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 03:10:41 +0500

    feat(navigation): actualize exports

--- packages/navigation/src/index.ts
+++ packages/navigation/src/index.ts
@@ -1,21 +1,24 @@
-export { createNavigator } from './createNavigator.js';
-export { createNavigatorFromLocation } from './createNavigatorFromLocation.js';
-export { createSafeURL } from './createSafeURL.js';
-export { dropHistory } from './dropHistory.js';
-export { ensurePrefix } from './ensurePrefix.js';
-export { ERR_HISTORY_EMPTY, ERR_CURSOR_INVALID } from './errors.js';
-export { getFirstNavigationEntry } from './getFirstNavigationEntry.js';
-export { historyGo } from './historyGo.js';
-export { initNavigator, type InitNavigatorOptions } from './initNavigator.js';
-export { isPageReload } from './isPageReload.js';
+export { dropHistory } from './history/dropHistory.js';
+export { getFirstNavigationEntry } from './history/getFirstNavigationEntry.js';
+export { historyGo } from './history/historyGo.js';
+export { isPageReload } from './history/isPageReload.js';
+
+export { createNavigator } from './navigator/createNavigator.js';
+export { createNavigatorFromLocation } from './navigator/createNavigatorFromLocation.js';
+export { initNavigator, type InitNavigatorOptions } from './navigator/initNavigator.js';
 export type {
   Navigator,
-  URLLike,
-  CtrOptions,
-  HistoryItem,
   NavigationType,
   AnyHistoryItem,
   BackButtonOptions,
   HashMode,
-} from './types.js';
-export { urlToPath } from './urlToPath.js';
+  CtrOptions,
+  HistoryItem,
+} from './navigator/types.js';
+
+export { createSafeURL } from './url/createSafeURL.js';
+export type { URLLike } from './url/types.js';
+export { urlToPath } from './url/urlToPath.js';
+
+export { ensurePrefix } from './ensurePrefix.js';
+export { ERR_HISTORY_EMPTY, ERR_CURSOR_INVALID } from './errors.js';
