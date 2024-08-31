3e0f60981d468b1f57caf28c98399bf5640dca4c
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 15:32:46 +0500

    refactor(sdk): refactor invoice scope according to new "ideology" and changes done in other packages

--- packages/sdk/src/scopes/invoice/index.ts
+++ packages/sdk/src/scopes/invoice/index.ts
@@ -1,10 +1,11 @@
 import { request, type InvoiceStatus } from '@telegram-apps/bridge';
 import { computed } from '@telegram-apps/signals';
+import { AsyncOptions, BetterPromise } from '@telegram-apps/toolkit';
 
 import { decorateWithIsSupported, type WithIsSupported } from '@/scopes/decorateWithIsSupported.js';
 import { $postEvent } from '@/scopes/globals/globals.js';
 import { ERR_INVALID_HOSTNAME, ERR_INVALID_SLUG, ERR_INVOICE_OPENED } from '@/errors/errors.js';
-import { createError } from '@/errors/createError.js';
+import { SDKError } from '@/errors/SDKError.js';
 
 import * as _ from './private.js';
 
@@ -17,6 +18,7 @@ type OpenFn = WithIsSupported<{
   /**
    * Opens an invoice using its slug.
    * @param slug - invoice slug.
+   * @param options - additional options.
    * @throws {SDKError} ERR_INVOICE_OPENED
    * @throws {SDKError} ERR_INVALID_HOSTNAME
    * @throws {SDKError} ERR_INVALID_SLUG
@@ -24,11 +26,12 @@ type OpenFn = WithIsSupported<{
    * @see ERR_INVALID_HOSTNAME
    * @see ERR_INVALID_SLUG
    */
-  (slug: string): Promise<InvoiceStatus>;
+  (slug: string, options?: AsyncOptions): BetterPromise<InvoiceStatus>;
   /**
    * Opens an invoice using its url. It expects to pass a link in full format, with hostname "t.me".
    * @param url - invoice URL.
    * @param type - value type.
+   * @param options - additional options.
    * @throws {SDKError} ERR_INVOICE_OPENED
    * @throws {SDKError} ERR_INVALID_HOSTNAME
    * @throws {SDKError} ERR_INVALID_SLUG
@@ -36,7 +39,7 @@ type OpenFn = WithIsSupported<{
    * @see ERR_INVALID_HOSTNAME
    * @see ERR_INVALID_SLUG
    */
-  (url: string, type: 'url'): Promise<InvoiceStatus>;
+  (url: string, type: 'url', options?: AsyncOptions): BetterPromise<InvoiceStatus>;
 }>;
 
 const MINI_APPS_METHOD = 'web_app_open_invoice';
@@ -44,50 +47,51 @@ const MINI_APPS_METHOD = 'web_app_open_invoice';
 /**
  * True if the invoice is currently opened.
  */
-export const isOpened = computed(_.isOpened);
+export const isOpened = computed(_.$isOpened);
 
 export const open: OpenFn = decorateWithIsSupported(
-  async (urlOrSlug, type?) => {
-    if (_.isOpened()) {
-      throw createError(ERR_INVOICE_OPENED);
-    }
-
-    let slug: string;
-    if (!type) {
-      slug = urlOrSlug;
-    } else {
-      const { hostname, pathname } = new URL(urlOrSlug, window.location.href);
-      if (hostname !== 't.me') {
-        throw createError(ERR_INVALID_HOSTNAME);
+  (urlOrSlug, optionsOrType?, options?) => {
+    return BetterPromise.withFn(() => {
+      if (_.$isOpened()) {
+        throw new SDKError(ERR_INVOICE_OPENED);
       }
 
-      // Valid examples:
-      // "/invoice/my-slug"
-      // "/$my-slug"
-      const match = pathname.match(/^\/(\$|invoice\/)([A-Za-z0-9\-_=]+)$/);
-      if (!match) {
-        throw createError(ERR_INVALID_SLUG);
+      let slug: string;
+
+      if (optionsOrType === 'url') {
+        const { hostname, pathname } = new URL(urlOrSlug, window.location.href);
+        if (hostname !== 't.me') {
+          throw new SDKError(ERR_INVALID_HOSTNAME);
+        }
+
+        // Valid examples:
+        // "/invoice/my-slug"
+        // "/$my-slug"
+        const match = pathname.match(/^\/(\$|invoice\/)([A-Za-z0-9\-_=]+)$/);
+        if (!match) {
+          throw new SDKError(ERR_INVALID_SLUG);
+        }
+        [, , slug] = match;
+      } else {
+        slug = urlOrSlug;
+        options = optionsOrType;
       }
-      [, , slug] = match;
-    }
 
-    _.isOpened.set(true);
+      _.$isOpened.set(true);
 
-    try {
-      const result = await request({
-        method: MINI_APPS_METHOD,
-        event: 'invoice_closed',
+      return request(MINI_APPS_METHOD, 'invoice_closed', {
+        ...options || {},
         params: { slug },
         postEvent: $postEvent(),
         capture(data) {
           return slug === data.slug;
         },
-      });
-
-      return result.status;
-    } finally {
-      _.isOpened.set(false);
-    }
+      })
+        .then(r => r.status)
+        .finally(() => {
+          _.$isOpened.set(false);
+        });
+    });
   },
   MINI_APPS_METHOD,
 );
