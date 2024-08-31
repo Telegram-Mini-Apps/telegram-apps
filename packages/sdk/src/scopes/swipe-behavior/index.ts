203d3246685c56685384dfdc4e26ce2eb3760b6d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 20:50:27 +0500

    refactor(sdk): refactor swipe-behavior scope according to new "ideology" and changes done in other packages

--- packages/sdk/src/scopes/swipe-behavior/index.ts
+++ packages/sdk/src/scopes/swipe-behavior/index.ts
@@ -1,8 +1,8 @@
 import { computed } from '@telegram-apps/signals';
 import { isPageReload } from '@telegram-apps/navigation';
+import { getStorageValue, setStorageValue } from '@telegram-apps/toolkit';
 
 import { $postEvent } from '@/scopes/globals/globals.js';
-import { getStorageValue, setStorageValue } from '@/utils/storage.js';
 import { decorateWithIsSupported, type WithIsSupported } from '@/scopes/decorateWithIsSupported.js';
 
 import * as _ from './private.js';
@@ -13,6 +13,8 @@ import * as _ from './private.js';
  * @see API: https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/components/swipe-behavior
  */
 
+type StorageValue = boolean;
+
 const MINI_APPS_METHOD = 'web_app_setup_swipe_behavior';
 const STORAGE_KEY = 'swipeBehavior';
 
@@ -20,25 +22,25 @@ const STORAGE_KEY = 'swipeBehavior';
  * Disables vertical swipes.
  */
 export const disableVerticalSwipes: WithIsSupported<() => void> = decorateWithIsSupported(() => {
-  _.isVerticalSwipesEnabled.set(false);
+  _.$isVerticalSwipesEnabled.set(false);
 }, MINI_APPS_METHOD);
 
 /**
  * Enables vertical swipes.
  */
 export const enableVerticalSwipes: WithIsSupported<() => void> = decorateWithIsSupported(() => {
-  _.isVerticalSwipesEnabled.set(true);
+  _.$isVerticalSwipesEnabled.set(true);
 }, MINI_APPS_METHOD);
 
 /**
  * True if vertical swipes are enabled.
  */
-export const isVerticalSwipesEnabled = computed(_.isVerticalSwipesEnabled);
+export const isVerticalSwipesEnabled = computed(_.$isVerticalSwipesEnabled);
 
 /**
  * True if the component is currently mounted.
  */
-export const isMounted = computed(_.isMounted);
+export const isMounted = computed(_.$isMounted);
 
 /**
  * Mounts the component.
@@ -47,22 +49,22 @@ export const isMounted = computed(_.isMounted);
  * if it changed.
  */
 export function mount(): void {
-  if (!_.isMounted()) {
-    _.isVerticalSwipesEnabled.set(isPageReload() && getStorageValue(STORAGE_KEY) || false);
-    _.isVerticalSwipesEnabled.sub(onStateChanged);
-    _.isMounted.set(true);
+  if (!_.$isMounted()) {
+    _.$isVerticalSwipesEnabled.set(isPageReload() && getStorageValue<StorageValue>(STORAGE_KEY) || false);
+    _.$isVerticalSwipesEnabled.sub(onStateChanged);
+    _.$isMounted.set(true);
   }
 }
 
 function onStateChanged(value: boolean): void {
   $postEvent()(MINI_APPS_METHOD, { allow_vertical_swipe: value });
-  setStorageValue(STORAGE_KEY, value);
+  setStorageValue<StorageValue>(STORAGE_KEY, value);
 }
 
 /**
  * Unmounts the component, removing the listener, saving the component state in the local storage.
  */
 export function unmount(): void {
-  _.isVerticalSwipesEnabled.unsub(onStateChanged);
-  _.isMounted.set(false);
+  _.$isVerticalSwipesEnabled.unsub(onStateChanged);
+  _.$isMounted.set(false);
 }
