b88de0b8a8f086b52a97d4336009ff1b9d3c8c85
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-31 15:21:27 +0500

    refactor(sdk): refactor global scope according to new "ideology" and changes done in other packages

--- packages/sdk/src/scopes/globals/globals.ts
+++ packages/sdk/src/scopes/globals/globals.ts
@@ -1,12 +1,12 @@
 import {
-  type PostEvent,
+  retrieveLaunchParams,
   postEvent as defaultPostEvent,
   createPostEvent,
-  type Version, OnUnsupportedFn,
+  type PostEventFn,
+  type Version,
+  type CreatePostEventMode,
 } from '@telegram-apps/bridge';
 import { signal } from '@telegram-apps/signals';
-
-import { retrieveLaunchParams } from '@/scopes/launch-params/retrieveLaunchParams.js';
 
 export interface ConfigureOptions {
   /**
@@ -28,7 +28,7 @@ export interface ConfigureOptions {
    * @default 'strict'
    * @see createPostEvent
    */
-  postEvent?: PostEvent | 'strict' | 'non-strict';
+  postEvent?: PostEventFn | CreatePostEventMode;
 }
 
 /**
@@ -51,28 +51,16 @@ export function configure(options?: ConfigureOptions): void {
   $postEvent.set(
     typeof optionsPostEvent === 'function'
       ? optionsPostEvent
-      : createPostEvent(v, optionsPostEvent === 'non-strict'
-        ? onNonStrictUnsupported
-        : undefined,
-      ),
+      : createPostEvent(v, optionsPostEvent),
   );
 }
 
 /**
  * Signal with a currently used postEvent function across the package.
  */
-export const $postEvent = signal<PostEvent>(defaultPostEvent);
+export const $postEvent = signal<PostEventFn>(defaultPostEvent);
 
 /**
  * Signal with a currently supported maximum Mini Apps version. This value is usually set via
  */
 export const $version = signal<Version>('0.0');
-
-const onNonStrictUnsupported: OnUnsupportedFn = data => {
-  const { method, version } = data;
-  if ('param' in data) {
-    console.warn(`Parameter "${data.param}" of "${method}" method is unsupported in Mini Apps version ${version}`);
-  } else {
-    console.warn(`Method "${method}" is unsupported in Mini Apps version ${version}`);
-  }
-};
