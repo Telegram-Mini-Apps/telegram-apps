b0c763efd2b4a74553a86a5843475577f2e129de
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-02 03:39:59 +0500

    tests(sdk,mini-app): actualize tests

--- packages/sdk/src/scopes/mini-app/methods.test.ts
+++ packages/sdk/src/scopes/mini-app/methods.test.ts
@@ -1,142 +1,153 @@
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, Mock, MockInstance, vi } from 'vitest';
+import { createWindow } from 'test-utils';
 
-import type { PostEventFn, RGB } from '@telegram-apps/bridge';
+import { resetPackageState, resetSignal } from '@test-utils/reset.js';
+import { $postEvent } from '@/scopes/globals/globals.js';
+import * as themeParams from '@/scopes/theme-params/instance.js';
 
-// fixme
-import { createRequestIdGenerator } from '@/utils/createRequestIdGenerator.js';
+import {
+  headerColor,
+  backgroundColor,
+  state,
+  isMounted,
+  isDark,
+  isCssVarsBound,
+} from './signals.js';
+import { bindCssVars, mount, setBackgroundColor, setHeaderColor } from './methods.js';
 
-import { MiniApp } from './index.js';
-import type { MiniAppHeaderColor } from './types.js';
+type SetPropertyFn = typeof document.documentElement.style.setProperty;
+let setPropertySpy: MockInstance<Parameters<SetPropertyFn>, ReturnType<SetPropertyFn>>;
 
-interface CreateWebAppOptions {
-  bgColor?: RGB;
-  botInline?: boolean;
-  headerColor?: MiniAppHeaderColor;
-  postEvent?: PostEvent;
-  version?: string;
-}
+vi.mock('@/scopes/launch-params/static.js', () => ({
+  retrieve: () => ({
+    themeParams: {
+      bgColor: '#ffcccc',
+      secondaryBgColor: '#ccccff',
+    },
+  }),
+}));
 
-function createMiniApp(options: CreateWebAppOptions = {}): MiniApp {
-  const {
-    postEvent = vi.fn(),
-    bgColor = '#000000',
-    headerColor = 'bg_color',
-    version = '6.0',
-    botInline = false,
-  } = options;
-
-  return new MiniApp({
-    version,
-    botInline,
-    postEvent,
+beforeEach(() => {
+  vi.restoreAllMocks();
+  resetPackageState();
+  [
+    themeParams.isCssVarsBound,
+    themeParams.isMounted,
+    themeParams.state,
     headerColor,
-    bgColor,
-    createRequestId: createRequestIdGenerator(),
-  });
-}
+    backgroundColor,
+    state,
+    isMounted,
+    isDark,
+    isCssVarsBound,
+  ].forEach(resetSignal);
 
-describe('setBackgroundColor', () => {
-  it('should call "web_app_set_background_color" method with { color: {{color}} }', () => {
-    const postEvent = vi.fn();
-    const miniApp = createMiniApp({ postEvent });
-
-    expect(postEvent).toHaveBeenCalledTimes(0);
-    miniApp.setBgColor('#ffaabb');
-    expect(postEvent).toHaveBeenCalledTimes(1);
-    expect(postEvent).toHaveBeenCalledWith('web_app_set_background_color', { color: '#ffaabb' });
-  });
-
-  it('should emit "change:bgColor" event with specified value', () => {
-    const miniApp = createMiniApp({ bgColor: '#ffffff' });
-    const listener = vi.fn();
-
-    miniApp.on('change:bgColor', listener);
-    expect(listener).toHaveBeenCalledTimes(0);
-    miniApp.setBgColor('#ffaacc');
-    expect(listener).toHaveBeenCalledTimes(1);
-    expect(listener).toHaveBeenCalledWith('#ffaacc');
-  });
+  createWindow();
+  setPropertySpy = vi
+    .spyOn(document.documentElement.style, 'setProperty')
+    .mockImplementation(() => {
+    });
+  $postEvent.set(() => null);
 });
 
-describe('setHeaderColor', () => {
-  it('should call "web_app_set_header_color" method with { color_key: {{color_key}} }', () => {
-    const postEvent = vi.fn();
-    const miniApp = createMiniApp({ postEvent, headerColor: 'bg_color' });
-
-    expect(postEvent).toHaveBeenCalledTimes(0);
-    miniApp.setHeaderColor('secondary_bg_color');
-    expect(postEvent).toHaveBeenCalledTimes(1);
-    expect(postEvent).toHaveBeenCalledWith('web_app_set_header_color', { color_key: 'secondary_bg_color' });
-  });
-
-  it('should emit "change:headerColor" event with specified value', () => {
-    const miniApp = createMiniApp({ headerColor: 'bg_color' });
-    const listener = vi.fn();
-
-    miniApp.on('change:headerColor', listener);
-    expect(listener).toHaveBeenCalledTimes(0);
-    miniApp.setHeaderColor('secondary_bg_color');
-    expect(listener).toHaveBeenCalledTimes(1);
-    expect(listener).toHaveBeenCalledWith('secondary_bg_color');
-  });
-});
-
-describe('off', () => {
-  describe('"change:bgColor" event', () => {
-    it('should remove event listener from event', () => {
-      const listener = vi.fn();
-      const miniApp = createMiniApp({ bgColor: '#ffffff' });
-
-      miniApp.on('change:bgColor', listener);
-
-      expect(listener).toHaveBeenCalledTimes(0);
-      miniApp.setBgColor('#aaddcc');
-      expect(listener).toHaveBeenCalledTimes(1);
-
-      miniApp.off('change:bgColor', listener);
-      listener.mockClear();
-
-      expect(listener).toHaveBeenCalledTimes(0);
-      miniApp.setBgColor('#ffaaaa');
-      expect(listener).toHaveBeenCalledTimes(0);
+describe('bindCssVars', () => {
+  describe('backgroundColor', () => {
+    it('should set --tg-bg-color == backgroundColor()', () => {
+      backgroundColor.set('#abcdef');
+      bindCssVars();
+      expect(setPropertySpy).toHaveBeenCalledOnce();
+      expect(setPropertySpy).toHaveBeenCalledWith('--tg-bg-color', '#abcdef');
     });
   });
 
-  describe('"change:headerColor" event', () => {
-    it('should remove event listener from event', () => {
-      const listener = vi.fn();
-      const miniApp = createMiniApp({ headerColor: 'bg_color' });
+  describe('headerColor', () => {
+    it('should set --tg-header-color == headerColor() if header color is RGB', () => {
+      headerColor.set('#fedcba');
+      bindCssVars();
+      expect(setPropertySpy).toHaveBeenCalledTimes(2);
+      expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#fedcba');
+    });
 
-      miniApp.on('change:headerColor', listener);
+    it('should set --tg-header-color equal to theme params bgColor if headerColor is bg_color', () => {
+      headerColor.set('bg_color');
+      themeParams.state.set({ bgColor: '#aaaaaa' });
+      bindCssVars();
+      expect(setPropertySpy).toHaveBeenCalledTimes(2);
+      expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#aaaaaa');
+    });
 
-      expect(listener).toHaveBeenCalledTimes(0);
-      miniApp.setHeaderColor('secondary_bg_color');
-      expect(listener).toHaveBeenCalledTimes(1);
-
-      miniApp.off('change:headerColor', listener);
-      listener.mockClear();
-
-      expect(listener).toHaveBeenCalledTimes(0);
-      miniApp.setHeaderColor('bg_color');
-      expect(listener).toHaveBeenCalledTimes(0);
+    it('should set --tg-header-color equal to theme params secondaryBgColor if headerColor is secondary_bg_color', () => {
+      headerColor.set('secondary_bg_color');
+      themeParams.state.set({ secondaryBgColor: '#dddddd' });
+      bindCssVars();
+      expect(setPropertySpy).toHaveBeenCalledTimes(2);
+      expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#dddddd');
     });
   });
-});
 
-describe('supports', () => {
-  describe('setHeaderColor / setBackgroundColor', () => {
-    it('should return true in case, WebApp version is 6.1 or higher. False, otherwise', () => {
-      const miniApp1 = createMiniApp({ version: '6.0' });
-      expect(miniApp1.supports('setHeaderColor')).toBe(false);
-      expect(miniApp1.supports('setBackgroundColor')).toBe(false);
+  describe('mounted', () => {
+    beforeEach(mount);
 
-      const miniApp2 = createMiniApp({ version: '6.1' });
-      expect(miniApp2.supports('setHeaderColor')).toBe(true);
-      expect(miniApp2.supports('setBackgroundColor')).toBe(true);
+    describe('backgroundColor', () => {
+      it('should set --tg-bg-color == backgroundColor() when background color changed', () => {
+        bindCssVars();
+        setPropertySpy.mockClear();
+        backgroundColor.set('#aaaaaa');
+        expect(setPropertySpy).toHaveBeenCalledOnce();
+        expect(setPropertySpy).toHaveBeenCalledWith('--tg-bg-color', '#aaaaaa');
+      });
+    });
 
-      const miniApp3 = createMiniApp({ version: '6.2' });
-      expect(miniApp3.supports('setHeaderColor')).toBe(true);
-      expect(miniApp3.supports('setBackgroundColor')).toBe(true);
+    describe('headerColor', () => {
+      it('should update --tg-header-color each time headerColor or related theme param changes', () => {
+        bindCssVars();
+        themeParams.state.set({
+          bgColor: '#ffffff',
+          secondaryBgColor: '#eeeeee',
+        });
+        setPropertySpy.mockClear();
+        headerColor.set('secondary_bg_color');
+        expect(setPropertySpy).toHaveBeenCalledOnce();
+        expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#eeeeee');
+
+        setPropertySpy.mockClear();
+        headerColor.set('bg_color');
+        expect(setPropertySpy).toHaveBeenCalledOnce();
+        expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#ffffff');
+
+        setPropertySpy.mockClear();
+        themeParams.state.set({
+          bgColor: '#aaaaaa',
+        });
+        expect(setPropertySpy).toHaveBeenCalledOnce();
+        expect(setPropertySpy).toHaveBeenCalledWith('--tg-header-color', '#aaaaaa');
+      });
+    });
+
+    let spy: Mock;
+
+    beforeEach(() => {
+      mount();
+      spy = vi.fn();
+      $postEvent.set(spy);
+    });
+
+    describe('setBackgroundColor', () => {
+      it('should call "web_app_set_background_color" method with { color: {{color}} }', () => {
+        expect(spy).toHaveBeenCalledTimes(0);
+        setBackgroundColor('#abcdef');
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith('web_app_set_background_color', { color: '#abcdef' });
+      });
+    });
+
+    describe('setHeaderColor', () => {
+      it('should call "web_app_set_header_color" method with { color_key: {{color_key}} }', () => {
+        expect(spy).toHaveBeenCalledTimes(0);
+        setHeaderColor('secondary_bg_color');
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith('web_app_set_header_color', { color_key: 'secondary_bg_color' });
+      });
     });
   });
 });
