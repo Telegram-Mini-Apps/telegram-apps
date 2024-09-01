3fba64b317a6f231a033368e6021c45f6e8c13fa
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-01 20:12:32 +0500

    refactor(bridge): refactor qr-scanner scope

---
+++ packages/sdk/src/scopes/qr-scanner/methods.ts
@@ -0,0 +1,120 @@
+import { request, on } from '@telegram-apps/bridge';
+import { type AsyncOptions, BetterPromise, createCbCollector } from '@telegram-apps/toolkit';
+
+import { decorateWithIsSupported, type WithIsSupported } from '@/scopes/decorateWithIsSupported.js';
+import { $postEvent } from '@/scopes/globals/globals.js';
+import { ERR_SCANNER_OPENED } from '@/errors/errors.js';
+import { SDKError } from '@/errors/SDKError.js';
+
+import { isOpened } from './signals.js';
+
+type OpenFn = WithIsSupported<{
+  /**
+   * Opens the scanner.
+   *
+   * Whenever a user scans a QR, the passed `capture` function is being called with the QR
+   * content. It should return true if this QR must be captured and promise resolved.
+   * @param options - method options.
+   * @returns A captured QR content or null, if the scanner was closed.
+   */
+  (options: AsyncOptions & {
+    /**
+     * Title to be displayed in the scanner.
+     */
+    text?: string;
+    /**
+     * Function, which should return true if a scanned QR should be captured and promise resolved.
+     * @param qr - scanned QR content.
+     */
+    capture(this: void, qr: string): boolean;
+  }): BetterPromise<string | null>;
+  /**
+   * Opens the scanner in stream mode.
+   *
+   * Whenever a user scans a QR, the passed `onCaptured` function will be called.
+   * @param options - method options.
+   */
+  (options: AsyncOptions & {
+    /**
+     * Title to be displayed in the scanner.
+     */
+    text?: string;
+    /**
+     * Function which will be called in case, some QR code was scanned.
+     * @param qr - scanned QR content.
+     */
+    onCaptured(this: void, qr: string): void;
+  }): void;
+}>
+
+const CLOSE_METHOD = 'web_app_close_scan_qr_popup';
+const OPEN_METHOD = 'web_app_open_scan_qr_popup';
+const CLOSED_EVENT = 'scan_qr_popup_closed';
+const SCANNED_EVENT = 'qr_text_received';
+
+/**
+ * Closes the scanner.
+ */
+export const close: WithIsSupported<() => void> = decorateWithIsSupported(() => {
+  isOpened.set(false);
+  $postEvent()(CLOSE_METHOD);
+}, CLOSE_METHOD);
+
+export const open: OpenFn = decorateWithIsSupported((options) => {
+  if (isOpened()) {
+    throw new SDKError(ERR_SCANNER_OPENED);
+  }
+  isOpened.set(true);
+
+  const { text } = options;
+
+  // Capture single QR mode.
+  if ('capture' in options) {
+    const { capture } = options;
+
+    return request(OPEN_METHOD, [SCANNED_EVENT, CLOSED_EVENT], {
+      ...options,
+      postEvent: $postEvent(),
+      params: { text },
+      capture(ev) {
+        return ev.event === 'scan_qr_popup_closed' || capture(ev.payload.data);
+      },
+    })
+      .then((result) => {
+        const qr = result ? result.data : null;
+        if (qr !== null) {
+          // Some valid QR was scanned. After this, we can close the scanner.
+          close();
+        }
+        return qr;
+      })
+      .finally(() => {
+        isOpened.set(false);
+      });
+  }
+
+  // Stream mode.
+
+  let cleanup: VoidFunction;
+  return BetterPromise
+    .withOptions((res) => {
+      [, cleanup] = createCbCollector(
+        on('qr_text_received', ({ data }) => {
+          options.onCaptured(data);
+        }),
+        on('scan_qr_popup_closed', () => {
+          isOpened.set(false);
+        }),
+        // Whenever the scanner open state was changed, it means, it was closed by the developer.
+        // We interpret such an action as a successful QR scan.
+        isOpened.sub(res),
+      );
+
+      $postEvent()(OPEN_METHOD, { text: options.text });
+    }, options)
+    .then(() => {
+      isOpened.set(false);
+    })
+    .catch(close)
+    .finally(cleanup!);
+}, OPEN_METHOD) as OpenFn;
--- packages/sdk/src/scopes/qr-scanner/signals.ts
+++ packages/sdk/src/scopes/qr-scanner/signals.ts
@@ -1,3 +1,6 @@
 import { signal } from '@telegram-apps/signals';
 
+/**
+ * True if the scanner is currently opened.
+ */
 export const isOpened = signal(false);
