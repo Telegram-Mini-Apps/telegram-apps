f62ce2dff051a9c8662937c3a22e9b34de02a57f
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-01 20:50:42 +0500

    refactor(bridge): refactor haptic-feedback scope

---
+++ packages/sdk/src/scopes/haptic-feedback/instance.ts
@@ -0,0 +1,7 @@
+/*
+ * fixme
+ * @see Usage: https://docs.telegram-mini-apps.com/platform/haptic-feedback
+ * @see API: https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/components/haptic-feedback
+ */
+
+export * from './methods.js';
