f9505807247edb1b4f844d8c79e01e70b54d799c
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-01 19:01:30 +0500

    feat(bridge): implement createMountFn

---
+++ packages/sdk/src/scopes/createMountFn.ts
@@ -0,0 +1,62 @@
+import { batch, type Signal } from '@telegram-apps/signals';
+import { type AsyncOptions, BetterPromise } from '@telegram-apps/toolkit';
+
+/**
+ * Creates a mount function for a component.
+ * @param mount - function mounting the component.
+ * @param onMounted - callback which will be called with the mount result.
+ * @param mountPromise - signal containing mount promise.
+ * @param isMounted - signal containing mount state.
+ * @param mountError - signal containing mount error.
+ */
+// #__NO_SIDE_EFFECTS__
+export function createMountFn<T = void>(
+  mount: (options?: AsyncOptions) => (T | BetterPromise<T>),
+  onMounted: (result: T) => void,
+  {
+    mountPromise,
+    isMounted,
+    mountError,
+  }: {
+    isMounted: Signal<boolean>,
+    mountPromise: Signal<BetterPromise<void> | undefined>,
+    mountError: Signal<Error | undefined>,
+  },
+): (options?: AsyncOptions) => BetterPromise<void> {
+  return function mountFn(mountOptions) {
+    let promise = mountPromise();
+    if (promise) {
+      return promise;
+    }
+
+    if (isMounted()) {
+      return BetterPromise.resolve();
+    }
+
+    promise = BetterPromise
+      .withFn(mount, mountOptions)
+      .then<[true, T], [false, Error]>(result => [true, result], e => [false, e])
+      .then(tuple => {
+        batch(() => {
+          let error: Error | undefined;
+
+          if (tuple[0]) {
+            onMounted(tuple[1]);
+            isMounted.set(true);
+          } else {
+            error = tuple[1];
+          }
+
+          mountError.set(error);
+          mountPromise.set(undefined);
+
+          if (error) {
+            throw error;
+          }
+        });
+      });
+    mountPromise.set(promise);
+
+    return promise;
+  };
+}
