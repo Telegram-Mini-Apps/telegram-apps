72a11272fcb47d7155bd22ad1b8b119b90a8c7fb
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-01 19:59:19 +0500

    refactor(bridge): refactor swipe-behavior scope

---
+++ packages/sdk/src/scopes/swipe-behavior/methods.ts
@@ -0,0 +1,59 @@
+import { isPageReload } from '@telegram-apps/navigation';
+import { getStorageValue, setStorageValue } from '@telegram-apps/toolkit';
+
+import { $postEvent } from '@/scopes/globals/globals.js';
+import { decorateWithIsSupported, type WithIsSupported } from '@/scopes/decorateWithIsSupported.js';
+
+import { isMounted, isVerticalSwipesEnabled } from './signals.js';
+
+/*
+ * fixme
+ * @see Usage: https://docs.telegram-mini-apps.com/platform/swipe-behavior
+ * @see API: https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/components/swipe-behavior
+ */
+
+type StorageValue = boolean;
+
+const MINI_APPS_METHOD = 'web_app_setup_swipe_behavior';
+const STORAGE_KEY = 'swipeBehavior';
+
+/**
+ * Disables vertical swipes.
+ */
+export const disableVerticalSwipes: WithIsSupported<() => void> = decorateWithIsSupported(() => {
+  isVerticalSwipesEnabled.set(false);
+}, MINI_APPS_METHOD);
+
+/**
+ * Enables vertical swipes.
+ */
+export const enableVerticalSwipes: WithIsSupported<() => void> = decorateWithIsSupported(() => {
+  isVerticalSwipesEnabled.set(true);
+}, MINI_APPS_METHOD);
+
+/**
+ * Mounts the component.
+ *
+ * This function restores the component state and is automatically saving it in the local storage
+ * if it changed.
+ */
+export function mount(): void {
+  if (!isMounted()) {
+    isVerticalSwipesEnabled.set(isPageReload() && getStorageValue<StorageValue>(STORAGE_KEY) || false);
+    isVerticalSwipesEnabled.sub(onStateChanged);
+    isMounted.set(true);
+  }
+}
+
+function onStateChanged(value: boolean): void {
+  $postEvent()(MINI_APPS_METHOD, { allow_vertical_swipe: value });
+  setStorageValue<StorageValue>(STORAGE_KEY, value);
+}
+
+/**
+ * Unmounts the component, removing the listener, saving the component state in the local storage.
+ */
+export function unmount(): void {
+  isVerticalSwipesEnabled.unsub(onStateChanged);
+  isMounted.set(false);
+}
