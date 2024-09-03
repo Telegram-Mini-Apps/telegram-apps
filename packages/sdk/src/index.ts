0e6a0bc32c7dca813ee02761cf526079ecb38429
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-03 21:31:29 +0500

    chore(sdk): refactor exports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,13 +1,13 @@
 export { classNames } from '@/classnames/classNames.js';
 export { mergeClassNames, type MergeClassNames } from '@/classnames/mergeClassNames.js';
 
-export { isColorDark } from '@/utils/isColorDark.js';
-
-export { setCssVar, deleteCssVar } from '@/utils/css-vars.js';
-
 export { init } from '@/env/init.js';
 export { initWeb } from '@/env/initWeb.js';
 export { isSSR } from '@/env/isSSR.js';
+
+export { isColorDark } from '@/utils/isColorDark.js';
+
+export { setCssVar, deleteCssVar } from '@/utils/css-vars.js';
 
 export {
   type ErrorType,
@@ -23,11 +23,11 @@ export {
 } from '@/errors/errors.js';
 export { SDKError } from '@/errors/SDKError.js';
 
-export * as backButton from '@/scopes/back-button/index.js';
-export * as biometryManager from '@/scopes/biometry-manager/index.js';
+export * as backButton from '@/scopes/back-button/instance.js';
+export * as biometryManager from '@/scopes/biometry-manager/instance.js';
 export * as BiometryManager from '@/scopes/biometry-manager/static.js';
-export * as closingBehavior from '@/scopes/closing-behavior/index.js';
-export * as cloudStorage from '@/scopes/cloud-storage/index.js';
+export * as closingBehavior from '@/scopes/closing-behavior/instance.js';
+export * as cloudStorage from '@/scopes/cloud-storage/instance.js';
 export {
   shareURL,
   openLink,
@@ -41,33 +41,110 @@ export {
   requestContact,
 } from '@/scopes/common/privacy.js';
 export { switchInlineQuery, readTextFromClipboard } from '@/scopes/common/utils.js';
-export { $postEvent, $version, $createRequestId, configure } from '@/scopes/globals/globals.js';
-export * as hapticFeedback from '@/scopes/haptic-feedback/index.js';
-export * as initData from '@/scopes/init-data/index.js';
+export {
+  $postEvent,
+  $version,
+  $createRequestId,
+  configure,
+  type ConfigureOptions,
+} from '@/scopes/globals/globals.js';
+export * as hapticFeedback from '@/scopes/haptic-feedback/instance.js';
+export * as initData from '@/scopes/init-data/instance.js';
 export * as InitData from '@/scopes/init-data/static.js';
-export * as invoice from '@/scopes/invoice/index.js';
+export * as invoice from '@/scopes/invoice/instance.js';
 export * as LaunchParams from '@/scopes/launch-params/static.js';
-export * as mainButton from '@/scopes/main-button/index.js';
+export * as mainButton from '@/scopes/main-button/instance.js';
 export * as MainButton from '@/scopes/main-button/static.js';
-export * as miniApp from '@/scopes/mini-app/index.js';
-export * as MiniApp from '@/scopes/main-button/types.js';
-export * as popup from '@/scopes/popup/index.js';
+export * as miniApp from '@/scopes/mini-app/instance.js';
+export * as MiniApp from '@/scopes/main-button/static.js';
+export * as popup from '@/scopes/popup/instance.js';
 export * as Popup from '@/scopes/popup/static.js';
-export * as qrScanner from '@/scopes/qr-scanner/index.js';
-export * as QRScanner from '@/scopes/qr-scanner/static.js';
-export * as settingsButton from '@/scopes/settings-button/index.js';
-export * as swipeBehavior from '@/scopes/swipe-behavior/index.js';
-export * as themeParams from '@/scopes/theme-params/index.js';
+export * as qrScanner from '@/scopes/qr-scanner/instance.js';
+export * as settingsButton from '@/scopes/settings-button/instance.js';
+export * as swipeBehavior from '@/scopes/swipe-behavior/instance.js';
+export * as themeParams from '@/scopes/theme-params/instance.js';
 export * as ThemeParams from '@/scopes/theme-params/static.js';
-export * as viewport from '@/scopes/viewport/index.js';
+export * as viewport from '@/scopes/viewport/instance.js';
 export * as Viewport from '@/scopes/viewport/static.js';
 
+export {
+  BridgeError,
+  defineEventHandlers,
+  emitMiniAppsEvent,
+  isIframe,
+  removeEventHandlers,
+  compareVersions,
+  createPostEvent,
+  $debug,
+  ERR_INVOKE_CUSTOM_METHOD_RESPONSE,
+  ERR_METHOD_PARAMETER_UNSUPPORTED,
+  ERR_METHOD_UNSUPPORTED,
+  ERR_TIMED_OUT,
+  ERR_UNKNOWN_ENV,
+  invokeCustomMethod,
+  isTMA,
+  on,
+  off,
+  postEvent,
+  $targetOrigin,
+  request,
+  subscribe,
+  supports,
+  unsubscribe,
+  mockTelegramEnv,
+} from '@telegram-apps/bridge';
 export type {
+  AnyHapticFeedbackParams,
+  AnyInvokeCustomMethodParams,
+  BiometryAuthRequestStatus,
+  BiometryTokenUpdateStatus,
+  BiometryType,
   Chat,
   ChatType,
+  CustomMethodName,
+  CustomMethodParams,
+  CustomMethodsParams,
+  ErrorType as BridgeErrorType,
+  EventListener,
+  EventName,
+  EventPayload,
+  Events,
+  ExecuteWithOptions,
+  ExecuteWithPostEvent,
+  HeaderColorKey,
+  ImpactHapticFeedbackParams,
+  ImpactHapticFeedbackStyle,
+  InvoiceStatus,
+  MethodName,
+  MethodNameWithOptionalParams,
+  MethodNameWithoutParams,
+  MethodNameWithRequiredParams,
+  MethodNameWithVersionedParams,
+  MethodParams,
+  Methods,
+  MethodVersionedParams,
+  NotificationHapticFeedbackParams,
+  NotificationHapticFeedbackType,
+  OpenLinkBrowser,
+  OnUnsupportedFn,
+  PhoneRequestedStatus,
   Platform,
+  PopupButton,
+  PopupParams,
+  PostEventFn,
+  RequestCaptureEventFn,
+  RequestCaptureEventsFn,
   RGB,
   RGBShort,
+  SelectionHapticFeedbackParams,
+  SwitchInlineQueryChatType,
+  ThemeParamsKey,
   User,
   Version,
-} from '@telegram-apps/types';
+  WriteAccessRequestedStatus,
+  CreatePostEventMode,
+  RequestBasicOptions,
+  RequestCaptureFn,
+  RequestFn,
+  RequestResult
+} from '@telegram-apps/bridge';
