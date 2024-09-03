c70c30d57e5ce929b1dc1af3a43bb5b5a9aeb602
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-04 02:00:50 +0500

    refactor(react-router-integration): refactor useIntegration

--- packages/react-router-integration/src/useIntegration.ts
+++ packages/react-router-integration/src/useIntegration.ts
@@ -1,53 +1,51 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import type { Location, Navigator, To, NavigateOptions } from 'react-router-dom';
-import { BrowserNavigator, createSafeURL, urlToPath } from '@telegram-apps/sdk';
+import { useMemo } from 'react';
+import { type Navigator, createSafeURL, urlToPath } from '@telegram-apps/navigation';
+import { useSignal } from '@telegram-apps/react-signals';
+import type {
+  Location as RouterLocation,
+  Navigator as RouterNavigator,
+  To,
+  NavigateOptions,
+} from 'react-router-dom';
 
 /**
  * Uses the passed Mini Apps navigator and returns a tuple containing reactive values
  * representing current location and react-router-dom navigator.
  * @param nav - Mini Apps navigator.
  */
-export function useIntegration<State>(nav: BrowserNavigator<State>): [Location, Navigator] {
-  // Creates location based on the Mini Apps navigator.
-  const createLocation = useCallback((): Location => ({
-    state: nav.state,
-    key: nav.id,
-    pathname: nav.pathname,
-    hash: nav.hash,
-    search: nav.search,
-  }), [nav]);
+export function useIntegration<State>(nav: Navigator<State>): [
+  RouterLocation<State>,
+  RouterNavigator
+] {
+  const [location] = useSignal(nav.location);
 
-  const [location, setLocation] = useState(createLocation);
+  return [
+    useMemo(() => ({
+      ...location,
+      state: location.state as State,
+      key: location.id,
+    }), [location]),
+    useMemo(() => {
+      function navigate(to: To, state?: State, options?: NavigateOptions) {
+        options ||= {};
+        if (state === undefined) {
+          state = options.state;
+        }
+        const path = urlToPath(to);
+        if (options.replace) {
+          nav.replace(path, state);
+        } else {
+          nav.push(path, state);
+        }
+      }
 
-  // todo: We should check if state is always State.
-  const navigate = useCallback((to: To, state?: State, options?: NavigateOptions) => {
-    options ||= {};
-    if (state === undefined) {
-      state = options.state;
-    }
-    const path = urlToPath(to);
-    if (options.replace) {
-      nav.replace(path, state);
-    } else {
-      nav.push(path, state);
-    }
-  }, [nav]);
-
-  // Create Navigator appropriate to the react-router-dom package.
-  const navigator = useMemo<Navigator>(() => ({
-    go(delta) {
-      nav.go(delta);
-    },
-    push: navigate,
-    replace: navigate,
-    createHref: (to) => nav.renderPath(urlToPath(to)),
-    encodeLocation: (to) => createSafeURL(nav.renderPath(urlToPath(to))),
-  }), [nav]);
-
-  // When Mini Apps navigator changes its location, we should actualize the reactive values.
-  useEffect(() => {
-    return nav.on('change', () => setLocation(createLocation()));
-  }, [nav, createLocation]);
-
-  return [location, navigator];
+      return {
+        go: nav.go,
+        push: navigate,
+        replace: navigate,
+        createHref: (to) => nav.renderPath(urlToPath(to)),
+        encodeLocation: (to) => createSafeURL(nav.renderPath(urlToPath(to))),
+      };
+    }, [nav]),
+  ];
 }
