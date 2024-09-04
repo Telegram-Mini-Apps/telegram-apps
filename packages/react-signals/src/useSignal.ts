56a2996ae8aec36b9c93ed518faf4a6662ee8f58
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-04 23:00:15 +0500

    fix(react-signals): simplify useSignal

--- packages/react-signals/src/useSignal.ts
+++ packages/react-signals/src/useSignal.ts
@@ -1,11 +1,10 @@
-import { computed, type Computed } from '@telegram-apps/signals';
-import { useMemo, useSyncExternalStore } from 'react';
+import { useSyncExternalStore } from 'react';
+import type { Computed } from '@telegram-apps/signals';
 
 /**
  * Returns the underlying signal value updating it each time the signal value changes.
  * @param signal - a signal.
  */
-export function useSignal<T>(signal: () => T): [value: T, signal: Computed<T>] {
-  const c = useMemo(() => computed(signal), []);
-  return [useSyncExternalStore(c.sub, c), c];
+export function useSignal<T>(signal: Computed<T>): T {
+  return useSyncExternalStore(signal.sub, signal);
 }
