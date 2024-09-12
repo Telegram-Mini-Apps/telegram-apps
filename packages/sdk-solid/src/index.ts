320b9917531a65dd222949a909802c4a7dd34b3d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-13 02:02:39 +0500

    feat(packages,sdk-solid): rework the package

--- packages/sdk-solid/src/index.ts
+++ packages/sdk-solid/src/index.ts
@@ -1,25 +1,2 @@
-export { useBackButton, withBackButton } from './hooks-hocs/back-button.js';
-export { useBiometryManager, withBiometryManager } from './hooks-hocs/biometry-manager.js';
-export { useClosingBehavior, withClosingBehavior } from './hooks-hocs/closing-behavior.js';
-export { useCloudStorage, withCloudStorage } from './hooks-hocs/cloud-storage.js';
-export { useHapticFeedback, withHapticFeedback } from './hooks-hocs/haptic-feedback.js';
-export { useInitData, withInitData } from './hooks-hocs/init-data.js';
-export { useInvoice, withInvoice } from './hooks-hocs/invoice.js';
-export { useMainButton, withMainButton } from './hooks-hocs/main-button.js';
-export { useMiniApp, withMiniApp } from './hooks-hocs/mini-app.js';
-export { usePopup, withPopup } from './hooks-hocs/popup.js';
-export { useQRScanner, withQRScanner } from './hooks-hocs/qr-scanner.js';
-export { useSettingsButton, withSettingsButton } from './hooks-hocs/settings-button.js';
-export { useSwipeBehavior, withSwipeBehavior } from './hooks-hocs/swipe-behavior.js';
-export { useThemeParams, withThemeParams } from './hooks-hocs/theme-params.js';
-export { useUtils, withUtils } from './hooks-hocs/utils.js';
-export { useViewport, withViewport } from './hooks-hocs/viewport.js';
-export { useSDK } from './SDKProvider/SDKContext.js';
-export { SDKProvider } from './SDKProvider/SDKProvider.js';
-export type {
-  SDKContextType,
-  SDKProviderProps,
-  SDKContextItem,
-  FactorySignal,
-} from './SDKProvider/SDKProvider.types.js';
+export { useSignal } from './useSignal.js';
 export * from '@telegram-apps/sdk';
