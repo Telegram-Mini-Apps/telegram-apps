a60eef2c91474078c0b8035e05928680a64f392a
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-23 22:26:39 +0500

    chore(sdk,test-utils): remove createWindow, dispatchWindowMessageEvent. Improve mockPostEvent

--- packages/sdk/test-utils/mockPostEvent.ts
+++ packages/sdk/test-utils/mockPostEvent.ts
@@ -1,8 +1,10 @@
 import { vi } from 'vitest';
+import type { PostEventFn } from '@telegram-apps/bridge';
+
 import { $postEvent } from '@/scopes/globals/globals.js';
 
-export function mockPostEvent() {
-  const fn = vi.fn(() => null);
-  $postEvent.set(fn);
+export function mockPostEvent(postEvent: PostEventFn = () => null) {
+  const fn = vi.fn(postEvent);
+  $postEvent.set(fn as any);
   return fn;
 }
