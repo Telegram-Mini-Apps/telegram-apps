ce67c255afc63278059318607d4acbfd2a085188
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-30 02:03:54 +0500

    refactor(sdk): move some files around

--- packages/sdk/test-utils/mockPostEvent.ts
+++ packages/sdk/test-utils/mockPostEvent.ts
@@ -1,7 +1,7 @@
 import { vi } from 'vitest';
 import type { PostEventFn } from '@telegram-apps/bridge';
 
-import { $postEvent } from '@/scopes/globals/globals.js';
+import { $postEvent } from '@/scopes/globals.js';
 
 export function mockPostEvent(postEvent: PostEventFn = () => null) {
   const fn = vi.fn(postEvent);
--- packages/sdk/test-utils/reset.ts
+++ packages/sdk/test-utils/reset.ts
@@ -1,7 +1,7 @@
 import { resetPackageState as resetBridgeState } from '@telegram-apps/bridge';
 import type { Computed, Signal } from '@telegram-apps/signals';
 
-import { $createRequestId, $postEvent, $version } from '@/scopes/globals/globals.js';
+import { $createRequestId, $postEvent, $version } from '@/scopes/globals.js';
 
 export function resetSignal(s: Signal<any> | Computed<any>) {
   s.unsubAll();
