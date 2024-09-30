43a2c34a58d51f73b5ca2b2ab844f4ce4955d353
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-09-30 14:13:55 +0500

    refactor(navigation): remove navigator from exports

--- packages/navigation/src/index.ts
+++ packages/navigation/src/index.ts
@@ -1,18 +1,6 @@
 export { getFirstNavigationEntry } from './history/getFirstNavigationEntry.js';
 export { isPageReload } from './history/isPageReload.js';
 
-export { createNavigator } from './navigator/createNavigator.js';
-export { createNavigatorFromLocation } from './navigator/createNavigatorFromLocation.js';
-export { initNavigator, type InitNavigatorOptions } from './navigator/initNavigator.js';
-export type {
-  Navigator,
-  NavigationType,
-  AnyHistoryItem,
-  BackButtonOptions,
-  NavigatorCtrOptions,
-  HistoryItem
-} from './navigator/types.js';
-
 export { createSafeURL } from './url/createSafeURL.js';
 export type { URLLike } from './url/types.js';
 export { urlToPath } from './url/urlToPath.js';
