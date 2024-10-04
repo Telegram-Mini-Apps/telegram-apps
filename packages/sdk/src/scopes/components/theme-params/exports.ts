ca12bd1dbdc9dae62416e13e889d3493eedbec4d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-05 03:59:52 +0500

    refactor(sdk,theme-params): move parseThemeParams to a separate file. Refactor signals

--- packages/sdk/src/scopes/components/theme-params/exports.ts
+++ packages/sdk/src/scopes/components/theme-params/exports.ts
@@ -28,5 +28,6 @@ export type {
   ThemeParams,
   ThemeParamsKey,
 } from './types.js';
+export * from './parseThemeParams.js';
 
 export { serializeThemeParams } from '@telegram-apps/transformers';
