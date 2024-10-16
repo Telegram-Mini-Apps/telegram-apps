7b6269b5da4fa70664e060fce3dff522ae034fd9
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-17 01:32:48 +0500

    fix(sdk,haptic-feedback): make method require isSupported check

--- packages/sdk/src/scopes/components/haptic-feedback/haptic-feedback.ts
+++ packages/sdk/src/scopes/components/haptic-feedback/haptic-feedback.ts
@@ -5,23 +5,26 @@ import {
 } from '@telegram-apps/bridge';
 
 import { $version, postEvent } from '@/scopes/globals.js';
+import { createWithIsSupported } from '@/scopes/toolkit/createWithIsSupported.js';
 
-const MINI_APPS_METHOD = 'web_app_trigger_haptic_feedback';
+const WEB_APP_TRIGGER_HAPTIC_FEEDBACK = 'web_app_trigger_haptic_feedback';
+
+const withIsSupported = createWithIsSupported(isSupported);
 
 /**
  * A method tells that an impact occurred. The Telegram app may play the appropriate haptics based
  * on style value passed.
  * @param style - impact style.
  */
-export function impactOccurred(style: ImpactHapticFeedbackStyle): void {
-  postEvent(MINI_APPS_METHOD, { type: 'impact', impact_style: style });
-}
+export const impactOccurred = withIsSupported((style: ImpactHapticFeedbackStyle): void => {
+  postEvent(WEB_APP_TRIGGER_HAPTIC_FEEDBACK, { type: 'impact', impact_style: style });
+});
 
 /**
  * @returns True if the haptic feedback is supported.
  */
 export function isSupported(): boolean {
-  return supports(MINI_APPS_METHOD, $version());
+  return supports(WEB_APP_TRIGGER_HAPTIC_FEEDBACK, $version());
 }
 
 /**
@@ -29,9 +32,9 @@ export function isSupported(): boolean {
  * app may play the appropriate haptics based on type value passed.
  * @param type - notification type.
  */
-export function notificationOccurred(type: NotificationHapticFeedbackType): void {
-  postEvent(MINI_APPS_METHOD, { type: 'notification', notification_type: type });
-}
+export const notificationOccurred = withIsSupported((type: NotificationHapticFeedbackType): void => {
+  postEvent(WEB_APP_TRIGGER_HAPTIC_FEEDBACK, { type: 'notification', notification_type: type });
+});
 
 /**
  * A method tells that the user has changed a selection. The Telegram app may play the
@@ -40,6 +43,6 @@ export function notificationOccurred(type: NotificationHapticFeedbackType): void
  * Do not use this feedback when the user makes or confirms a selection; use it only when the
  * selection changes.
  */
-export function selectionChanged(): void {
-  postEvent(MINI_APPS_METHOD, { type: 'selection_change' });
-}
+export const selectionChanged = withIsSupported((): void => {
+  postEvent(WEB_APP_TRIGGER_HAPTIC_FEEDBACK, { type: 'selection_change' });
+});
