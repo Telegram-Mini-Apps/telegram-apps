937603ca171d714cb736c6d2b3d7621a02284600
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-16 13:26:30 +0500

    feat(sdk): allow passing withIsSupported function instead of Mini Apps method

--- packages/sdk/src/scopes/withIsSupported.ts
+++ packages/sdk/src/scopes/withIsSupported.ts
@@ -11,25 +11,47 @@ export type WithIsSupported<F extends (...args: any) => any> = F & {
 };
 
 /**
+ * Adds the "isSupported" method to the passed function returning true if the wrapped
+ * function is supported.
+ *
+ * Also, if the "isSupported" method returned true during the function execution,
+ * the ERR_NOT_SUPPORTED error will be thrown.
+ * @param fn - function to extend.
+ * @param isSupported - function to check if the function is supported.
+ */
+export function withIsSupported<F extends (...args: any) => any>(
+  fn: F,
+  isSupported: () => boolean,
+): WithIsSupported<F>;
+
+/**
  * Adds the "isSupported" method to the passed function returning true, if the current Mini Apps
  * version supports passed Mini Apps method.
  *
  * Also, if the "isSupported" method returned true during the function execution,
  * the ERR_NOT_SUPPORTED error will be thrown.
  * @param fn - function to extend.
- * @param miniAppsMethod - Mini Apps method.
+ * @param method - Mini Apps method.
  * @param additionalCheck - additional function to be called along with method support.
  */
-
-/*@__NO_SIDE_EFFECTS__*/
 export function withIsSupported<F extends (...args: any) => any>(
   fn: F,
   method: MethodName,
   // todo: refactor it
   additionalCheck?: () => boolean,
+): WithIsSupported<F>;
+
+/*@__NO_SIDE_EFFECTS__*/
+export function withIsSupported<F extends (...args: any) => any>(
+  fn: F,
+  arg2: MethodName | (() => boolean),
+  // todo: refactor it
+  arg3?: () => boolean,
 ): WithIsSupported<F> {
-  function isSupported() {
-    return supports(method, $version()) && (!additionalCheck || additionalCheck());
+  function isSupported(): boolean {
+    return typeof arg2 === 'string'
+      ? supports(arg2, $version()) && (!arg3 || arg3())
+      : arg2();
   }
 
   return Object.assign(((...args) => {
