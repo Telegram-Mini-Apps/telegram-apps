cd22d614a9a4b2421f390dc5757f2c14970978f5
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-16 13:11:01 +0500

    fix(sdk): withIsSupported now throws error if method is not supported

--- packages/sdk/src/scopes/withIsSupported.ts
+++ packages/sdk/src/scopes/withIsSupported.ts
@@ -1,6 +1,7 @@
-import { type MethodName, supports } from '@telegram-apps/bridge';
+import { type MethodName, supports, TypedError } from '@telegram-apps/bridge';
 
 import { $version } from '@/scopes/globals.js';
+import { ERR_NOT_SUPPORTED } from '@/errors.js';
 
 export type WithIsSupported<F extends (...args: any) => any> = F & {
   /**
@@ -12,6 +13,9 @@ export type WithIsSupported<F extends (...args: any) => any> = F & {
 /**
  * Adds the "isSupported" method to the passed function returning true, if the current Mini Apps
  * version supports passed Mini Apps method.
+ *
+ * Also, if the "isSupported" method returned true during the function execution,
+ * the ERR_NOT_SUPPORTED error will be thrown.
  * @param fn - function to extend.
  * @param miniAppsMethod - Mini Apps method.
  * @param additionalCheck - additional function to be called along with method support.
@@ -21,11 +25,19 @@ export type WithIsSupported<F extends (...args: any) => any> = F & {
 export function withIsSupported<F extends (...args: any) => any>(
   fn: F,
   method: MethodName,
+  // todo: refactor it
   additionalCheck?: () => boolean,
 ): WithIsSupported<F> {
-  return Object.assign(fn, {
-    isSupported() {
-      return supports(method, $version()) && (!additionalCheck || additionalCheck());
-    },
+  function isSupported() {
+    return supports(method, $version()) && (!additionalCheck || additionalCheck());
+  }
+
+  return Object.assign(((...args) => {
+    if (!isSupported()) {
+      throw new TypedError(ERR_NOT_SUPPORTED);
+    }
+    fn(...args);
+  }) as F, {
+    isSupported,
   });
 }
