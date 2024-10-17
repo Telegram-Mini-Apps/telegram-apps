c544def939ac903e8df13aa905523b3aa732b682
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-17 11:17:39 +0500

    feat(sdk): support isSupported flag in createMountFn

--- packages/sdk/src/scopes/createMountFn.ts
+++ packages/sdk/src/scopes/createMountFn.ts
@@ -1,6 +1,7 @@
 import { batch, type Signal } from '@telegram-apps/signals';
 import { type AsyncOptions, CancelablePromise, TypedError } from '@telegram-apps/bridge';
 import { ERR_ALREADY_CALLED } from '@/errors.js';
+import { withIsSupported } from '@/scopes/toolkit/withIsSupported.js';
 
 /**
  * Creates a mount function for a component.
@@ -9,6 +10,7 @@ import { ERR_ALREADY_CALLED } from '@/errors.js';
  * @param mountPromise - signal containing mount promise.
  * @param isMounted - signal containing mount state.
  * @param mountError - signal containing mount error.
+ * @param isSupported - signal containing the component support flag.
  */
 // #__NO_SIDE_EFFECTS__
 export function createMountFn<T = void>(
@@ -18,41 +20,45 @@ export function createMountFn<T = void>(
     isMounting,
     isMounted,
     mountError,
+    isSupported,
   }: {
     isMounted: Signal<boolean>,
     isMounting: Signal<boolean>,
     mountError: Signal<Error | undefined>,
+    isSupported?: () => boolean,
   },
 ): (options?: AsyncOptions) => CancelablePromise<void> {
-  return function mountFn(mountOptions) {
-    if (isMounted()) {
-      return CancelablePromise.resolve();
-    }
+  return withIsSupported(mountOptions => {
+      if (isMounted()) {
+        return CancelablePromise.resolve();
+      }
 
-    if (isMounting()) {
-      throw new TypedError(ERR_ALREADY_CALLED);
-    }
-    isMounting.set(true);
+      if (isMounting()) {
+        throw new TypedError(ERR_ALREADY_CALLED);
+      }
+      isMounting.set(true);
 
-    return CancelablePromise
-      .withFn((abortSignal) => mount({ abortSignal }), mountOptions)
-      .then<[true, T], [false, Error]>(
-        r => [true, r],
-        e => [false, e],
-      )
-      .then(tuple => {
-        batch(() => {
-          isMounting.set(false);
-          isMounted.set(true);
+      return CancelablePromise
+        .withFn((abortSignal) => mount({ abortSignal }), mountOptions)
+        .then<[true, T], [false, Error]>(
+          r => [true, r],
+          e => [false, e],
+        )
+        .then(tuple => {
+          batch(() => {
+            isMounting.set(false);
+            isMounted.set(true);
 
-          if (tuple[0]) {
-            onMounted(tuple[1]);
-          } else {
-            const error = tuple[1];
-            mountError.set(error);
-            throw error;
-          }
+            if (tuple[0]) {
+              onMounted(tuple[1]);
+            } else {
+              const error = tuple[1];
+              mountError.set(error);
+              throw error;
+            }
+          });
         });
-      });
-  };
+    },
+    isSupported || (() => true),
+  );
 }
