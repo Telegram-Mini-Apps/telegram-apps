d6abfc3a74ee4401a376234b1cb1de111a1b8b71
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-10-17 10:52:30 +0500

    feat(sdk): make createIsSupported return signal

--- packages/sdk/src/scopes/toolkit/createIsSupported.ts
+++ packages/sdk/src/scopes/toolkit/createIsSupported.ts
@@ -1,12 +1,14 @@
-import { MethodName, supports } from '@telegram-apps/bridge';
+import { type Computed, computed } from '@telegram-apps/signals';
+
+import { type MethodName, supports } from '@telegram-apps/bridge';
 import { $version } from '@/scopes/globals.js';
 
 /**
- * @returns A function returning true if the specified Mini Apps method is supported.
+ * @returns A signal returning true if the specified Mini Apps method is supported.
  * @param method - Mini Apps method name
  */
-export function createIsSupported(method: MethodName) {
-  return function isSupported(): boolean {
+export function createIsSupported(method: MethodName): Computed<boolean> {
+  return computed(() => {
     return supports(method, $version());
-  }
+  });
 }
