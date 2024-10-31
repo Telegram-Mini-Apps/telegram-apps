3a93a3e36d23da26984c2fb8057173b727b8fffd
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-11-01 02:52:29 +0500

    feat(sdk/toolkit): implement createSafeWrap. Add more comments on safeWrap

--- packages/sdk/src/scopes/toolkit/safeWrap.ts
+++ packages/sdk/src/scopes/toolkit/safeWrap.ts
@@ -1,33 +1,68 @@
 import type { AnyFn } from '@/types.js';
-import { MethodName, supports, TypedError } from '@telegram-apps/bridge';
+import { If, MethodName, supports, TypedError } from '@telegram-apps/bridge';
 import { $version } from '@/scopes/globals.js';
-import { ERR_NOT_MOUNTED, ERR_NOT_SUPPORTED } from '@/errors.js';
+import { ERR_NOT_INITIALIZED, ERR_NOT_MOUNTED, ERR_NOT_SUPPORTED } from '@/errors.js';
 
-export type SafeWrap<Fn extends AnyFn, O extends Options> =
+export type SafeWrapped<Fn extends AnyFn, S extends boolean> =
   & Fn
   & {
   /**
-   * @returns True if the call function is safe in terms of support and
-   * mount checks.
+   * The method which is mostly used to check if the function call will
+   * not surely lead to error.
+   *
+   * It checks if the component is mounted and the function itself is
+   * supported. It also checks if the package itself is initialized.
+   *
+   * You should use this function when possible because it provides
+   * must-have code security mechanisms and makes a developer sure that
+   * he is using the package properly.
+   *
+   * @returns True if the owner component is mounted, function is supported
+   * and the packages initialized.
+   *
+   * @example
+   * if (showBackButton.isAvailable()) {
+   *   showBackButton();
+   * }
    */
   isAvailable: () => boolean;
 }
-  & (
-  O extends { isSupported: any }
-    ? {
-      /**
-       * @returns True if this function is supported.
-       */
-      isSupported: () => boolean;
-    }
-    : {}
-  );
+  & If<S, {
+  /**
+   * The method which notifies a developer that the function is supported by
+   * the current Mini Apps version including some possible additional
+   * conditions.
+   *
+   * @returns True if this function is supported.
+   *
+   * @example
+   * if (setMiniAppBottomBarColor.isSupported()) {
+   *   setMiniAppBottomBarColor('#ff00aa');
+   * }
+   */
+  isSupported: () => boolean;
+}, {}>
+
+export type IsSupported = MethodName | (() => boolean);
 
 interface Options {
+  /**
+   * The owner component name.
+   */
   component: string;
+  /**
+   * The method name.
+   */
   method: string;
-  isMounted: () => boolean;
-  isSupported?: MethodName | (() => boolean),
+  /**
+   * Signal returning true if the owner component is mounted.
+   */
+  isMounted?: () => boolean;
+  /**
+   * Mini Apps method name or function returning true determining if
+   * the function is supported.
+   */
+  isSupported?: IsSupported,
 }
 
 /**
@@ -46,35 +81,36 @@ export function safeWrap<Fn extends AnyFn, O extends Options>(
     isMounted,
     component,
     ...options
-  }: Options,
-): SafeWrap<Fn, O> {
-  const methodName = `${component}.${options.method}()`;
-
-  let isSupported: (() => boolean) | undefined;
-  if (isSupportedOrMethod) {
-    isSupported = () => {
-      return typeof isSupportedOrMethod === 'string'
-        ? supports(isSupportedOrMethod, $version())
-        : isSupportedOrMethod();
-    };
-  }
-
-  function isAvailable(): boolean {
-    return isMounted() && (!isSupported || isSupported());
-  }
+  }: O,
+): SafeWrapped<Fn, O extends { isSupported: any } ? true : false> {
+  const isSupported = isSupportedOrMethod
+    ? typeof isSupportedOrMethod === 'function'
+      ? isSupportedOrMethod
+      : () => supports(isSupportedOrMethod, $version())
+    : undefined;
+  const isInitialized = () => $version() !== '0.0';
+  const isAvailable = () => {
+    return isInitialized() && (!isMounted || isMounted()) && (!isSupported || isSupported());
+  };
 
   return Object.assign(
     (...args: Parameters<Fn>): ReturnType<Fn> => {
+      if (!isInitialized()) {
+        throw new TypedError(
+          ERR_NOT_INITIALIZED,
+          'The package was not initialized. Consider using the package init() function',
+        );
+      }
       if (isSupported && !isSupported()) {
         throw new TypedError(
           ERR_NOT_SUPPORTED,
-          `${methodName} method is not supported in version ${$version()}`,
+          `${component}.${options.method}() method is not supported in Mini Apps version ${$version()}`,
         );
       }
-      if (!isMounted()) {
+      if (isMounted && !isMounted()) {
         throw new TypedError(
           ERR_NOT_MOUNTED,
-          `${component} component is not mounted`,
+          `${component} component is not mounted. Consider using the mount() method`,
         );
       }
       return fn(...args);
@@ -82,12 +118,5 @@ export function safeWrap<Fn extends AnyFn, O extends Options>(
     fn,
     { isAvailable },
     isSupported ? { isSupported } : {},
-  ) as SafeWrap<Fn, O>;
-}
-
-export function createSafeWrap(
-  component: string,
-  isMounted: () => boolean,
-) {
-
+  ) as SafeWrapped<Fn, O extends { isSupported: any } ? true : false>;
 }
