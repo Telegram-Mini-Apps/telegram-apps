67349ec230c000b1c0d982d2d88e5ece58721d49
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-11-30 22:46:42 +0500

    feat(sdk): finalize signalifyAsyncFn

--- packages/sdk/src/scopes/signalifyAsyncFn.ts
+++ packages/sdk/src/scopes/signalifyAsyncFn.ts
@@ -5,13 +5,21 @@ import {
   type TypedError,
 } from '@telegram-apps/toolkit';
 
-type AllowedFn<R> = (options?: AsyncOptions) => CancelablePromise<R>;
+type AllowedFn<R> = (options?: AsyncOptions) => R | CancelablePromise<R>;
 
+/**
+ * Function doing the following:
+ * 1. Prevents the wrapped function from being called concurrently.
+ * 2. Being called, updates the passed promise and error signals.
+ * @param fn - function to wrap.
+ * @param createPendingError - function that creates error in case of concurrent call
+ * @param promise - signal containing the execution promise
+ * @param error - signal containing the last call error.
+ */
 // #__NO_SIDE_EFFECTS__
 export function signalifyAsyncFn<Fn extends AllowedFn<Result>, Result>(
   fn: Fn,
   createPendingError: () => TypedError<any>,
-  isCompleted: Signal<boolean>,
   promise: Signal<CancelablePromise<Result> | undefined>,
   error: Signal<Error | undefined>,
 ): Fn {
@@ -21,28 +29,30 @@ export function signalifyAsyncFn<Fn extends AllowedFn<Result>, Result>(
       .then(async () => {
         // Check if the operation is currently not in progress.
         if (promise()) {
-          throw createPendingError();
+          const err = createPendingError();
+          error.set(err);
+          throw err;
         }
 
-        // Perform the operation.
+        // Start performing the wrapped function.
+        batch(() => {
+          promise.set(CancelablePromise.resolve(fn(options)));
+          error.set(undefined);
+        });
         let result: [completed: true, result: Result] | [completed: false, err: Error];
         try {
-          promise.set(fn(options));
           result = [true, await promise()!];
         } catch (e) {
           result = [false, e as Error];
         }
 
+        // Actualize the state.
         batch(() => {
           promise.set(undefined);
-
-          if (result[0]) {
-            isCompleted.set(true);
-          } else {
-            error.set(result[1]);
-          }
+          error.set(result[0] ? undefined : result[1]);
         });
 
+        // Throw an error or return result.
         if (!result[0]) {
           throw result[1];
         }
