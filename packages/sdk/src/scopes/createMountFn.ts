b867cda8570d404295b3e1280bb889d60bb7a6ff
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-11-30 22:49:47 +0500

    refactor(sdk): rework createMountFn and re-use signalifyAsyncFn

--- packages/sdk/src/scopes/createMountFn.ts
+++ packages/sdk/src/scopes/createMountFn.ts
@@ -10,90 +10,45 @@ import { signalifyAsyncFn } from '@/scopes/signalifyAsyncFn.js';
 
 /**
  * Creates a mount function for a component.
- * @param component - the component name.
- * @param mount - function mounting the component.
- * @param onMounted - callback which will be called with the mount result.
- * @param isMounted - signal containing mount state.
- * @param promise
- * @param error
+ * @param component - the component name
+ * @param mount - function mounting the component
+ * @param onMounted - function that will be called whenever mount was completed.
+ * @param isMounted - signal containing the current mount completion state
+ * @param data - signal containing the current mount state
+ * @param promise - signal containing the mount promise
+ * @param error - signal containing the mount error
  */
 // #__NO_SIDE_EFFECTS__
 export function createMountFn<R>(
   component: string,
-  mount: (options?: AsyncOptions) => CancelablePromise<R>,
+  mount: (options?: AsyncOptions) => R | CancelablePromise<R>,
+  onMounted: (result: R) => void,
   isMounted: Signal<boolean>,
+  data: Signal<R>,
   promise: Signal<CancelablePromise<R> | undefined>,
   error: Signal<Error | undefined>,
-) {
-  return signalifyAsyncFn(
+): (options?: AsyncOptions) => CancelablePromise<R> {
+  const noConcurrent = signalifyAsyncFn(
     mount,
-    (): TypedError<any> => {
-      return new TypedError(
-        ERR_ALREADY_MOUNTING,
-        `The ${component} component is already mounting`,
-      );
-    },
-    isMounted,
+    () => new TypedError(
+      ERR_ALREADY_MOUNTING,
+      `The ${component} component is already mounting`,
+    ),
     promise,
     error,
   );
-}
 
-/**
- * Creates a mount function for a component.
- * @param component - the component name.
- * @param mount - function mounting the component.
- * @param onMounted - callback which will be called with the mount result.
- * @param mountPromise - signal containing mount promise.
- * @param isMounted - signal containing mount state.
- * @param mountError - signal containing mount error.
- */
-// #__NO_SIDE_EFFECTS__
-export function createMountFn2<T = void>(
-  component: string,
-  mount: (options: AsyncOptions) => (T | CancelablePromise<T>),
-  onMounted: (result: T) => void,
-  {
-    isMounted,
-    mountPromise,
-    mountError,
-  }: {
-    isMounted: Signal<boolean>,
-    mountPromise: Signal<Promise<void>>,
-    mountError: Signal<Error | undefined>,
-  },
-): (options?: AsyncOptions) => CancelablePromise<void> {
-  return mountOptions => {
-    return CancelablePromise.withFn(async abortSignal => {
-      if (isMounted()) {
-        return;
+  return (options?) => {
+    return CancelablePromise.resolve().then(async () => {
+      if (!isMounted()) {
+        const result = await noConcurrent(options);
+        batch(() => {
+          data.set(result);
+          isMounted.set(true);
+          onMounted(result);
+        });
       }
-      if (isMounting()) {
-        throw new TypedError(
-          ERR_ALREADY_MOUNTING,
-          `The ${component} component is already mounting`,
-        );
-      }
-      isMounting.set(true);
-
-      let result: [completed: true, result: T] | [completed: false, err: Error];
-      try {
-        result = [true, await mount({ abortSignal })];
-      } catch (e) {
-        result = [false, e as Error];
-      }
-      batch(() => {
-        isMounting.set(false);
-        isMounted.set(true);
-
-        if (result[0]) {
-          onMounted(result[1]);
-        } else {
-          const error = result[1];
-          mountError.set(error);
-          throw error;
-        }
-      });
-    }, mountOptions);
+      return data();
+    });
   };
 }
