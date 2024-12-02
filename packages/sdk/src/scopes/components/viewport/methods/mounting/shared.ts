da8c5546f4aa224d1859feae13e2a95f68a9a275
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-02 23:54:00 +0500

    fix(sdk/viewport): fix invalid code in onFullscreenChanged

--- packages/sdk/src/scopes/components/viewport/methods/mounting/shared.ts
+++ packages/sdk/src/scopes/components/viewport/methods/mounting/shared.ts
@@ -1,6 +1,5 @@
 import type { EventListener } from '@telegram-apps/bridge';
 
-import { isFullscreen } from '../../signals/fullscreen.js';
 import { setState } from '../../signals/state.js';
 
 export const onViewportChanged: EventListener<'viewport_changed'> = (data) => {
@@ -14,5 +13,5 @@ export const onViewportChanged: EventListener<'viewport_changed'> = (data) => {
 };
 
 export const onFullscreenChanged: EventListener<'fullscreen_changed'> = (data) => {
-  isFullscreen.set(data.is_fullscreen);
+  setState({ isFullscreen: data.is_fullscreen });
 };
