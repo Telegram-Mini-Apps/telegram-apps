e3d5c5e4f0bcdd45686072a6ff594de3c4b51062
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-03 02:10:34 +0500

    feat(sdk): implement requestEmojiStatusAccess

--- packages/sdk/src/scopes/utilities/privacy/exports.ts
+++ packages/sdk/src/scopes/utilities/privacy/exports.ts
@@ -1,3 +1,9 @@
 export { requestContact, type RequestedContact } from './requestContact.js';
+export {
+  requestEmojiStatusAccess,
+  requestEmojiStatusAccessError,
+  requestEmojiStatusAccessPromise,
+  isRequestingEmojiStatusAccess,
+} from './requestEmojiStatusAccess.js';
 export { requestPhoneAccess, isRequestingPhoneAccess } from './requestPhoneAccess.js';
 export { requestWriteAccess, isRequestingWriteAccess } from './requestWriteAccess.js';
