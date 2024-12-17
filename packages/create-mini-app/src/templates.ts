5011c22bae77263c2b784553ccfcf78a5dee6540
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-17 16:12:37 +0500

    refactor(create-mini-app): refactor templates.ts a bit

--- packages/create-mini-app/src/templates.ts
+++ packages/create-mini-app/src/templates.ts
@@ -1,16 +1,12 @@
-import type { Template, Framework, Language, SDK, TemplateRepository } from './types.js';
+import type { Template, Framework, Language, SDK } from './types.js';
 
-function createRepository(name: string): TemplateRepository {
-  return {
-    clone: {
-      https: `https://github.com/Telegram-Mini-Apps/${name}.git`,
-      ssh: `git@github.com:Telegram-Mini-Apps/${name}.git`,
-    },
-    link: `github.com/Telegram-Mini-Apps/${name}`,
+const templates: {
+  [lang in Language]?: {
+    [sdk in SDK]?: {
+      [framework in Framework]?: string;
+    };
   };
-}
-
-const settings = {
+} = {
   js: {
     telegramApps: {
       react: 'reactjs-js-template',
@@ -42,44 +38,30 @@ const settings = {
       jquery: 'typescript-tsdk-template',
     },
   },
-} as const;
-
-/**
- * A list of the templates known to the CLI.
- */
-const templates: Template[] = [];
-
-for (const language in settings) {
-  const languageSettings = settings[language as keyof typeof settings];
-
-  for (const sdk in languageSettings) {
-    const frameworkSettings = languageSettings[sdk as keyof typeof languageSettings];
-
-    for (const framework in frameworkSettings) {
-      templates.push({
-        framework: framework as Framework,
-        sdk: sdk as SDK,
-        language: language as Language,
-        repository: createRepository(frameworkSettings[framework as keyof typeof frameworkSettings]),
-      });
-    }
-  }
-}
+};
 
 /**
  * Leaves only templates with specified technologies.
- * @param language
- * @param sdk
- * @param framework
+ * @param language - programming language.
+ * @param sdk - SDK to use as the base one.
+ * @param framework - target framework.
  */
 export function findTemplate(
   language: Language,
   sdk: SDK,
   framework: Framework,
 ): Template | undefined {
-  return templates.find((t) => (
-    t.sdk === sdk
-    && t.language === language
-    && t.framework === framework
-  ));
+  const repo = templates[language]?.[sdk]?.[framework];
+  return repo ? {
+    sdk,
+    language,
+    framework,
+    repository: {
+      clone: {
+        https: `https://github.com/Telegram-Mini-Apps/${repo}.git`,
+        ssh: `git@github.com:Telegram-Mini-Apps/${repo}.git`,
+      },
+      link: `github.com/Telegram-Mini-Apps/${repo}`,
+    },
+  } : undefined;
 }
