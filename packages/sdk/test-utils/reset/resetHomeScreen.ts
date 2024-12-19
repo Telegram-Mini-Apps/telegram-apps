a19a68207c3020142eca331c3c7bb745e665d64d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-12-20 01:47:15 +0500

    tests(sdk): fix resetHomeScreen

--- packages/sdk/test-utils/reset/resetHomeScreen.ts
+++ packages/sdk/test-utils/reset/resetHomeScreen.ts
@@ -2,7 +2,7 @@ import {
   isCheckingHomeScreenStatus,
   checkHomeScreenStatusError,
   checkHomeScreenStatusPromise,
-} from '@/scopes/utilities/home-screen/checkHomeScreenStatus.js';
+} from '@/scopes/utilities/home-screen/home-screen.js';
 import { resetSignal } from '@test-utils/reset/reset.js';
 
 export function resetHomeScreen() {
