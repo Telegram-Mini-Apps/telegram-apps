89f3711f07b350855a122904abd6c69358585c45
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-08 19:45:00 +0500

    chore(toolkit): remove async and replace with better-promises

--- packages/toolkit/src/sleep.ts
+++ packages/toolkit/src/sleep.ts
@@ -1,4 +1,4 @@
-import { CancelablePromise } from '@/async/promises/CancelablePromise.js';
+import { CancelablePromise } from 'better-promises';
 
 /**
  * Awaits for specified amount of time.
@@ -7,6 +7,7 @@ import { CancelablePromise } from '@/async/promises/CancelablePromise.js';
  */
 export function sleep(duration: number, abortSignal?: AbortSignal): CancelablePromise<void> {
   return new CancelablePromise(r => {
+    // eslint-disable-next-line @typescript-eslint/no-implied-eval
     setTimeout(r, duration);
   }, { abortSignal });
 }
