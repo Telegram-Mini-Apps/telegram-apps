d1e574003322ba198638f9256686e33174ccfc20
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-09 12:32:27 +0500

    refactor(transformers): remove complex and validators folders

--- packages/transformers/src/validators/isRGB.ts
+++
@@ -1,9 +0,0 @@
-import type { RGB } from '@telegram-apps/types';
-
-/**
- * Returns true in case, passed value has #RRGGBB format.
- * @param value - value to check.
- */
-export function isRGB(value: string): value is RGB {
-  return /^#[\da-f]{6}$/i.test(value);
-}
--- packages/transformers/src/validators/isRecord.ts
+++
@@ -1,7 +0,0 @@
-/**
- * States that passed value is Record and not Array.
- * @param value - value to check.
- */
-export function isRecord(value: unknown): value is Record<string, unknown> {
-  return !!value && typeof value === 'object' && !Array.isArray(value);
-}
