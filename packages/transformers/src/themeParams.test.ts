28d249390ef71263b04e1bee3866b58b95a3d22e
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-11 17:08:00 +0500

    refactor(transformers): rework theme params transformers

--- packages/transformers/src/themeParams.test.ts
+++ packages/transformers/src/themeParams.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from 'vitest';
 import { is, parse } from 'valibot';
 
-import { ThemeParams, ThemeParamsCamelCased } from '@/theme-params.js';
+import { themeParams } from '@/themeParams.js';
 
-describe('ThemeParams', () => {
+describe('themeParams', () => {
   it('should properly validate the value', () => {
-    expect(is(ThemeParams, {})).toBe(true);
-    expect(is(ThemeParams, {
+    expect(is(themeParams(), {})).toBe(true);
+    expect(is(themeParams(), {
       accent_text_color: '#6ab3f2',
       bg_color: '#17212b',
       bottom_bar_bg_color: '#17212b',
@@ -23,15 +23,12 @@ describe('ThemeParams', () => {
       subtitle_text_color: '#708599',
       text_color: '#f5f5f5',
     })).toBe(true);
-    expect(is(ThemeParams, { accent_text_color: '#aabbc' })).toBe(false);
-    expect(is(ThemeParams, 'aaa')).toBe(false);
+    expect(is(themeParams(), { accent_text_color: '#aabbc' })).toBe(false);
+    expect(is(themeParams(), 'aaa')).toBe(false);
   });
-});
 
-describe('ThemeParams', () => {
-  it('should properly validate the value', () => {
-    expect(is(ThemeParamsCamelCased, {})).toBe(true);
-    expect(is(ThemeParamsCamelCased, {
+  it('should camelize keys if true passed', () => {
+    const input = {
       accent_text_color: '#6ab3f2',
       bg_color: '#17212b',
       bottom_bar_bg_color: '#17212b',
@@ -47,31 +44,8 @@ describe('ThemeParams', () => {
       section_separator_color: '#111921',
       subtitle_text_color: '#708599',
       text_color: '#f5f5f5',
-    })).toBe(true);
-    expect(is(ThemeParamsCamelCased, { accent_text_color: '#aabbc' })).toBe(false);
-    expect(is(ThemeParamsCamelCased, 'aaa')).toBe(false);
-  });
-
-  it('should camelize keys', () => {
-    expect(
-      parse(ThemeParamsCamelCased, {
-        accent_text_color: '#6ab3f2',
-        bg_color: '#17212b',
-        bottom_bar_bg_color: '#17212b',
-        button_color: '#5289c1',
-        button_text_color: '#ffffff',
-        destructive_text_color: '#ec3942',
-        header_bg_color: '#17212b',
-        hint_color: '#708599',
-        link_color: '#6ab3f3',
-        secondary_bg_color: '#232e3c',
-        section_bg_color: '#17212b',
-        section_header_text_color: '#6ab3f3',
-        section_separator_color: '#111921',
-        subtitle_text_color: '#708599',
-        text_color: '#f5f5f5',
-      }),
-    ).toStrictEqual({
+    };
+    const inputCamelized = {
       accentTextColor: '#6ab3f2',
       bgColor: '#17212b',
       bottomBarBgColor: '#17212b',
@@ -87,6 +61,10 @@ describe('ThemeParams', () => {
       sectionSeparatorColor: '#111921',
       subtitleTextColor: '#708599',
       textColor: '#f5f5f5',
-    });
+    };
+
+    expect(parse(themeParams(), input)).not.toStrictEqual(inputCamelized);
+    expect(parse(themeParams(false), input)).not.toStrictEqual(inputCamelized);
+    expect(parse(themeParams(true), input)).toStrictEqual(inputCamelized);
   });
 });
