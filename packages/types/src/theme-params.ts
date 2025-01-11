8c4facad50c17a9e599852f5bc630b44a98b19fc
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-11 16:52:53 +0500

    fix(types): rename ThemeParams properties to their original names. Provide camel-cased alternative

--- packages/types/src/theme-params.ts
+++ packages/types/src/theme-params.ts
@@ -1,26 +1,27 @@
+import type { SnakeToCamelCase } from '@telegram-apps/toolkit';
+
 import type { RGB } from './colors.js';
 
-export type ThemeParamsKey =
-  | 'accentTextColor'
-  | 'bgColor'
-  | 'buttonColor'
-  | 'buttonTextColor'
-  | 'bottomBarBgColor'
-  | 'destructiveTextColor'
-  | 'headerBgColor'
-  | 'hintColor'
-  | 'linkColor'
-  | 'secondaryBgColor'
-  | 'sectionBgColor'
-  | 'sectionHeaderTextColor'
-  | 'sectionSeparatorColor'
-  | 'subtitleTextColor'
-  | 'textColor';
+type KnownThemeParamsKey =
+  | 'accent_text_color'
+  | 'bg_color'
+  | 'button_color'
+  | 'button_text_color'
+  | 'bottom_bar_bg_color'
+  | 'destructive_text_color'
+  | 'header_bg_color'
+  | 'hint_color'
+  | 'link_color'
+  | 'secondary_bg_color'
+  | 'section_bg_color'
+  | 'section_header_text_color'
+  | 'section_separator_color'
+  | 'subtitle_text_color'
+  | 'text_color';
 
 /**
  * Application [theme parameters](https://docs.telegram-mini-apps.com/platform/theming).
  * Defines palette used by the Telegram application.
  */
-export interface ThemeParams {
-  [key: ThemeParamsKey | string]: RGB;
-}
+export type ThemeParams = Record<KnownThemeParamsKey | string, RGB>;
+export type ThemeParamsCamelCased = Record<SnakeToCamelCase<KnownThemeParamsKey> | string, RGB>
