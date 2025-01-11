e6cf8d21365251df59855cd9dfb8ad890fe972e0
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-11 17:39:30 +0500

    feat(transformers): refactor init data validators. Implement serializeInitDataQuery

--- packages/transformers/src/init-data.ts
+++ packages/transformers/src/init-data.ts
@@ -9,15 +9,12 @@ import {
   date,
   integer,
   type BaseSchema,
+  type InferOutput,
 } from 'valibot';
 import type { InitData } from '@telegram-apps/types';
-import {
-  type ConvertCamelKeysToSnakeCase,
-  deepSnakeToCamelObjKeys,
-  type SnakeToCamelCase,
-} from '@telegram-apps/toolkit';
 
 import { query, queryToRecord, transformUsing, jsonParse } from '@/transformers.js';
+import { serializeToQuery } from '@/serializeToQuery.js';
 
 const User = pipe(
   string(),
@@ -72,23 +69,14 @@ export const InitDataQuery = pipe(
     user: optional(User),
   } satisfies {
     // Making sure, we specified transformers for all init data properties.
-    [K in keyof ConvertCamelKeysToSnakeCase<InitData>]-?: BaseSchema<
-      string | undefined,
-      Date extends InitData[SnakeToCamelCase<K>]
-        ? InitData[SnakeToCamelCase<K>]
-        : ConvertCamelKeysToSnakeCase<InitData[SnakeToCamelCase<K>]>,
-      any
-    >;
+    [K in keyof InitData]-?: BaseSchema<string | undefined, InitData[K], any>;
   })),
-  // Adjust the output type as long as looseObject breaks it a bit.
-  transform(v => v as typeof v & Record<string, string | string[]>),
 );
 
 /**
- * Transformer extracting init data in its initial format from the query parameters.
- * The result will be deeply camel-cased.
+ * Serializes the InitDataQuery shape.
+ * @param value - value to serialize.
  */
-export const InitDataQueryCamelCased = pipe(
-  InitDataQuery,
-  transform(deepSnakeToCamelObjKeys),
-);
+export function serializeInitDataQuery(value: InferOutput<typeof InitDataQuery>): string {
+  return serializeToQuery(value);
+}
