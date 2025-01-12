7d7fb8a9173faed27d73a33887ae53912ff57bfe
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-12 23:06:03 +0500

    refactor(bridge): rework debug section

--- packages/bridge/src/debug.ts
+++ packages/bridge/src/debug.ts
@@ -1,5 +1,7 @@
-import { signal } from '@telegram-apps/signals';
 import { createLogger } from '@telegram-apps/toolkit';
+
+import type { SubscribeListener } from '@/events/types/index.js';
+import { off, on } from '@/events/emitter.js';
 
 /**
  * The package debug mode.
@@ -7,10 +9,27 @@ import { createLogger } from '@telegram-apps/toolkit';
  * Enabling debug mode leads to printing additional messages in the console related to the
  * processes inside the package.
  */
-export const $debug = signal(false);
+let debug = false;
 
 export const [logInfo, logError] = createLogger('Bridge', {
   bgColor: '#9147ff',
   textColor: 'white',
-  shouldLog: $debug,
+  shouldLog() {
+    return debug;
+  },
 });
+
+const listener: SubscribeListener = event => {
+  logInfo(false, 'Event received:', event);
+};
+
+/**
+ * Sets the package debug mode leading to outputting additional logs.
+ * @param value - enable debug mode.
+ */
+export function setDebug(value: boolean): void {
+  if (value !== debug) {
+    debug = value;
+    debug ? on('*', listener) : off('*', listener);
+  }
+}
