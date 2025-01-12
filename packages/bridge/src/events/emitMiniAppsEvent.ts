32ba912401d6256e4926917542d0d30ab6c417ce
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-12 21:22:23 +0500

    feat(bridge): enhance emitMiniAppsEvent typings

--- packages/bridge/src/events/emitMiniAppsEvent.ts
+++ packages/bridge/src/events/emitMiniAppsEvent.ts
@@ -1,3 +1,59 @@
+import type { EventName, EventPayload } from '@/events/types/index.js';
+import type { If, IsNever } from '@telegram-apps/toolkit';
+
+type EventsWithoutPayload = {
+  [E in EventName]: If<IsNever<EventPayload<E>>, never, E>
+}[EventName];
+
+type EventsWithRequiredPayload = {
+  [E in EventName]: undefined extends EventPayload<E> ? never : E
+}[EventName];
+
+/**
+ * Emits an event without payload sent from the Telegram native application like it was sent in
+ * a default web environment between two iframes.
+ *
+ * It dispatches a new MessageEvent and expects it to be handled via
+ * the `window.addEventListener('message', ...)` call, as a developer would do it to handle
+ * messages sent from the parent iframe.
+ * @param eventType - event name.
+ */
+export function emitMiniAppsEvent<E extends EventsWithoutPayload>(eventType: E): void;
+
+/**
+ * Emits an event with payload sent from the Telegram native application like it was sent in
+ * a default web environment between two iframes.
+ *
+ * It dispatches a new MessageEvent and expects it to be handled via
+ * the `window.addEventListener('message', ...)` call, as a developer would do it to handle
+ * messages sent from the parent iframe.
+ * @param eventType - event name.
+ * @param eventData - event payload.
+ */
+export function emitMiniAppsEvent<E extends EventsWithRequiredPayload>(
+  eventType: E,
+  eventData: EventPayload<E>,
+): void;
+
+/**
+ * Emits an unknown event sent from the Telegram native application like it was sent in a default
+ * web environment between two iframes.
+ *
+ * It dispatches a new MessageEvent and expects it to be handled via
+ * the `window.addEventListener('message', ...)` call, as a developer would do it to handle
+ * messages sent from the parent iframe.
+ * @param eventType - event name.
+ * @param eventData - event payload.
+ */
+export function emitMiniAppsEvent<E extends string>(
+  eventType: E,
+  eventData: E extends EventsWithoutPayload
+    ? never
+    : E extends EventsWithRequiredPayload
+      ? EventPayload<E>
+      : unknown,
+): void;
+
 /**
  * Emits an event sent from the Telegram native application like it was sent in a default web
  * environment between two iframes.
