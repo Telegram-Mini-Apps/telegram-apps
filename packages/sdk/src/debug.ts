67b16750d1657310ed337ffb6cac1f4360d1baa7
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-16 02:07:11 +0500

    fix(sdk): rework debug section

--- packages/sdk/src/debug.ts
+++ packages/sdk/src/debug.ts
@@ -1,7 +1,27 @@
-import { $debug, createLogger } from '@telegram-apps/bridge';
+import { setDebug as setBridgeDebug } from '@telegram-apps/bridge';
+import { createLogger } from '@telegram-apps/toolkit';
+
+/**
+ * The package debug mode.
+ *
+ * Enabling debug mode leads to printing additional messages in the console related to the
+ * processes inside the package.
+ */
+let debug = false;
 
 export const [logInfo, logError] = createLogger('SDK', {
   bgColor: 'forestgreen',
   textColor: 'white',
-  shouldLog: $debug,
+  shouldLog() {
+    return debug;
+  },
 });
+
+/**
+ * Sets the package debug mode leading to outputting additional logs.
+ * @param value - enable debug mode.
+ */
+export function setDebug(value: boolean): void {
+  debug = value;
+  setBridgeDebug(value);
+}
