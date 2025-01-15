943fae8a1203bb876fc58090de46b2b4bda7ca7d
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-16 02:09:56 +0500

    refactor(sdk): refactor the wrapSafe function. Add more comments, simplify some types and functions

--- packages/sdk/src/scopes/toolkit/wrapSafe.ts
+++ packages/sdk/src/scopes/toolkit/wrapSafe.ts
@@ -1,14 +1,13 @@
 import {
   ERR_UNKNOWN_ENV,
-  type If,
   type MethodName,
   supports,
   isTMA,
-  TypedError,
   type MethodNameWithVersionedParams,
-  type MethodVersionedParams, IsNever,
+  type MethodVersionedParams,
 } from '@telegram-apps/bridge';
 import { type Computed, computed } from '@telegram-apps/signals';
+import { type If, type IsNever, TypedError } from '@telegram-apps/toolkit';
 
 import { $version } from '@/scopes/globals.js';
 import {
@@ -19,47 +18,48 @@ import {
 import { isSSR } from '@/utils/isSSR.js';
 import type { AnyFn } from '@/types.js';
 
-type HasCheckSupportMethodTuple<FnArgs extends any[]> = {
+export type CustomSupportValidatorFn = () => string | undefined;
+
+export type IsSupportedType =
+  | MethodName
+  | CustomSupportValidatorFn
+  | (MethodName | CustomSupportValidatorFn)[]
+  | { any: (MethodName | CustomSupportValidatorFn)[] };
+
+/**
+ * A map where the key is a method name with versioned parameters, and the value is a tuple
+ * containing the method and parameter names. The third tuple value is a function accepting
+ * the wrapped function arguments and returning true if support check must be applied.
+ */
+export type Supports<Fn extends AnyFn> = Record<string, {
   [M in MethodNameWithVersionedParams]: [
     method: M,
-    parameter: MethodVersionedParams<M>,
-    shouldCheck: (...args: FnArgs) => boolean,
-  ]
-}[MethodNameWithVersionedParams];
+    param: MethodVersionedParams<M>,
+    shouldCheck: (...args: Parameters<Fn>) => boolean,
+  ];
+}[MethodNameWithVersionedParams]>;
 
-interface CustomSupportValidator {
-  fn: () => boolean;
-  error: string;
-}
-
-export type IsSupported =
-  | MethodName
-  | CustomSupportValidator
-  | (MethodName | CustomSupportValidator)[]
-  | { any: (MethodName | CustomSupportValidator)[] };
-
-export type Supports<Fn extends AnyFn> = Record<string, HasCheckSupportMethodTuple<Parameters<Fn>>>
+export type IfAvailableFnResult<Data> = [called: true, data: Data] | [called: false];
 
 export type SafeWrapped<
   Fn extends AnyFn,
-  Supported extends boolean,
+  HasSupportCheck extends boolean,
   SupportsSchema extends Record<string, any>
 > =
   & Fn
   & {
   /**
-   * The signal returning `true` if the function is available in the
-   * current environment.
+   * The signal returning `true` if the function is available in the current environment and
+   * conditions.
    *
    * To be more accurate, the method checks the following:
-   * 1. The current environment is not TMA or server.
+   * 1. The current environment is Telegram Mini Apps.
    * 2. The SDK package is initialized.
    * 3. If passed, the `isSupported` signal returns true.
    * 4. If passed, the `isMounted` signal returns true.
    *
-   * *You should use this function when possible because it provides
-   * must-have code security mechanisms and makes a developer sure that
-   * he is using the package properly.*
+   * *You should use this function when possible because it provides must-have code security
+   * mechanisms and makes a developer sure that he is using the package properly.*
    *
    * @returns True if the function is available in the current environment.
    * @example
@@ -70,31 +70,31 @@ export type SafeWrapped<
   isAvailable: Computed<boolean>;
   /**
    * Calls the function only in case it is available.
+   *
+   * It uses the `isAvailable` internally to check if the function is supported.
    * @example
    * showBackButton.ifAvailable();
    */
-  ifAvailable(...args: Parameters<Fn>): ReturnType<Fn> | undefined;
+  ifAvailable(...args: Parameters<Fn>): IfAvailableFnResult<ReturnType<Fn>>;
 }
-  & If<Supported, {
+  & If<HasSupportCheck, {
   /**
-   * The signal returning `true` if the function is supported by
-   * the current Mini Apps version including some possible additional
-   * conditions.
+   * The signal returning `true` if the function is supported by the Telegram client,
+   * including some possible additional conditions.
    *
-   * It is highly recommended to use this signal only in certain narrow cases
-   * when only the function support check is required.
+   * It is highly recommended to use this signal only in certain narrow cases when only the
+   * function support check is required, but not its availability.
    *
-   * This signal is not applying additional operations like checking if the
-   * current environment is Mini Apps and the SDK is initialized.
+   * This signal is not applying additional operations like checking if the current environment
+   * is Mini Apps and the SDK is initialized.
    *
-   * To check if the function is available for use, use the `isAvailable`
-   * signal.
+   * To check if the function is available for use, use the `isAvailable` signal.
    *
    * @returns True if this function is supported.
    * @see isAvailable
    * @example
    * if (setMiniAppBottomBarColor.isSupported()) {
-   *   console.log('Mini App bottom bar is supported, but the function is still probably unavailable');
+   *   console.log('Mini App bottom bar is supported, but the function may be unavailable');
    * }
    */
   isSupported: Computed<boolean>;
@@ -115,120 +115,145 @@ export type SafeWrapped<
   supports: Record<keyof SupportsSchema, Computed<boolean>>
 }>
 
-interface Options<Fn extends AnyFn> {
+export interface WrapSafeOptions<Fn extends AnyFn> {
+  /**
+   * The component name owning the wrapped function.
+   */
   component?: string;
+  /**
+   * Signal returning true if the owning component is mounted.
+   */
   isMounted?: () => boolean;
-  isSupported?: IsSupported;
+  /**
+   * Signal returning true if the owning component is mounting.
+   */
+  isMounting?: () => boolean;
+  /**
+   * Value determining if the function is supported by the current environment.
+   */
+  isSupported?: IsSupportedType;
+  /**
+   * A map where the key is a method name with versioned parameters, and the value is a tuple
+   * containing the method and parameter names. The third tuple value is a function accepting
+   * the wrapped function arguments and returning true if support check must be applied.
+   */
   supports?: Supports<Fn>,
 }
 
-export function wrapSafe<Fn extends AnyFn>(
-  method: string,
-  fn: Fn,
-): SafeWrapped<Fn, false, never>;
-
-export function wrapSafe<Fn extends AnyFn, O extends Options<Fn>>(
+/**
+ * Wraps the function enhancing it with the useful utilities described in the SafeWrapped type.
+ * @see SafeWrapped
+ * @param method - method name
+ * @param fn - wrapped function
+ */
+export function wrapSafe<Fn extends AnyFn>(method: string, fn: Fn): SafeWrapped<Fn, false, never>;
+/**
+ * Wraps the function enhancing it with the useful utilities described in the SafeWrapped type.
+ * @see SafeWrapped
+ * @param method - method name
+ * @param fn - wrapped function
+ * @param options - additional options
+ */
+export function wrapSafe<Fn extends AnyFn, O extends WrapSafeOptions<Fn>>(
   method: string,
   fn: Fn,
   options: O,
 ): SafeWrapped<
   Fn,
   O extends { isSupported: any } ? true : false,
-  O extends { supports: infer S extends Record<string, any> } ? S : never
+  O extends { supports: any } ? O['supports'] : never
 >
-
 /*@__NO_SIDE_EFFECTS__*/
 export function wrapSafe<Fn extends AnyFn>(
   method: string,
   fn: Fn,
-  { isSupported, isMounted, component, supports: supportsOption }: Options<Fn> = {},
+  options?: WrapSafeOptions<Fn>,
 ): SafeWrapped<Fn, boolean, Record<string, any> | never> {
-  // Full function identifier.
-  const fullName = `${component ? `${component}.` : ''}${method}()`;
+  options ||= {};
+  const {
+    isSupported: optionsIsSupported,
+    isMounted,
+    isMounting,
+    component,
+    supports: optionSupports,
+  } = options || {};
+
+  const functionId = `${component ? `${component}.` : ''}${method}()`;
 
   // Simplify the isSupported value to work with an array of validators or a single object.
-  isSupported = isSupported
-    ? Array.isArray(isSupported)
+  const isSupported = optionsIsSupported
+    ? Array.isArray(optionsIsSupported)
       // (MethodName | CustomSupportValidator)[]
-      ? isSupported
-      : typeof isSupported === 'object' && 'any' in isSupported
+      ? optionsIsSupported
+      : typeof optionsIsSupported === 'object' && 'any' in optionsIsSupported
         // { any: (MethodName | CustomSupportValidator)[] }
-        ? isSupported
+        ? optionsIsSupported
         // MethodName | CustomSupportValidator
-        : [isSupported]
+        : [optionsIsSupported]
     : undefined;
 
   /**
    * @returns True if the specified option is supported.
    * @param option - option name.
    */
-  function isOptionSupported(option: string): boolean {
-    if (supportsOption) {
-      const tuple = supportsOption[option];
+  function supportsOption(option: string): boolean {
+    if (optionSupports) {
+      const tuple = optionSupports[option];
       return supports(tuple[0], tuple[1], $version());
     }
     return true;
   }
 
   /**
-   * @returns An error related to isSupported() check.
+   * @returns All found errors according to the isSupported variable value.
    */
-  const supportError = (): string | undefined => {
-    // isSupported was not specified. In this case we assume that the function has no dependencies
-    // and is always supported.
+  function supportErrors(): string[] {
+    // isSupported was not specified.
+    // In this case, we assume that the function has no dependencies and is always supported.
     if (!isSupported) {
-      return;
+      return [];
     }
 
-    const defaultErr = `it is unsupported in Mini Apps version ${$version()}`;
-
-    function getError(item: MethodName | CustomSupportValidator): string | undefined {
-      return typeof item === 'string'
-        ? supports(item, $version())
+    function getError(item: MethodName | CustomSupportValidatorFn): string | undefined {
+      return typeof item === 'function'
+        ? item()
+        : supports(item, $version())
           ? undefined
-          : defaultErr
-        : item.fn()
-          ? undefined
-          : item.error;
+          : `it is unsupported in Mini Apps version ${$version()}`;
     }
 
-    // Should check each array item.
-    // (MethodName | CustomSupportValidator)[]
-    if (Array.isArray(isSupported)) {
-      for (const item of isSupported) {
-        const err = getError(item);
-        if (err) {
-          return err;
-        }
-      }
-      return;
-    }
+    const isSupportedItems = Array.isArray(isSupported) ? isSupported : isSupported.any;
+    const errors = isSupportedItems.map(getError).filter(Boolean) as string[];
 
-    // Should check if any item didn't return an error.
-    let lastErr: string | undefined;
-    for (const item of isSupported.any) {
-      lastErr = getError(item);
-      if (!lastErr) {
-        return;
-      }
-    }
-    return defaultErr;
-  };
+    return Array.isArray(isSupported)
+      // An array is passed.
+      // Should return all found errors.
+      ? errors
+      // An object with the "any" property is passed.
+      // Should return nothing if at least one item didn't return an error.
+      : errors.length === isSupportedItems.length ? errors : [];
+  }
 
   /**
    * @returns An error related to supports.<name> check.
    */
-  const supportsOptionError = (...args: Parameters<Fn>): string | undefined => {
-    if (supportsOption) {
-      for (const k in supportsOption) {
-        if (supportsOption[k][2](...args) && !isOptionSupported(k)) {
-          return `option ${k} is not supported in Mini Apps version ${$version()}`;
-        }
+  function supportsOptionError(...args: Parameters<Fn>): string | undefined {
+    for (const k in optionSupports) {
+      if (optionSupports[k][2](...args) && !supportsOption(k)) {
+        return `option ${k} is not supported in Mini Apps version ${$version()}`;
       }
     }
-  };
+  }
 
-  const $isSupported = computed(() => !supportError());
+  let supportsMap: Record<string, Computed<boolean>> | undefined;
+  if (optionSupports) {
+    supportsMap = {};
+    for (const option in optionSupports) {
+      supportsMap[option] = computed(() => supportsOption(option));
+    }
+  }
+
+  const $isSupported = computed(() => !supportErrors());
   const $isInitialized = computed(() => $version() !== '0.0');
   const $isMounted = computed(() => !isMounted || isMounted());
   const $isAvailable = computed(
@@ -239,17 +264,9 @@ export function wrapSafe<Fn extends AnyFn>(
       && $isMounted(),
   );
 
-  let supportsMap: Record<string, Computed<boolean>> | undefined;
-  if (supportsOption) {
-    supportsMap = {};
-    for (const option in supportsOption) {
-      supportsMap[option] = computed(() => isOptionSupported(option));
-    }
-  }
-
   return Object.assign(
     (...args: Parameters<Fn>): ReturnType<Fn> => {
-      const errMessagePrefix = `Unable to call the ${fullName} ${component ? 'method' : 'function'}:`;
+      const errMessagePrefix = `Unable to call the ${functionId} ${component ? 'method' : 'function'}:`;
 
       if (isSSR() || !isTMA('simple')) {
         throw new TypedError(
@@ -263,36 +280,30 @@ export function wrapSafe<Fn extends AnyFn>(
           `${errMessagePrefix} the SDK was not initialized. Use the SDK init() function`,
         );
       }
-      const supportErr = supportError();
-      if (supportErr) {
-        throw new TypedError(ERR_NOT_SUPPORTED, `${errMessagePrefix} ${supportErr}`);
+      const supportErr = supportErrors();
+      if (supportErr.length) {
+        throw new TypedError(ERR_NOT_SUPPORTED, `${errMessagePrefix} ${supportErr.join(' / ')}`);
       }
       const supportsOptionErr = supportsOptionError(...args);
       if (supportsOptionErr) {
         throw new TypedError(ERR_NOT_SUPPORTED, `${errMessagePrefix} ${supportsOptionErr}`);
       }
       if (!$isMounted()) {
-        throw new TypedError(
-          ERR_NOT_MOUNTED,
-          `${errMessagePrefix} the component is not mounted. Use the ${component}.mount() method`,
-        );
+        const message = isMounting && isMounting()
+          ? 'mounting. Wait for the mount completion'
+          : `unmounted. Use the ${component}.mount() method`;
+        throw new TypedError(ERR_NOT_MOUNTED, `${errMessagePrefix} the component is ${message}`);
       }
       return fn(...args);
     },
     fn,
     {
       isAvailable: $isAvailable,
-      ifAvailable(...args: Parameters<Fn>): ReturnType<Fn> | undefined {
-        return $isAvailable()
-          ? fn(...args)
-          : undefined;
+      ifAvailable(...args: Parameters<Fn>): IfAvailableFnResult<ReturnType<Fn>> {
+        return $isAvailable() ? [true, fn(...args)] : [false];
       },
     },
-    isSupported ? {
-      isSupported: $isSupported,
-    } : {},
-    supportsMap ? {
-      supports: supportsMap,
-    } : {},
+    isSupported ? { isSupported: $isSupported } : {},
+    supportsMap ? { supports: supportsMap } : {},
   );
 }
