75ba11b6c43b6dc9b5841deebe0572ec3aa0a283
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-20 21:46:00 +0500

    refactor(sdk): actualize the createMountFn code

--- packages/sdk/src/scopes/createMountFn.ts
+++ packages/sdk/src/scopes/createMountFn.ts
@@ -1,47 +1,42 @@
 import { batch, type Signal } from '@telegram-apps/signals';
-import {
-  type AsyncOptions,
-  CancelablePromise,
-  TypedError,
-} from '@telegram-apps/bridge';
+import { CancelablePromise, type PromiseOptions } from 'better-promises';
 
-import { ERR_ALREADY_MOUNTING } from '@/errors.js';
-import { signalifyAsyncFn } from '@/scopes/signalifyAsyncFn.js';
+import { ConcurrentCallError } from '@/errors.js';
+import { signalifyFn } from '@/scopes/signalifyFn.js';
 
 /**
  * Creates a mount function for a component.
  * @param component - the component name
  * @param mount - function mounting the component
  * @param onMounted - function that will be called whenever mount was completed.
- * @param isMounted - signal containing the current mount completion state
- * @param promise - signal containing the mount promise
- * @param error - signal containing the mount error
+ * @param $isMounted - signal containing the current mount completion state
+ * @param $promise - signal containing the mount promise
+ * @param $error - signal containing the mount error
  */
 // #__NO_SIDE_EFFECTS__
 export function createMountFn<R>(
   component: string,
-  mount: (options?: AsyncOptions) => R | CancelablePromise<R>,
+  mount: (options?: PromiseOptions) => R | CancelablePromise<R>,
   onMounted: (result: R) => void,
-  isMounted: Signal<boolean>,
-  promise: Signal<CancelablePromise<Awaited<R>> | undefined>,
-  error: Signal<Error | undefined>,
-): (options?: AsyncOptions) => CancelablePromise<void> {
-  const noConcurrent = signalifyAsyncFn(
+  $isMounted: Signal<boolean>,
+  $promise: Signal<CancelablePromise<R> | undefined>,
+  $error: Signal<Error | undefined>,
+): (options?: PromiseOptions) => CancelablePromise<void> {
+  const noConcurrent = signalifyFn(
     mount,
-    () => new TypedError(
-      ERR_ALREADY_MOUNTING,
-      `The ${component} component is already mounting`,
-    ),
-    promise,
-    error,
+    () => {
+      return new ConcurrentCallError(`The ${component} component is already mounting`);
+    },
+    $promise,
+    $error,
   );
 
   return (options?) => {
     return CancelablePromise.resolve().then(async () => {
-      if (!isMounted()) {
+      if (!$isMounted()) {
         const result = await noConcurrent(options);
         batch(() => {
-          isMounted.set(true);
+          $isMounted.set(true);
           onMounted(result);
         });
       }
