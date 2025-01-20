676e953d12dbd7e755b43a1f556d7e648b7c41b1
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-20 21:38:06 +0500

    feat(sdk): make createIsSupported use a signal creator from registry

--- packages/sdk/src/scopes/createIsSupported.ts
+++ packages/sdk/src/scopes/createIsSupported.ts
@@ -1,12 +1,13 @@
-import { type Computed, computed } from '@telegram-apps/signals';
+import { type Computed } from '@telegram-apps/signals';
 import { type MethodName, supports } from '@telegram-apps/bridge';
 
-import { $version } from '@/scopes/globals.js';
+import { version } from '@/globals.js';
+import { createComputed } from '@/signals-registry.js';
 
 /**
  * @returns A signal indicating if the specified Mini Apps method is supported.
  * @param method - Mini Apps method name
  */
 export function createIsSupported(method: MethodName): Computed<boolean> {
-  return computed(() => supports(method, $version()));
+  return createComputed(() => supports(method, version()));
 }
