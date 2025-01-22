f3ed6400a4bc9bc2baece6b11b9c0cd8fe11d2cb
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-22 21:42:02 +0500

    feat(sdk): allow passing external promise and error types in defineNonConcurrentFn

--- packages/sdk/src/scopes/defineNonConcurrentFn.ts
+++ packages/sdk/src/scopes/defineNonConcurrentFn.ts
@@ -1,7 +1,11 @@
 import { CancelablePromise } from 'better-promises';
 
 import { ConcurrentCallError } from '@/errors.js';
-import { defineAsyncFn, type DefineAsyncFnResult } from '@/scopes/defineAsyncFn.js';
+import {
+  defineAsyncFn,
+  type DefineAsyncFnResult,
+  type DefineAsyncOptions,
+} from '@/scopes/defineAsyncFn.js';
 
 /**
  * Uses the defineAsyncFn function and returning its result, but adds an additional check to
@@ -9,28 +13,28 @@ import { defineAsyncFn, type DefineAsyncFnResult } from '@/scopes/defineAsyncFn.
  * @param fn - a wrapped function.
  * @param errorMessage - a message to place in the ConcurrentCallError constructor if concurrent
  * call was performed.
+ * @param options - additional options.
  */
 export function defineNonConcurrentFn<Fn extends (...args: any) => any>(
   fn: Fn,
   errorMessage: string,
+  options?: DefineAsyncOptions<Awaited<ReturnType<Fn>>>,
 ): DefineAsyncFnResult<Fn> {
   const [
     asyncFn,
     [_promise, ...restPromise],
     [_error, error],
-  ] = defineAsyncFn(fn);
+  ] = defineAsyncFn(fn, options);
 
   return [
-    (...args) => {
-      return CancelablePromise.withFn(() => {
-        if (_promise()) {
-          const err = new ConcurrentCallError(errorMessage);
-          _error.set(err);
-          throw err;
-        }
-        return asyncFn(...args);
-      });
-    },
+    (...args) => CancelablePromise.withFn(() => {
+      if (_promise()) {
+        const err = new ConcurrentCallError(errorMessage);
+        _error.set(err);
+        throw err;
+      }
+      return asyncFn(...args);
+    }),
     [_promise, ...restPromise],
     [_error, error],
   ];
