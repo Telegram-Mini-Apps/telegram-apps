3cf1bf43f5a0f01b6346a680dcadd679f5a1e671
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-22 21:36:33 +0500

    feat(sdk): allow passing external promise and error types in defineAsyncFn

--- packages/sdk/src/scopes/defineAsyncFn.ts
+++ packages/sdk/src/scopes/defineAsyncFn.ts
@@ -1,20 +1,27 @@
 import { CancelablePromise } from 'better-promises';
-import { batch, type Computed, Signal } from '@telegram-apps/signals';
+import { batch, type Computed, type Signal } from '@telegram-apps/signals';
 
-import { createComputed, createSignalsTuple } from '@/signals-registry.js';
+import { createComputed, createSignalsTuple, SignalsTuple } from '@/signals-registry.js';
 
 export type DefineAsyncFnResult<Fn extends (...args: any) => any> = [
   fn: (...args: Parameters<Fn>) => CancelablePromise<Awaited<ReturnType<Fn>>>,
-  promiseTuple: [
-    _promise: Signal<CancelablePromise<Awaited<ReturnType<Fn>>> | undefined>,
-    promise: Computed<CancelablePromise<Awaited<ReturnType<Fn>>> | undefined>,
+  promise: [
+    ...SignalsTuple<CancelablePromise<Awaited<ReturnType<Fn>>> | undefined>,
     isRequesting: Computed<boolean>,
   ],
-  errorTuple: [
-    _error: Signal<Error | undefined>,
-    error: Computed<Error | undefined>,
-  ],
+  error: SignalsTuple<Error | undefined>
 ];
+
+export interface DefineAsyncOptions<T> {
+  /**
+   * A signal with the promise to use instead of the generated one.
+   */
+  promise?: Signal<CancelablePromise<T> | undefined>;
+  /**
+   * A signal with the error to use instead of the generated one.
+   */
+  error?: Signal<Error | undefined>;
+}
 
 /**
  * Wraps the specified function preventing it from calling concurrently.
@@ -25,10 +32,25 @@ export type DefineAsyncFnResult<Fn extends (...args: any) => any> = [
  * 3. A signal with promise containing true if the wrapped function execution is currently in progress.
  * 4. A signal with promise containing the last call wrapped function execution error.
  * @param fn - a wrapped function.
+ * @param options - additional options.
  */
-export function defineAsyncFn<Fn extends (...args: any) => any>(fn: Fn): DefineAsyncFnResult<Fn> {
-  const [_promise, promise] = createSignalsTuple<CancelablePromise<Awaited<ReturnType<Fn>>> | undefined>();
-  const [_error, error] = createSignalsTuple<Error | undefined>();
+export function defineAsyncFn<Fn extends (...args: any) => any>(
+  fn: Fn,
+  options?: DefineAsyncOptions<Awaited<ReturnType<Fn>>>,
+): DefineAsyncFnResult<Fn> {
+  options ||= {};
+  const {
+    promise: optionsPromise,
+    error: optionsError,
+  } = options;
+  const [_promise, promise] =
+    optionsPromise
+      ? [optionsPromise, createComputed(optionsPromise)]
+      : createSignalsTuple<CancelablePromise<Awaited<ReturnType<Fn>>> | undefined>();
+  const [_error, error] =
+    optionsError
+      ? [optionsError, createComputed(optionsError)]
+      : createSignalsTuple<Error | undefined>();
 
   return [
     (...args: Parameters<Fn>): CancelablePromise<Awaited<ReturnType<Fn>>> => {
