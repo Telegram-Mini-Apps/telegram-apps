4a3dc274476c3f031b04431a0cbd88fc796ce3e5
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-01-26 21:15:40 +0500

    fix(sdk-*): remove useLaunchParams

--- packages/sdk-react/src/useLaunchParams.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { retrieveLaunchParams, type LaunchParams } from '@telegram-apps/sdk';
-
-/**
- * Retrieves Mini Apps launch parameters.
- */
-export function useLaunchParams(): LaunchParams {
-  return useMemo(retrieveLaunchParams, []);
-}
