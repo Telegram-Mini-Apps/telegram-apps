fc535f1c6e344f6878d21af363df38a13456c678
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-02-02 17:14:54 +0500

    feat(sdk): implement shareMessage utility

--- packages/sdk/src/scopes/utilities/uncategorized/exports.ts
+++ packages/sdk/src/scopes/utilities/uncategorized/exports.ts
@@ -1,5 +1,6 @@
 export { getCurrentTime } from './getCurrentTime.js';
 export { readTextFromClipboard } from './readTextFromClipboard.js';
 export { sendData } from './sendData.js';
+export { shareMessage } from './shareMessage.js';
 export { shareStory, type ShareStoryOptions } from './shareStory.js';
 export { switchInlineQuery } from './switchInlineQuery.js';
