bf6b64a9f0c18aad680eabb04d5fcf02437319ac
optimistiks <mail@potapovmax.com>
2025-02-14 20:16:32 +0300

    fix(sdk-react): make useSignal work with SSR

--- packages/sdk-react/src/useSignal.ts
+++ packages/sdk-react/src/useSignal.ts
@@ -3,10 +3,19 @@ import { useSyncExternalStore } from 'react';
 /**
  * Returns the underlying signal value updating it each time the signal value changes.
  * @param signal - a signal.
+ * @param getServerSnapshot - an optional function returning the signal value snapshot. It is used only during SSR
+ * to provide an initial value of the signal. When not set, defaults to the signal itself.
  */
-export function useSignal<T>(signal: {
-  (): T;
-  sub(fn: VoidFunction): VoidFunction;
-}): T {
-  return useSyncExternalStore((onStoreChange) => signal.sub(onStoreChange), signal);
+export function useSignal<T>(
+  signal: {
+    (): T;
+    sub(fn: VoidFunction): VoidFunction;
+  },
+  getServerSnapshot?: () => T
+): T {
+  return useSyncExternalStore(
+    (onStoreChange) => signal.sub(onStoreChange),
+    signal,
+    getServerSnapshot || signal
+  );
 }
