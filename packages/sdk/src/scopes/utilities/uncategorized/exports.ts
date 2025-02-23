e8ac0a38ed1d9697639b4588b83d7bf327156c6a
Vladislav Kibenko <wolfram.deus@gmail.com>
2025-02-23 16:26:54 +0500

    feat(sdk): implement copyTextToClipboard and downloadFile

--- packages/sdk/src/scopes/utilities/uncategorized/exports.ts
+++ packages/sdk/src/scopes/utilities/uncategorized/exports.ts
@@ -1,3 +1,5 @@
+export { copyTextToClipboard } from './copyTextToClipboard.js';
+export { downloadFile } from './downloadFile.js';
 export { getCurrentTime } from './getCurrentTime.js';
 export { readTextFromClipboard } from './readTextFromClipboard.js';
 export { sendData } from './sendData.js';
