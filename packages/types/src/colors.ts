905cf593c65cdbdd32cd5e4bf9b553aaf9fa380f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-02-27 19:47:51 +0500

    chore(types): remove RGBInt type

--- packages/types/src/colors.ts
+++ packages/types/src/colors.ts
@@ -4,11 +4,6 @@
 export type RGB = `#${string}`;
 
 /**
- * Color in int format -10177041.
- */
-export type RGBInt = number;
-
-/**
  * Color in format #RGB.
  */
 export type RGBShort = `#${string}`;
