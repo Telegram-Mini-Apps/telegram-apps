ecbd680a6e3da447fbda9dd355cf56d5e19d2d9b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-06-23 19:40:13 +0500

    feat(bridge): add more overrides for the decodeStartParam function

--- packages/bridge/src/start-param/decodeStartParam.ts
+++ packages/bridge/src/start-param/decodeStartParam.ts
@@ -1,7 +1,33 @@
 import { decodeBase64Url } from '@/base64-url/decodeBase64Url.js';
 
 /**
- * Decodes the start parameter.
+ * Decodes a start parameter using a custom parser.
+ * @param value - a start parameter value.
+ * @param parse - a custom value parser.
  * @see decodeBase64Url
  */
-export const decodeStartParam = decodeBase64Url;
+export function decodeStartParam<T>(value: string, parse: (value: string) => T): T;
+/**
+ * Decodes a start parameter assuming that the result is a JSON value.
+ * @param value - a start parameter value.
+ * @param as - result kind.
+ * @see decodeBase64Url
+ */
+export function decodeStartParam(value: string, as: 'json'): unknown;
+/**
+ * Decodes a start parameter and returns its decoded representation.
+ * @param value - a value to decode.
+ * @see decodeBase64Url
+ */
+export function decodeStartParam(value: string): string;
+export function decodeStartParam<T>(
+  value: string,
+  arg2?: 'json' | ((value: string) => T),
+): string | unknown | T {
+  const decoded = decodeBase64Url(value);
+  return arg2 === 'json'
+    ? JSON.parse(decoded)
+    : arg2
+      ? arg2(decoded)
+      : decoded;
+}
