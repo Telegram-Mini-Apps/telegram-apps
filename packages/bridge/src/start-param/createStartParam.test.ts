8cf99d2afcee06ae758ae43aa57e1addb04b0741
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-06-23 19:39:59 +0500

    feat(bridge): allow specifying unknown types of the argument in createStartParam

--- packages/bridge/src/start-param/createStartParam.test.ts
+++ packages/bridge/src/start-param/createStartParam.test.ts
@@ -6,6 +6,14 @@ it('should return value\'s base64 representation if its length is less than 513'
   expect(createStartParam('ABCDEF')).toBe('QUJDREVG');
 });
 
+it(
+  'should return a base64 value based on the value\'s JSON.stringify representation if the value is not a string',
+  () => {
+    expect(createStartParam({ field: 'value' })).toBe('eyJmaWVsZCI6InZhbHVlIn0=');
+  },
+);
+
 it('should throw if value\'s base64 representation length exceeds 512', () => {
-  expect(() => createStartParam(new Array(385).fill('A').join(''))).toThrow('Value is too long for start parameter');
+  expect(() => createStartParam(new Array(385).fill('A').join('')))
+    .toThrow('Value is too long for start parameter');
 });
--- packages/bridge/src/start-param/createStartParam.ts
+++ packages/bridge/src/start-param/createStartParam.ts
@@ -1,14 +1,16 @@
 import { encodeBase64Url } from '@/base64-url/encodeBase64Url.js';
 
 /**
- * Creates a safe start parameter value.
+ * Creates a safe start parameter value. If the value is not a string, the function applies
+ * JSON.stringify to it, so make sure you are not passing an object with circular references.
+ *
  * @param value - value to create start parameter from.
  * @throws {Error} If the value length is too big for the allowed one.
  * @see Learn more about start parameter:
  * https://docs.telegram-mini-apps.com/platform/start-parameter
  */
-export function createStartParam(value: string): string {
-  const b64 = encodeBase64Url(value);
+export function createStartParam(value: unknown): string {
+  const b64 = encodeBase64Url(typeof value === 'string' ? value : JSON.stringify(value));
   if (b64.length > 512) {
     throw new Error('Value is too long for start parameter');
   }
