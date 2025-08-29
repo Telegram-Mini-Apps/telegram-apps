49c56d08dc2987df72d3eb962e217065fbdcc790
qbnk <wolfram.deus@gmail.com>
2025-08-30 01:46:41 +0500

    feat(bridge): implement retrieveRawInitDataFp

--- packages/bridge/src/launch-params/retrieveRawInitData.ts
+++ packages/bridge/src/launch-params/retrieveRawInitData.ts
@@ -1,9 +1,35 @@
-import { retrieveRawLaunchParams } from '@/launch-params/retrieveRawLaunchParams.js';
+import { pipe } from 'fp-ts/lib/function.js';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+
+import {
+  retrieveRawLaunchParams,
+  retrieveRawLaunchParamsFp,
+} from '@/launch-params/retrieveRawLaunchParams.js';
+import type { LeftOfFnReturnType } from '@/fp/types.js';
 
 /**
  * @returns Raw init data from any known source.
- * @throws {LaunchParamsRetrieveError} Unable to retrieve launch params from any known source.
+ * @throws {LaunchParamsRetrieveError} Unable to retrieve launch params from
+ *   any known source.
+ * @deprecated Use `retrieveRawInitDataFp`.
  */
 export function retrieveRawInitData(): string | undefined {
   return new URLSearchParams(retrieveRawLaunchParams()).get('tgWebAppData') || undefined;
+}
+
+/**
+ * @returns Raw init data from any known source.
+ */
+export function retrieveRawInitDataFp(): E.Either<
+  LeftOfFnReturnType<typeof retrieveRawLaunchParamsFp>,
+  O.Option<string>
+> {
+  return pipe(
+    retrieveRawLaunchParamsFp(),
+    E.chain(raw => {
+      const v = new URLSearchParams(raw).get('tgWebAppData');
+      return E.right(v ? O.some(v) : O.none);
+    }),
+  );
 }
