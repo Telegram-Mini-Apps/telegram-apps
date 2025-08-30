d9514ae30aa12fc0813e55101422c7192397ea63
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-30 21:53:46 +0500

    refactor(bridge): move fp utils to the toolkit

--- packages/toolkit/src/either-monad/eitherGet.ts
+++ packages/toolkit/src/either-monad/eitherGet.ts
@@ -1,13 +1,14 @@
-import { either, function as fn } from 'fp-ts';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/lib/function.js';
 
 /**
  * Returns either's right if it is present. Throws left otherwise.
  * @param e - either instance.
  */
-export function eitherGet<L, R>(e: either.Either<L, R>): R {
-  return fn.pipe(
+export function eitherGet<L, R>(e: E.Either<L, R>): R {
+  return pipe(
     e,
-    either.match(
+    E.match(
       e => {
         // eslint-disable-next-line @typescript-eslint/only-throw-error
         throw e;
