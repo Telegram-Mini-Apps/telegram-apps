ebd14e22b45ea0ed8fcc3ca39027ab0527a39536
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-31 16:27:08 +0500

    chore(init-data-node): rename parse.ts to parsing.ts

--- tma.js/init-data-node/src/parse.ts
+++
@@ -1,6 +0,0 @@
-import { parseInitDataQuery } from '@tma.js/transformers';
-
-/**
- * Parses an incoming value as init data.
- */
-export const parse = parseInitDataQuery;
---
+++ tma.js/init-data-node/src/parsing.ts
@@ -0,0 +1,11 @@
+import { parseInitDataQuery, parseInitDataQueryFp } from '@tma.js/transformers';
+
+/**
+ * Parses an incoming value as init data.
+ */
+export const parse = parseInitDataQuery;
+
+/**
+ * Parses an incoming value as init data.
+ */
+export const parseFp = parseInitDataQueryFp;
