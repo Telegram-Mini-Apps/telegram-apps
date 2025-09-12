ccc1d0fb51f3b3c1711547e374a0d2227052420f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-09-12 19:18:20 +0500

    feat(init-data-node): mark errors as classes

--- tma.js/init-data-node/src/errors.ts
+++ tma.js/init-data-node/src/errors.ts
@@ -1,26 +1,30 @@
 import { errorClass, errorClassWithData } from 'error-kid';
 
-export const AuthDateInvalidError = errorClassWithData<
+export class AuthDateInvalidError extends errorClassWithData<
   { value: string | undefined },
   [value?: string]
 >(
   'AuthDateInvalidError',
   value => ({ value }),
   value => [`"auth_date" is invalid: ${value || 'value is missing'}`],
-);
+) {
+}
 
-export const SignatureInvalidError = errorClass('SignatureInvalidError');
+export class SignatureInvalidError extends errorClass('SignatureInvalidError') {
+}
 
-export const HexStringLengthInvalidError = errorClass(
+export class HexStringLengthInvalidError extends errorClass(
   'HexStringLengthInvalidError',
-);
+) {
+}
 
-export const SignatureMissingError = errorClass<[thirdParty: boolean]>(
+export class SignatureMissingError extends errorClass<[thirdParty: boolean]>(
   'SignatureMissingError',
   thirdParty => [`"${thirdParty ? 'signature' : 'hash'}" parameter is missing`],
-);
+) {
+}
 
-export const ExpiredError = errorClassWithData<
+export class ExpiredError extends errorClassWithData<
   { issuedAt: Date; expiresAt: Date },
   [issuedAt: Date, expiresAt: Date, now: Date]
 >(
@@ -29,4 +33,5 @@ export const ExpiredError = errorClassWithData<
   (issuedAt, expiresAt, now) => [
     `Init data expired. Issued at ${issuedAt.toISOString()}, expires at ${expiresAt.toISOString()}, now is ${now.toISOString()}`,
   ],
-);
+) {
+}
