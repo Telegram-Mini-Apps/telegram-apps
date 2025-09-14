5bb3ffa3db7fe91e82d8570e18362271f22a811d
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-09-14 22:38:04 +0500

    tests(sdk): allow using optional minVersion in testComponentMethodSafety

--- tma.js/sdk/test-utils/predefined/testComponentMethodSafety.ts
+++ tma.js/sdk/test-utils/predefined/testComponentMethodSafety.ts
@@ -23,14 +23,11 @@ export function testComponentMethodSafety<C>({
    * Creates a new component instance.
    * @param opts
    */
-  instantiate: (opts: {
-    isTma: boolean;
-    version: string;
-  }) => C;
+  instantiate: (opts: { isTma: boolean; version: string }) => C;
   /**
    * Minimal version for the method to be supported.
    */
-  minVersion: string;
+  minVersion?: string;
   /**
    * Mounts the component.
    */
@@ -41,52 +38,72 @@ export function testComponentMethodSafety<C>({
    */
   try: (component: C) => any;
 }) {
-  const [a, b = 0] = minVersion.split('.').map(Number);
-  const prevVersion = `${b === 0 ? a - 1 : a}.${b === 0 ? 99 : b - 1}`;
+  const hasMinVersion = !!minVersion;
+  const versions: { prev: string; min: string } | undefined = minVersion
+    ? (() => {
+      const [a, b = 0] = minVersion.split('.').map(Number);
+      return {
+        prev: `${b === 0 ? a - 1 : a}.${b === 0 ? 99 : b - 1}`,
+        min: minVersion,
+      };
+    })()
+    : { prev: '99.9', min: '100' };
 
-  describe('isSupported', () => {
-    testIsSupported(
-      version => get(instantiate({ isTma: true, version })),
-      minVersion,
-    );
-  });
+  if (hasMinVersion) {
+    describe('isSupported', () => {
+      testIsSupported(version => get(instantiate({ isTma: true, version })), versions.min);
+    });
+  }
 
   describe('isAvailable', () => {
     it('should return false if not mini apps env', () => {
-      expect(get(instantiate({ isTma: false, version: minVersion })).isAvailable()).toBe(false);
+      expect(get(instantiate({ isTma: false, version: versions.min })).isAvailable()).toBe(false);
     });
 
-    it('should return false if version is "0.0" (SDK not initialized)', () => {
-      expect(get(instantiate({ isTma: true, version: '0.0' })).isAvailable()).toBe(false);
-    });
-
-    it(`should return false if version is less than ${minVersion}`, () => {
-      expect(get(instantiate({ isTma: true, version: prevVersion })).isAvailable()).toBe(false);
-    });
-
-    if (mount) {
-      it('should return false if component is not mounted', () => {
-        expect(get(instantiate({ isTma: true, version: minVersion })).isAvailable()).toBe(false);
+    if (hasMinVersion) {
+      it('should return false if version is "0.0" (SDK not initialized)', () => {
+        expect(get(instantiate({ isTma: true, version: '0.0' })).isAvailable()).toBe(false);
       });
 
+      it(`should return false if version is less than ${versions.min}`, () => {
+        expect(get(instantiate({ isTma: true, version: versions.prev })).isAvailable()).toBe(false);
+      });
+    }
+
+    if (!mount) {
+      if (hasMinVersion) {
+        it(`should return true if mini apps env and version is ${versions.min}`, () => {
+          expect(get(instantiate({ isTma: true, version: versions.min })).isAvailable()).toBe(true);
+        });
+      }
+      return;
+    }
+
+    it('should return false if component is not mounted', () => {
+      expect(get(instantiate({ isTma: true, version: versions.min })).isAvailable()).toBe(false);
+    });
+
+    if (hasMinVersion) {
       it(
-        `should return true if mini apps env, version is ${minVersion} and component is mounted`,
+        `should return true if mini apps env, version is ${versions.min} and component is mounted`,
         () => {
-          const component = instantiate({ isTma: true, version: minVersion });
+          const component = instantiate({ isTma: true, version: versions.min });
           mount(component);
           expect(get(component).isAvailable()).toBe(true);
         },
       );
     } else {
-      it(`should return true if mini apps env and version is ${minVersion}`, () => {
-        expect(get(instantiate({ isTma: true, version: minVersion })).isAvailable()).toBe(true);
+      it('should return true if mini apps env and component is mounted', () => {
+        const component = instantiate({ isTma: true, version: versions.min });
+        mount(component);
+        expect(get(component).isAvailable()).toBe(true);
       });
     }
   });
 
   describe('not mini apps env', () => {
     it('should throw FunctionUnavailableError', () => {
-      expect(() => tryCall(instantiate({ isTma: false, version: minVersion }))).toThrow(
+      expect(() => tryCall(instantiate({ isTma: false, version: versions.min }))).toThrow(
         new FunctionUnavailableError(
           'Unable to call function: it can\'t be called outside Mini Apps',
         ),
@@ -95,47 +112,74 @@ export function testComponentMethodSafety<C>({
   });
 
   describe('mini apps env', () => {
-    describe('package uninitialized (version is "0.0")', () => {
-      it('should throw FunctionUnavailableError', () => {
-        expect(() => tryCall(instantiate({ isTma: true, version: '0.0' })))
-          .toThrow(new FunctionUnavailableError(
-            'Unable to call function: the SDK was not initialized. Use the SDK init() function',
-          ));
+    if (hasMinVersion) {
+      describe('package uninitialized (version is "0.0")', () => {
+        it('should throw FunctionUnavailableError', () => {
+          expect(() => tryCall(instantiate({ isTma: true, version: '0.0' })))
+            .toThrow(new FunctionUnavailableError(
+              'Unable to call function: the SDK was not initialized. Use the SDK init() function',
+            ));
+        });
       });
-    });
+    }
 
     describe('package initialized', () => {
-      describe(`minimal version not satisfied (${minVersion})`, () => {
+      if (hasMinVersion) {
+        describe(`minimal version not satisfied (${versions.min})`, () => {
+          it('should throw FunctionUnavailableError', () => {
+            expect(() => tryCall(instantiate({ isTma: true, version: versions.prev }))).toThrow(
+              new FunctionUnavailableError(
+                `Unable to call function: it is unsupported in Mini Apps version ${versions.prev}`,
+              ),
+            );
+          });
+        });
+
+        if (mount) {
+          describe(`minimal version satisfied (${versions.min})`, () => {
+            describe('component is not mounted', () => {
+              it('should throw FunctionUnavailableError', () => {
+                expect(() => tryCall(instantiate({ isTma: true, version: versions.min }))).toThrow(
+                  new FunctionUnavailableError(
+                    'Unable to call function: the component is unmounted. Use the mount() method',
+                  ),
+                );
+              });
+            });
+
+            describe('component is mounted', () => {
+              it('should not throw', () => {
+                const component = instantiate({ isTma: true, version: versions.min });
+                mount(component);
+                expect(() => tryCall(component)).not.toThrow();
+              });
+            });
+          });
+        }
+        return;
+      }
+
+      if (!mount) {
+        return;
+      }
+
+      describe('component is not mounted', () => {
         it('should throw FunctionUnavailableError', () => {
-          expect(() => tryCall(instantiate({ isTma: true, version: prevVersion }))).toThrow(
+          expect(() => tryCall(instantiate({ isTma: true, version: versions.min }))).toThrow(
             new FunctionUnavailableError(
-              `Unable to call function: it is unsupported in Mini Apps version ${prevVersion}`,
+              'Unable to call function: the component is unmounted. Use the mount() method',
             ),
           );
         });
       });
 
-      if (mount) {
-        describe(`minimal version satisfied (${minVersion})`, () => {
-          describe('component is not mounted', () => {
-            it('should throw FunctionUnavailableError', () => {
-              expect(() => tryCall(instantiate({ isTma: true, version: minVersion }))).toThrow(
-                new FunctionUnavailableError(
-                  'Unable to call function: the component is unmounted. Use the mount() method',
-                ),
-              );
-            });
-          });
-
-          describe('component is mounted', () => {
-            it('should not throw', () => {
-              const component = instantiate({ isTma: true, version: minVersion });
-              mount(component);
-              expect(() => tryCall(component)).not.toThrow();
-            });
-          });
+      describe('component is mounted', () => {
+        it('should not throw', () => {
+          const component = instantiate({ isTma: true, version: versions.min });
+          mount(component);
+          expect(() => tryCall(component)).not.toThrow();
         });
-      }
+      });
     });
   });
 }
