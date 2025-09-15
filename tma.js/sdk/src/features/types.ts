673739cb040a20bc9749d14f78209c6058bc483f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-09-15 14:11:01 +0500

    feat(sdk): define WithInvokeCustomMethod feature mixin

--- tma.js/sdk/src/features/types.ts
+++ tma.js/sdk/src/features/types.ts
@@ -1,14 +1,41 @@
 import type { Version } from '@tma.js/types';
-import type { PostEventFpFn } from '@tma.js/bridge';
+import type {
+  PostEventFpFn,
+  InvokeCustomMethodError,
+  InvokeCustomMethodFpOptions,
+  RequestError,
+  CustomMethodName,
+  CustomMethodParams,
+} from '@tma.js/bridge';
+import type * as TE from 'fp-ts/TaskEither';
 
 import type { MaybeAccessor } from '@/types.js';
 import type { ComponentStorage } from '@/component-storage.js';
+
+export interface InvokeCustomMethodFpFnNoRequestId {
+  <M extends CustomMethodName>(
+    this: void,
+    method: M,
+    params: CustomMethodParams<M>,
+    options?: InvokeCustomMethodFpOptions,
+  ): TE.TaskEither<InvokeCustomMethodError, unknown>;
+  (
+    this: void,
+    method: string,
+    params: object,
+    options?: InvokeCustomMethodFpOptions,
+  ): TE.TaskEither<RequestError, unknown>;
+}
 
 export interface WithVersion {
   /**
    * The currently supported Telegram Mini Apps version by the Telegram client.
    */
   version: MaybeAccessor<Version>;
+}
+
+export interface WithInvokeCustomMethod {
+  invokeCustomMethod: InvokeCustomMethodFpFnNoRequestId;
 }
 
 export interface WithPostEvent {
