f30ec743efd93e3d01f1b6440520888535a6228d
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-09-17 10:06:07 +0500

    chore(sdk): wip

---
+++ tma.js/sdk/src/index.ts
@@ -0,0 +1,176 @@
+export * from '@/scopes/components/back-button/exports.js';
+export * from '@/scopes/components/biometry/exports.js';
+export * from '@/scopes/components/closing-behavior/exports.js';
+export * from '@/scopes/components/cloud-storage/exports.js';
+export * from '@/scopes/components/haptic-feedback/exports.js';
+export * from '@/scopes/components/init-data/exports.js';
+export * from '@/scopes/components/invoice/exports.js';
+export * from '@/scopes/components/location-manager/exports.js';
+export * from '@/scopes/components/main-button/exports.js';
+export * from '@/scopes/components/mini-app/exports.js';
+export * from '@/scopes/components/popup/exports.js';
+export * from '@/scopes/components/qr-scanner/exports.js';
+export * from '@/scopes/components/secondary-button/exports.js';
+export * from '@/scopes/components/settings-button/exports.js';
+export * from '@/scopes/components/swipe-behavior/exports.js';
+export * from '@/scopes/components/theme-params/exports.js';
+export * from '@/scopes/components/viewport/exports.js';
+export * from '@/scopes/utilities/emoji-status/exports.js';
+export * from '@/scopes/utilities/home-screen/exports.js';
+export * from '@/scopes/utilities/links/exports.js';
+export * from '@/scopes/utilities/privacy/exports.js';
+export * from '@/scopes/utilities/uncategorized/exports.js';
+
+export {
+  wrapSafe,
+  type WrapSafeOptions,
+  type Supports,
+  type SafeWrapped,
+  type CustomSupportValidatorFn,
+  type IsSupportedType,
+  type IfAvailableFnResult,
+} from '@/scopes/wrappers/wrapSafe.js';
+
+export { ignoreCanceled } from '@/utils/ignoreCanceled.js';
+export { isColorDark } from '@/utils/isColorDark.js';
+export { isSSR } from '@/utils/isSSR.js';
+export { safeCall } from '@/utils/safeCall.js';
+
+export * from '@/errors.js';
+export {
+  invokeCustomMethod,
+  postEvent,
+  createRequestId,
+  configure,
+  request,
+  type ConfigureOptions,
+} from '@/globals.js';
+export { init, type InitOptions } from '@/init.js';
+export type { RequestOptionsNoCapture, AsyncOptions } from '@/types.js';
+
+export {
+  decodeBase64Url,
+  encodeBase64Url,
+  createStartParam,
+  decodeStartParam,
+  isSafeToCreateStartParam,
+  isTMA,
+  mockTelegramEnv,
+  targetOrigin,
+  on,
+  off,
+  retrieveLaunchParams,
+  retrieveRawLaunchParams,
+  retrieveRawInitData,
+  type RetrieveLPResult,
+  type RetrieveLPResultCamelCased,
+  type RequestOptions,
+  type InvokeCustomMethodOptions,
+  type EventListener,
+  type SubscribeListener,
+  type EventPayload,
+  type WriteAccessRequestedStatus,
+  UnknownEnvError,
+  isUnknownEnvError,
+  createPostEvent,
+  type CreatePostEventMode,
+  InvokeCustomMethodError,
+  isInvokeCustomMethodError,
+  MethodParameterUnsupportedError,
+  isMethodMethodParameterUnsupportedError,
+  type CustomMethodName,
+  type CustomMethodParams,
+  type CreateMethodParams,
+  type Methods,
+  type AnyInvokeCustomMethodParams,
+  type Events,
+  isMethodUnsupportedError,
+  type EventWithoutPayload,
+  type EventWithPayload,
+  type CustomMethodsParams,
+  type InvokeCustomMethodFn,
+  supports,
+  emitEvent,
+  MethodUnsupportedError,
+  type EmojiStatusAccessRequestedStatus,
+  type HomeScreenStatus,
+  type OnUnsupportedFn,
+  type ImpactHapticFeedbackParams,
+  type ImpactHapticFeedbackStyle,
+  type MethodName,
+  type MethodParams,
+  type PostEventFn,
+  type NotificationHapticFeedbackParams,
+  type RequestFn,
+  type BiometryAuthRequestStatus,
+  type PhoneRequestedStatus,
+  type SafeAreaInsets,
+  type BiometryType,
+  type BiometryTokenUpdateStatus,
+  type EventName,
+  type NotificationHapticFeedbackType,
+  type SelectionHapticFeedbackParams,
+  type MethodNameWithoutParams,
+  type MethodNameWithOptionalParams,
+  type MethodNameWithRequiredParams,
+  type EmojiStatusFailedError,
+  type FullScreenErrorStatus,
+  type InvoiceStatus,
+  type RequestResult,
+  isLaunchParamsRetrieveError,
+  LaunchParamsRetrieveError,
+  InvalidLaunchParamsError,
+  isInvalidLaunchParamsError,
+  type BackgroundColor,
+  type PopupParams,
+  type BottomBarColor,
+  type PopupButton,
+  type HeaderColorKey,
+  type OpenLinkBrowser,
+  type SecondaryButtonPosition,
+  type SwitchInlineQueryChatType,
+  isCancelledError,
+  isTimeoutError,
+  CancelledError,
+  TimeoutError,
+  AbortablePromise,
+  ManualPromise,
+  postMessage,
+  postMessageImplementation,
+  applyPolyfills,
+  type PostMessage,
+  logger,
+  type Logger,
+  setDebug,
+} from '@tma.js/bridge';
+export {
+  isRGB,
+  toRGB,
+  isRGBShort,
+  serializeToQuery,
+  serializeInitDataQuery,
+  serializeLaunchParamsQuery,
+  transformQueryUsing,
+  parseInitDataQuery,
+  parseLaunchParamsQuery,
+} from '@tma.js/transformers';
+export type {
+  RGB,
+  User,
+  Chat,
+  InitData,
+  ChatType,
+  RGBShort,
+  Platform,
+  ThemeParams,
+  LaunchParams,
+  KnownThemeParamsKey,
+  Version,
+} from '@tma.js/types';
+export {
+  createLogger,
+  type LogLevel,
+  type LoggerOptions,
+  type LoggerFn,
+  type LoggerForceFn,
+} from '@tma.js/toolkit';
