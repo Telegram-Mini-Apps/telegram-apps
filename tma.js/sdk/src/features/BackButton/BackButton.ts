a5c8f05621d1b43ad4f6844d57d21c50110184d0
qbnk <wolfram.deus@gmail.com>
2025-09-18 18:15:38 +0500

    refactor(sdk): redefine BackButton and make it based on composites

--- tma.js/sdk/src/features/BackButton/BackButton.ts
+++ tma.js/sdk/src/features/BackButton/BackButton.ts
@@ -1,65 +1,55 @@
-import { computed, type Computed, signal } from '@tma.js/signals';
+import type { Computed } from '@tma.js/signals';
 
 import { createWrapSafe, type SafeWrapped } from '@/wrappers/wrapSafe.js';
-import { isPageReload } from '@/navigation.js';
 import { createIsSupportedSignal } from '@/helpers/createIsSupportedSignal.js';
-import type { ComponentStorage } from '@/component-storage.js';
-import type {
-  SharedComponentOptions,
-  WithOnClickListener,
-  WithPostEvent,
-  WithStorage,
-  WithVersion,
-} from '@/features/types.js';
+import type { WithPostEvent, WithVersion } from '@/features/types.js';
+import { ButtonLike, ButtonLikeOptions } from '@/composites/ButtonLike.js';
 
-export type BackButtonStorage = ComponentStorage<{ isVisible: boolean }>;
+export interface BackButtonState {
+  isVisible: boolean;
+}
 
 export interface BackButtonOptions extends WithVersion,
-  WithStorage<BackButtonStorage>,
   WithPostEvent,
-  WithOnClickListener,
-  SharedComponentOptions {
+  Omit<ButtonLikeOptions<BackButtonState>, 'onChange' | 'initialState'> {
 }
 
 /**
  * @since Mini Apps v6.1
  */
 export class BackButton {
-  constructor({ version, postEvent, storage, isTma, onClick, offClick }: BackButtonOptions) {
-    const isMounted = signal(false);
-    const isVisible = signal(false);
+  constructor({ postEvent, version, isTma, ...rest }: BackButtonOptions) {
+    this.buttonLike = new ButtonLike({
+      ...rest,
+      initialState: { isVisible: false },
+      isTma,
+      onChange(state) {
+        postEvent('web_app_setup_back_button', { is_visible: state.isVisible });
+      },
+    });
+
     const wrapOptions = { version, isSupported: 'web_app_setup_back_button', isTma } as const;
     const wrapSupported = createWrapSafe(wrapOptions);
-    const wrapComplete = createWrapSafe({ ...wrapOptions, isMounted });
+    const wrapComplete = createWrapSafe({
+      ...wrapOptions,
+      isMounted: this.buttonLike.isMounted,
+    });
 
-    const setVisibility = (value: boolean): void => {
-      if (value !== isVisible()) {
-        postEvent('web_app_setup_back_button', { is_visible: value });
-        storage.set({ isVisible: value });
-        isVisible.set(value);
-      }
+    const setVisibility = (isVisible: boolean): void => {
+      this.buttonLike.setState({ isVisible });
     };
 
+    this.isVisible = this.buttonLike.isVisible;
+    this.isMounted = this.buttonLike.isMounted;
     this.isSupported = createIsSupportedSignal('web_app_setup_back_button', version);
-    this.isVisible = computed(isVisible);
-    this.isMounted = computed(isMounted);
     this.hide = wrapComplete(() => setVisibility(false));
     this.show = wrapComplete(() => setVisibility(true));
-    this.onClick = wrapSupported(onClick);
-    this.offClick = wrapSupported(offClick);
-    this.mount = wrapSupported(() => {
-      if (!isMounted()) {
-        const state = isPageReload() ? storage.get() : undefined;
-        if (state) {
-          isVisible.set(state.isVisible);
-        }
-        isMounted.set(true);
-      }
-    });
-    this.unmount = () => {
-      isMounted.set(false);
-    };
+    this.onClick = wrapSupported(this.buttonLike.onClick);
+    this.offClick = wrapSupported(this.buttonLike.offClick);
+    this.mount = wrapSupported(this.buttonLike.mount);
   }
+
+  private readonly buttonLike: ButtonLike<BackButtonState>;
 
   /**
    * Signal indicating if the component is currently visible.
@@ -77,21 +67,6 @@ export class BackButton {
   readonly isSupported: Computed<boolean>;
 
   /**
-   * Mounts the component restoring its state.
-   * @since Mini Apps v6.1
-   */
-  mount: SafeWrapped<() => void, true>;
-
-  /**
-   * Unmounts the component.
-   *
-   * Note that this function does not remove listeners added via the `onClick`
-   * function, so you have to remove them on your own.
-   * @see onClick
-   */
-  unmount: () => void;
-
-  /**
    * Hides the back button.
    * @since Mini Apps v6.1
    */
@@ -104,30 +79,53 @@ export class BackButton {
   show: SafeWrapped<() => void, true>;
 
   /**
-   * Adds a new back button listener.
+   * Adds a new button listener.
    * @param listener - event listener.
    * @param once - should the listener be called only once.
    * @returns A function to remove bound listener.
    * @since Mini Apps v6.1
    * @example
-   * const off = backButton.onClick(() => {
-   *   console.log('User clicked the back button');
+   * const off = button.onClick(() => {
+   *   console.log('User clicked the button');
    *   off();
    * });
    */
-  onClick: SafeWrapped<(listener: () => void, once?: boolean) => VoidFunction, true>;
+  readonly onClick: SafeWrapped<
+    (listener: VoidFunction, once?: boolean) => VoidFunction,
+    true
+  >;
 
   /**
-   * Removes the back button click listener.
+   * Removes the button click listener.
    * @param listener - event listener.
    * @param once - should the listener be called only once.
    * @since Mini Apps v6.1
    * @example
    * function listener() {
-   *   console.log('User clicked the back button');
-   *   backButton.offClick(listener);
+   *   console.log('User clicked the button');
+   *   button.offClick(listener);
    * }
-   * backButton.onClick(listener);
+   * button.onClick(listener);
    */
-  offClick: SafeWrapped<(listener: () => void, once?: boolean) => void, true>;
+  readonly offClick: SafeWrapped<
+    (listener: VoidFunction, once?: boolean) => void,
+    true
+  >;
+
+  /**
+   * Mounts the component restoring its state.
+   * @since Mini Apps v6.1
+   */
+  readonly mount: SafeWrapped<() => void, true>;
+
+  /**
+   * Unmounts the component.
+   *
+   * Note that this function does not remove listeners added via the `onClick`
+   * function, so you have to remove them on your own.
+   * @see onClick
+   */
+  unmount() {
+    this.buttonLike.unmount();
+  }
 }
