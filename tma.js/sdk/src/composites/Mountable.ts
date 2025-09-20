bd703c3fe3771ff42a9bf74eb30955e8d34f5124
qbnk <wolfram.deus@gmail.com>
2025-09-20 14:17:58 +0300

    fix(sdk): remove wrapping in Mountable

--- tma.js/sdk/src/composites/Mountable.ts
+++ tma.js/sdk/src/composites/Mountable.ts
@@ -1,37 +1,32 @@
 import { batch, computed, signal } from '@tma.js/signals';
 
-import type { SharedFeatureOptions } from '@/features/types.js';
-import { createWrapSafe, type SafeWrapped } from '@/wrappers/wrapSafe.js';
 import { isPageReload } from '@/navigation.js';
 
-export interface MountableOptions<S> extends SharedFeatureOptions {
+type RestoreStateFn<S> = () => (S | undefined);
+
+type OnStateRestoredFn<S> = (state: S) => void;
+
+export interface MountableOptions<S> {
   /**
    * Attempts to restore the component state.
    */
-  restoreState: () => (S | undefined);
+  restoreState: RestoreStateFn<S>;
   /**
    * A function to call whenever the state was restored during the mount.
    * @param state - restored state.
    */
-  onStateRestored: (state: S) => void;
+  onStateRestored: OnStateRestoredFn<S>;
 }
 
 export class Mountable<S extends object> {
-  constructor({ onStateRestored, restoreState, ...rest }: MountableOptions<S>) {
-    const wrapSafe = createWrapSafe(rest);
-
-    this.mount = wrapSafe(() => {
-      if (!this._isMounted()) {
-        batch(() => {
-          const state = isPageReload() ? restoreState() : undefined;
-          if (state) {
-            onStateRestored(state);
-          }
-          this._isMounted.set(true);
-        });
-      }
-    });
+  constructor({ onStateRestored, restoreState }: MountableOptions<S>) {
+    this.restoreState = restoreState;
+    this.onStateRestored = onStateRestored;
   }
+
+  private readonly restoreState: RestoreStateFn<S>;
+
+  private readonly onStateRestored: OnStateRestoredFn<S>;
 
   protected readonly _isMounted = signal(false);
 
@@ -44,7 +39,17 @@ export class Mountable<S extends object> {
    * Mounts the component restoring its state and calling required side
    * effects.
    */
-  mount: SafeWrapped<VoidFunction, false>;
+  mount() {
+    if (!this._isMounted()) {
+      batch(() => {
+        const state = isPageReload() ? this.restoreState() : undefined;
+        if (state) {
+          this.onStateRestored(state);
+        }
+        this._isMounted.set(true);
+      });
+    }
+  }
 
   /**
    * Unmounts the component.
