1bf97b082cf33018178d4c7682b628c3e1773ac4
qbnk <wolfram.deus@gmail.com>
2025-09-26 01:35:58 +0300

    refactor(sdk/BackButton): switch instance to mixins

--- tma.js/sdk/src/features/BackButton/instance.ts
+++ tma.js/sdk/src/features/BackButton/instance.ts
@@ -1,20 +1,24 @@
-import { on, off, postEventFp, isTMA } from '@tma.js/bridge';
+import { pipe } from 'fp-ts/function';
 
-import { BackButton } from '@/features/BackButton/BackButton.js';
-import { createComponentSessionStorage } from '@/component-storage.js';
-import { version } from '@/globals/version.js';
-import { isPageReload } from '@/navigation.js';
+import { BackButton, type BackButtonState } from '@/features/BackButton/BackButton.js';
+import {
+  withStateRestore,
+  sharedFeatureOptions,
+  withVersion,
+  withPostEvent, withClickListeners,
+} from '@/features/mixins.js';
 
-export const backButton = new BackButton({
-  isPageReload,
-  isTma: isTMA,
-  offClick(listener, once) {
-    off('back_button_pressed', listener, once);
-  },
-  onClick(listener, once) {
-    return on('back_button_pressed', listener, once);
-  },
-  postEvent: postEventFp,
-  storage: createComponentSessionStorage('backButton'),
-  version,
-});
+/**
+ * @internal
+ */
+export function instantiateBackButton() {
+  return new BackButton(pipe(
+    sharedFeatureOptions(),
+    withVersion,
+    withPostEvent,
+    withStateRestore<BackButtonState>('backButton'),
+    withClickListeners('back_button_pressed'),
+  ));
+}
+
+export const backButton = instantiateBackButton();
