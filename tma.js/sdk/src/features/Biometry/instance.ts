f290e0daf3b660084f812fde5a678c7e0c8beaf7
qbnk <wolfram.deus@gmail.com>
2025-09-26 18:47:54 +0500

    refactor(sdk/Biometry): switch instance to mixins

--- tma.js/sdk/src/features/Biometry/instance.ts
+++ tma.js/sdk/src/features/Biometry/instance.ts
@@ -1,21 +1,35 @@
-import { isTMA, on, requestFp, postEventFp, off } from '@tma.js/bridge';
+import { on, off } from '@tma.js/bridge';
+import { pipe } from 'fp-ts/function';
 
+import {
+  sharedFeatureOptions,
+  withPostEvent,
+  withRequest,
+  withStateRestore,
+  withVersion,
+} from '@/features/mixins.js';
 import { Biometry } from '@/features/Biometry/Biometry.js';
-import { isPageReload } from '@/navigation.js';
-import { createComponentSessionStorage } from '@/component-storage.js';
-import { version } from '@/globals/version.js';
+import type { BiometryState } from '@/features/Biometry/types.js';
 
-export const biometry = new Biometry({
-  isPageReload,
-  isTma: isTMA,
-  offBiometryInfoReceived(listener) {
-    off('biometry_info_received', listener);
-  },
-  onBiometryInfoReceived(listener) {
-    return on('biometry_info_received', listener);
-  },
-  postEvent: postEventFp,
-  request: requestFp,
-  storage: createComponentSessionStorage('biometry'),
-  version,
-});
+/**
+ * @internal
+ */
+export function instantiateBiometry() {
+  return new Biometry({
+    ...pipe(
+      sharedFeatureOptions(),
+      withVersion,
+      withPostEvent,
+      withRequest,
+      withStateRestore<BiometryState>('biometry'),
+    ),
+    offInfoReceived(listener) {
+      off('biometry_info_received', listener);
+    },
+    onInfoReceived(listener) {
+      return on('biometry_info_received', listener);
+    },
+  });
+}
+
+export const biometry = instantiateBiometry();
