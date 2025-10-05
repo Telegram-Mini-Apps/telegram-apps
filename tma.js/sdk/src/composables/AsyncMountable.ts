f91ca975a37ffd2fbd91c4ba253f2755d7ffa097
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-05 23:40:46 +0500

    fix(sdk/AsyncMountable): return monad in mount and refactor the composable

--- tma.js/sdk/src/composables/AsyncMountable.ts
+++ tma.js/sdk/src/composables/AsyncMountable.ts
@@ -1,32 +1,25 @@
 import { batch, computed, signal } from '@tma.js/signals';
-import { BetterPromise } from 'better-promises';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 
 import type { AsyncOptions, MaybeAccessor } from '@/types.js';
-import { teToPromise } from '@/helpers/teToPromise.js';
 import { access } from '@/helpers/access.js';
 
-type RestoreStateFn<S> = () => (S | undefined);
-
-type OnMounted<S> = (state: S) => void;
-
-type InitialStateFn<S, E> = (options?: AsyncOptions) => TE.TaskEither<E, S>;
-
 export interface AsyncMountableOptions<S, E> {
+  /**
+   * A function to retrieve the initial state.
+   * @param options - additional options.
+   */
+  initialState: (options?: AsyncOptions) => TE.TaskEither<E, S>;
   /**
    * @returns True if the current page was reloaded.
    */
   isPageReload: MaybeAccessor<boolean>;
   /**
-   * A function to retrieve the initial state.
-   * @param options - additional options.
-   */
-  initialState: InitialStateFn<S, E>;
-  /**
    * A function to call whenever the component was mounted.
    * @param state - restored state.
    */
-  onMounted?: OnMounted<S>;
+  onMounted?: (state: S) => void;
   /**
    * A function to call whenever the component was unmounted.
    */
@@ -35,7 +28,7 @@ export interface AsyncMountableOptions<S, E> {
    * Attempts to restore previously saved component state. This function
    * will only be called if the current page was reloaded.
    */
-  restoreState: RestoreStateFn<S>;
+  restoreState: () => (S | undefined);
 }
 
 export class AsyncMountable<S extends object, E> {
@@ -46,22 +39,36 @@ export class AsyncMountable<S extends object, E> {
     onUnmounted,
     isPageReload,
   }: AsyncMountableOptions<S, E>) {
-    this.restoreState = restoreState;
-    this.onMounted = onMounted;
-    this.onUnmounted = onUnmounted;
-    this.isPageReload = isPageReload;
-    this.initialState = initialState;
+    this.mount = options => {
+      if (this._isMounted()) {
+        return TE.right(undefined);
+      }
+      const restored = access(isPageReload) ? restoreState() : undefined;
+      return pipe(
+        restored ? TE.right(restored) : initialState(options),
+        TE.map(state => {
+          // The user could call mount several times in a row while the
+          // component was still mounting. We should prevent calling the
+          // same hooks several times in this case.
+          if (!this._isMounted()) {
+            batch(() => {
+              this._isMounted.set(true);
+              onMounted?.(state);
+            });
+          }
+        }),
+      );
+    };
+
+    this.unmount = () => {
+      if (this._isMounted()) {
+        batch(() => {
+          this._isMounted.set(false);
+          onUnmounted?.();
+        });
+      }
+    };
   }
-
-  private readonly restoreState: RestoreStateFn<S>;
-
-  private readonly onMounted?: OnMounted<S>;
-
-  private readonly onUnmounted?: VoidFunction;
-
-  private readonly isPageReload: MaybeAccessor<boolean>;
-
-  private readonly initialState: InitialStateFn<S, E>;
 
   private readonly _isMounted = signal(false);
 
@@ -71,41 +78,13 @@ export class AsyncMountable<S extends object, E> {
   readonly isMounted = computed(this._isMounted);
 
   /**
-   * Mounts the component restoring its state and calling required side
-   * effects.
+   * Mounts the component restoring its state and calling required side effects.
    * @param options - additional execution options.
    */
-  mount(options?: AsyncOptions): BetterPromise<void> {
-    return this._isMounted()
-      ? BetterPromise.resolve()
-      : BetterPromise
-        .fn(() => {
-          return (
-            access(this.isPageReload) ? this.restoreState() : undefined
-          ) || teToPromise(this.initialState(options));
-        })
-        .then(state => {
-          // The user could call mount several times in a row while the
-          // component was still mounting. We should prevent calling the
-          // same hooks several times in this case.
-          if (!this._isMounted()) {
-            batch(() => {
-              this._isMounted.set(true);
-              this.onMounted?.(state);
-            });
-          }
-        });
-  }
+  readonly mount: (options?: AsyncOptions) => TE.TaskEither<E, void>;
 
   /**
    * Unmounts the component.
    */
-  unmount() {
-    if (this._isMounted()) {
-      batch(() => {
-        this._isMounted.set(false);
-        this.onUnmounted?.();
-      });
-    }
-  }
+  readonly unmount: () => void;
 }
