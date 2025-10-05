4ded1d3114b63c4b9dc118014b5825e6044d41b8
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-05 22:47:49 +0500

    fix(toolkit): rework monads utils

--- tma.js/toolkit/src/monads.ts
+++ tma.js/toolkit/src/monads.ts
@@ -1,27 +1,54 @@
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
+
+import type { AnyFn } from '@/types/misc.js';
 
 export type AnyEither<L = any, R = any> = E.Either<L, R> | TE.TaskEither<L, R>;
 export type AnyFnAnyEither<L = any, R = any> = (...args: any) => AnyEither<L, R>;
 
-export type RightOfEither<E extends AnyEither> = E extends AnyEither<never, infer U> ? U : never;
-export type LeftOfEither<E extends AnyEither> = E extends AnyEither<infer U> ? U : never;
+export type RightOfEither<T extends AnyEither> = [T] extends [E.Either<any, infer U>]
+  ? U
+  : T extends TE.TaskEither<any, infer U>
+    ? U
+    : never;
+export type LeftOfEither<T extends AnyEither> = [T] extends [E.Either<infer U, any>]
+  ? U
+  : T extends TE.TaskEither<infer U, any>
+    ? U
+    : never;
 export type RightOfReturn<F extends AnyFnAnyEither> = RightOfEither<ReturnType<F>>;
 export type LeftOfReturn<F extends AnyFnAnyEither> = LeftOfEither<ReturnType<F>>;
 
-/**
- * @param either - Either monad.
- * @returns Either's right if its tag is right. Throws left otherwise.
- */
-export function eitherGet<T>(either: E.Either<any, T>): T {
-  if (either._tag === 'Left') {
-    throw either.left;
-  }
-  return either.right;
+export type MaybeMonadToCommon<T> = [T] extends [E.Either<any, infer U>]
+  ? U
+  : T extends TE.TaskEither<any, infer U>
+    ? Promise<U>
+    : T;
+
+export type MaybeMonadReturnTypeToCommon<Fn extends AnyFn> = MaybeMonadToCommon<ReturnType<Fn>>;
+
+export function throwifyAnyEither<E extends AnyEither>(either: E): MaybeMonadToCommon<E> {
+  const onError = (e: unknown) => {
+    throw e;
+  };
+  return (
+    typeof either === 'function'
+      ? pipe(either, TE.match(onError, data => data))()
+      : pipe(either, E.match(onError, data => data))
+  ) as MaybeMonadToCommon<E>;
 }
 
-export function eitherFnToSimple<Fn extends (...args: any) => E.Either<any, any>>(
+export function throwifyFpFn<Fn extends AnyFnAnyEither>(
   fn: Fn,
-): (...args: Parameters<Fn>) => RightOfReturn<Fn> {
-  return (...args) => eitherGet(fn(...args));
+): (
+  & ((...args: Parameters<Fn>) => MaybeMonadReturnTypeToCommon<Fn>)
+  & { [K in keyof Fn]: Fn[K] }
+) {
+  return Object.assign(
+    (...args: Parameters<Fn>) => {
+      return throwifyAnyEither(fn(...args)) as MaybeMonadReturnTypeToCommon<Fn>;
+    },
+    fn,
+  );
 }
