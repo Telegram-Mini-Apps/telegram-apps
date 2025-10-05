5e91aebaf7e9aa67ec46d7930913caf063b793e4
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-05 22:04:06 +0500

    feat(sdk/toolkit): implement some useful types for monads

--- tma.js/toolkit/src/monads.ts
+++ tma.js/toolkit/src/monads.ts
@@ -1,11 +1,13 @@
 import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
 
-export type RightOf<E extends E.Either<any, any>> = E extends E.Right<infer U>
-  ? U
-  : never;
+export type AnyEither<L = any, R = any> = E.Either<L, R> | TE.TaskEither<L, R>;
+export type AnyFnAnyEither<L = any, R = any> = (...args: any) => AnyEither<L, R>;
 
-export type RightOfFnReturnType<F extends (...args: any) => E.Either<any, any>> =
-  RightOf<ReturnType<F>>;
+export type RightOfEither<E extends AnyEither> = E extends AnyEither<any, infer U> ? U : never;
+export type LeftOfEither<E extends AnyEither> = E extends AnyEither<infer U> ? U : never;
+export type RightOfReturn<F extends AnyFnAnyEither> = RightOfEither<ReturnType<F>>;
+export type LeftOfReturn<F extends AnyFnAnyEither> = LeftOfEither<ReturnType<F>>;
 
 /**
  * @param either - Either monad.
@@ -20,6 +22,6 @@ export function eitherGet<T>(either: E.Either<any, T>): T {
 
 export function eitherFnToSimple<Fn extends (...args: any) => E.Either<any, any>>(
   fn: Fn,
-): (...args: Parameters<Fn>) => RightOfFnReturnType<Fn> {
+): (...args: Parameters<Fn>) => RightOfReturn<Fn> {
   return (...args) => eitherGet(fn(...args));
 }
