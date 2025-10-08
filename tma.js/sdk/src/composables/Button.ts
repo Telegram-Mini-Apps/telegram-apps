e57fe01fd37c356348d6593faee66242a75e42a3
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-09 00:10:44 +0500

    refactor(sdk): remove SimpleButton and move its functionality to Button

--- tma.js/sdk/src/composables/Button.ts
+++ tma.js/sdk/src/composables/Button.ts
@@ -1,11 +1,17 @@
-import type { Computed } from '@tma.js/signals';
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
+import type { Computed } from '@tma.js/signals';
+import type { PostEventError, MethodName, MethodParams } from '@tma.js/bridge';
 
-import { Mountable } from '@/composables/Mountable.js';
-import { Stateful } from '@/composables/Stateful.js';
-import { bound } from '@/helpers/bound.js';
+import { createIsSupportedSignal } from '@/helpers/createIsSupportedSignal.js';
+import { createWithChecksFp, type WithChecksFp, type WithChecks } from '@/wrappers/withChecksFp.js';
+import { throwifyWithChecksFp } from '@/wrappers/throwifyWithChecksFp.js';
+import type { WithVersionBasedPostEvent } from '@/fn-options/withVersionBasedPostEvent.js';
+import type { SharedFeatureOptions } from '@/fn-options/sharedFeatureOptions.js';
 import type { WithStateRestore } from '@/fn-options/withStateRestore.js';
+import { Stateful } from '@/composables/Stateful.js';
+import { Mountable } from '@/composables/Mountable.js';
+import { bound } from '@/helpers/bound.js';
 import { removeUndefined } from '@/helpers/removeUndefined.js';
 import { shallowEqual } from '@/helpers/shallowEqual.js';
 
@@ -13,13 +19,12 @@ export interface ButtonState {
   isVisible: boolean;
 }
 
-export interface ButtonOptions<S, Err> extends WithStateRestore<S> {
-  /**
-   * A function to commit the button state. This one will be called every time the button state
-   * changes.
-   * @param state - the actual state.
-   */
-  commit: (state: S) => E.Either<Err, void>;
+type ButtonError = PostEventError;
+
+export interface ButtonOptions<S extends ButtonState, M extends MethodName>
+  extends SharedFeatureOptions,
+  WithStateRestore<S>,
+  WithVersionBasedPostEvent {
   /**
    * The initial button state.
    */
@@ -37,17 +42,30 @@ export interface ButtonOptions<S, Err> extends WithStateRestore<S> {
    * @param once - should the listener be called only once.
    */
   onClick: (listener: VoidFunction, once?: boolean) => VoidFunction;
+  /**
+   * A Mini Apps method to commit changes.
+   */
+  method: M;
+  /**
+   * A function to create method payload.
+   * @param state
+   */
+  payload: (state: S) => MethodParams<M>;
 }
 
-export class Button<S extends ButtonState, Err> {
+export class Button<S extends ButtonState, M extends MethodName> {
   constructor({
+    isTma,
     storage,
     onClick,
     offClick,
     initialState,
     isPageReload,
-    commit,
-  }: ButtonOptions<S, Err>) {
+    postEvent,
+    payload,
+    method,
+    version,
+  }: ButtonOptions<S, M>) {
     const stateful = new Stateful({
       initialState,
       onChange(state) {
@@ -72,16 +90,42 @@ export class Button<S extends ButtonState, Err> {
       const nextState = { ...stateful.state(), ...removeUndefined(state) };
       return shallowEqual(nextState, stateful.state())
         ? E.right(undefined)
-        : pipe(commit(nextState), E.map(() => {
-          stateful.setState(nextState);
-        }));
+        : pipe(
+          postEvent(method as any, payload(nextState)),
+          E.map(() => {
+            stateful.setState(nextState);
+          }),
+        );
     };
-    this.mount = bound(mountable, 'mount');
-    this.unmount = bound(mountable, 'unmount');
-    this.onClick = onClick;
-    this.offClick = offClick;
-    this.hide = () => setVisibility(false);
-    this.show = () => setVisibility(true);
+
+    const wrapOptions = { version, isSupported: method, isTma };
+    const wrapSupportedPlain = createWithChecksFp({
+      ...wrapOptions,
+      returns: 'plain',
+    });
+    const wrapMountedEither = createWithChecksFp({
+      ...wrapOptions,
+      returns: 'either',
+      isMounted: mountable.isMounted,
+    });
+
+    this.isVisible = stateful.computedFromState('isVisible');
+    this.isMounted = mountable.isMounted;
+    this.isSupported = createIsSupportedSignal(method, version);
+    this.state = stateful.state;
+
+    this.hideFp = wrapMountedEither(() => setVisibility(false));
+    this.showFp = wrapMountedEither(() => setVisibility(true));
+    this.onClickFp = wrapSupportedPlain(onClick);
+    this.offClickFp = wrapSupportedPlain(offClick);
+    this.mountFp = wrapSupportedPlain(mountable.mount);
+    this.unmount = mountable.unmount;
+
+    this.hide = throwifyWithChecksFp(this.hideFp);
+    this.show = throwifyWithChecksFp(this.showFp);
+    this.onClick = throwifyWithChecksFp(this.onClickFp);
+    this.offClick = throwifyWithChecksFp(this.offClickFp);
+    this.mount = throwifyWithChecksFp(this.mountFp);
   }
 
   /**
@@ -95,6 +139,11 @@ export class Button<S extends ButtonState, Err> {
   readonly isMounted: Computed<boolean>;
 
   /**
+   * Signal indicating if the component is supported.
+   */
+  readonly isSupported: Computed<boolean>;
+
+  /**
    * Complete button state.
    */
   readonly state: Computed<S>;
@@ -102,17 +151,27 @@ export class Button<S extends ButtonState, Err> {
   /**
    * Updates the button state.
    */
-  readonly setState: (state: Partial<S>) => E.Either<Err, void>;
+  readonly setState: (state: Partial<S>) => E.Either<ButtonError, void>;
 
   /**
    * Hides the button.
    */
-  readonly hide: () => E.Either<Err, void>;
+  readonly hideFp: WithChecksFp<() => E.Either<ButtonError, void>, true>;
+
+  /**
+   * @see hideFp
+   */
+  readonly hide: WithChecks<() => void, true>;
 
   /**
    * Shows the button.
    */
-  readonly show: () => E.Either<Err, void>;
+  readonly showFp: WithChecksFp<() => E.Either<ButtonError, void>, true>;
+
+  /**
+   * @see showFp
+   */
+  readonly show: WithChecks<() => void, true>;
 
   /**
    * Adds a new button listener.
@@ -125,7 +184,15 @@ export class Button<S extends ButtonState, Err> {
    *   off();
    * });
    */
-  readonly onClick: (listener: VoidFunction, once?: boolean) => VoidFunction;
+  readonly onClickFp: WithChecksFp<
+    (listener: VoidFunction, once?: boolean) => VoidFunction,
+    true
+  >;
+
+  /**
+   * @see onClickFp
+   */
+  readonly onClick: WithChecks<(listener: VoidFunction, once?: boolean) => VoidFunction, true>;
 
   /**
    * Removes the button click listener.
@@ -138,12 +205,26 @@ export class Button<S extends ButtonState, Err> {
    * }
    * button.onClick(listener);
    */
-  readonly offClick: (listener: VoidFunction, once?: boolean) => void;
+  readonly offClickFp: WithChecksFp<
+    (listener: VoidFunction, once?: boolean) => void,
+    true
+  >;
+
+  /**
+   * @see offClickFp
+   */
+  readonly offClick: WithChecks<(listener: VoidFunction, once?: boolean) => void, true>;
 
   /**
    * Mounts the component restoring its state.
+   * @since Mini Apps v6.1
    */
-  readonly mount: () => void;
+  readonly mountFp: WithChecksFp<() => void, true>;
+
+  /**
+   * @see mountFp
+   */
+  readonly mount: WithChecks<() => void, true>;
 
   /**
    * Unmounts the component.
