6f759a25d69aa3d4ad0c889caf0e2bf22191949c
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-09 17:26:31 +0500

    feat(sdk): implement Viewport

---
+++ tma.js/sdk/src/features/Viewport/instance.ts
@@ -0,0 +1,55 @@
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { retrieveLaunchParamsFp, on, off, type EventName, EventListener } from '@tma.js/bridge';
+
+import { Viewport, ViewportState } from '@/features/Viewport/Viewport.js';
+import { sharedFeatureOptions } from '@/fn-options/sharedFeatureOptions.js';
+import { withStateRestore } from '@/fn-options/withStateRestore.js';
+import { withVersion } from '@/fn-options/withVersion.js';
+import { withRequest } from '@/fn-options/withRequest.js';
+import { withPostEvent } from '@/fn-options/withPostEvent.js';
+
+export function instantiateViewport() {
+  const createListeners = <E extends EventName>(event: E) => {
+    return {
+      on: (listener: EventListener<E>) => {
+        on(event, listener);
+      },
+      off: (listener: EventListener<E>) => {
+        off(event, listener);
+      },
+    };
+  };
+  const viewportListeners = createListeners('viewport_changed');
+  const fullscreenListeners = createListeners('fullscreen_changed');
+  const safeAreaListeners = createListeners('safe_area_changed');
+  const contentSafeAreaListeners = createListeners('content_safe_area_changed');
+
+  return new Viewport({
+    ...pipe(
+      sharedFeatureOptions(),
+      withStateRestore<ViewportState>('viewport'),
+      withVersion,
+      withPostEvent,
+      withRequest,
+    ),
+    isFullscreen() {
+      return pipe(retrieveLaunchParamsFp(), E.map(lp => !!lp.tgWebAppFullscreen));
+    },
+    isViewportStable() {
+      return pipe(retrieveLaunchParamsFp(), E.map(lp => {
+        return ['macos', 'tdesktop', 'unigram', 'webk', 'weba', 'web'].includes(lp.tgWebAppPlatform);
+      }));
+    },
+    offContentSafeAreaInsetsChanged: contentSafeAreaListeners.off,
+    offFullscreenChanged: fullscreenListeners.off,
+    offSafeAreaInsetsChanged: safeAreaListeners.off,
+    offViewportChanged: viewportListeners.off,
+    onContentSafeAreaInsetsChanged: contentSafeAreaListeners.on,
+    onFullscreenChanged: fullscreenListeners.on,
+    onSafeAreaInsetsChanged: safeAreaListeners.on,
+    onViewportChanged: viewportListeners.on,
+  });
+}
+
+export const viewport = instantiateViewport();
