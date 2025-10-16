7f33c8714d476d4b6f7eb677634a7a9cc7272b7f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-16 12:27:32 +0500

    feat(sdk/SecondaryButton): define instance

---
+++ tma.js/sdk/src/features/SecondaryButton/instance.ts
@@ -0,0 +1,20 @@
+import { computed } from '@tma.js/signals';
+
+import { SecondaryButton } from '@/features/SecondaryButton/SecondaryButton.js';
+import { bottomButtonOptions } from '@/fn-options/bottomButtonOptions.js';
+import { themeParams } from '@/features/ThemeParams/instance.js';
+import { miniApp } from '@/features/MiniApp/instance.js';
+
+/**
+ * @internal
+ */
+export function instantiateSecondaryButton() {
+  return new SecondaryButton(
+    bottomButtonOptions('secondaryButton', 'secondary_button_pressed', {
+      bgColor: computed(() => miniApp.bottomBarColorRgb() || '#000000'),
+      textColor: computed(() => themeParams.buttonColor() || '#2481cc'),
+    }),
+  );
+}
+
+export const secondaryButton = instantiateSecondaryButton();
