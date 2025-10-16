9b3bf5a2f19e28d4e0c1ee808ba7a1ae465455f7
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-16 13:27:29 +0500

    feat(sdk): implement home screen utils

---
+++ tma.js/sdk/src/features/home-screen/addToHomeScreen.ts
@@ -0,0 +1,39 @@
+import { pipe } from 'fp-ts/function';
+import type * as E from 'fp-ts/Either';
+import type { PostEventError } from '@tma.js/bridge';
+
+import {
+  sharedFeatureOptions,
+  type SharedFeatureOptions,
+} from '@/fn-options/sharedFeatureOptions.js';
+import { withPostEvent, type WithPostEvent } from '@/fn-options/withPostEvent.js';
+import { withVersion, type WithVersion } from '@/fn-options/withVersion.js';
+import { withChecksFp } from '@/wrappers/withChecksFp.js';
+import { throwifyWithChecksFp } from '@/wrappers/throwifyWithChecksFp.js';
+
+interface CreateAddToHomeScreenOptions extends SharedFeatureOptions, WithPostEvent, WithVersion {
+}
+
+/**
+ * @internal
+ */
+function createAddToHomeScreen({ postEvent, ...rest }: CreateAddToHomeScreenOptions) {
+  return withChecksFp((): E.Either<PostEventError, void> => {
+    return postEvent('web_app_add_to_home_screen');
+  }, { ...rest, isSupported: 'web_app_add_to_home_screen', returns: 'either' });
+}
+
+/**
+ * Prompts the user to add the Mini App to the home screen.
+ * @since Mini Apps v8.0
+ */
+export const addToHomeScreenFp = createAddToHomeScreen(pipe(
+  sharedFeatureOptions(),
+  withVersion,
+  withPostEvent,
+));
+
+/**
+ * @see addToHomeScreenFp
+ */
+export const addToHomeScreen = throwifyWithChecksFp(addToHomeScreenFp);
---
+++ tma.js/sdk/src/features/home-screen/checkHomeScreenStatus.ts
@@ -0,0 +1,47 @@
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import type { HomeScreenStatus, RequestError } from '@tma.js/bridge';
+
+import {
+  sharedFeatureOptions,
+  type SharedFeatureOptions,
+} from '@/fn-options/sharedFeatureOptions.js';
+import { withVersion, type WithVersion } from '@/fn-options/withVersion.js';
+import { withChecksFp } from '@/wrappers/withChecksFp.js';
+import { throwifyWithChecksFp } from '@/wrappers/throwifyWithChecksFp.js';
+import { withRequest, type WithRequest } from '@/fn-options/withRequest.js';
+import type { AsyncOptions } from '@/types.js';
+
+interface CreateCheckHomeScreenStatusOptions extends SharedFeatureOptions,
+  WithRequest,
+  WithVersion {
+}
+
+/**
+ * @internal
+ */
+function createCheckHomeScreenStatus({ request, ...rest }: CreateCheckHomeScreenStatusOptions) {
+  return withChecksFp((options?: AsyncOptions): TE.TaskEither<RequestError, HomeScreenStatus> => {
+    return pipe(
+      request('web_app_check_home_screen', 'home_screen_checked', options),
+      TE.map(response => response.status || 'unknown'),
+    );
+  }, { ...rest, isSupported: 'web_app_check_home_screen', returns: 'task' });
+}
+
+/**
+ * Sends a request to the native Telegram application to check if the current mini
+ * application is added to the device's home screen.
+ * @param options - additional options.
+ * @since Mini Apps v8.0
+ */
+export const checkHomeScreenStatusFp = createCheckHomeScreenStatus(pipe(
+  sharedFeatureOptions(),
+  withVersion,
+  withRequest,
+));
+
+/**
+ * @see checkHomeScreenStatusFp
+ */
+export const checkHomeScreenStatus = throwifyWithChecksFp(checkHomeScreenStatusFp);
