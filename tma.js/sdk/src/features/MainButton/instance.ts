93a904f0c987d13f08e806a9c0fb58d2d3a67c02
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-16 12:28:06 +0500

    fix(sdk/MainButton): mark instance's defaults as computed

--- tma.js/sdk/src/features/MainButton/instance.ts
+++ tma.js/sdk/src/features/MainButton/instance.ts
@@ -1,3 +1,5 @@
+import { computed } from '@tma.js/signals';
+
 import { MainButton } from '@/features/MainButton/MainButton.js';
 import { bottomButtonOptions } from '@/fn-options/bottomButtonOptions.js';
 import { themeParams } from '@/features/ThemeParams/instance.js';
@@ -8,8 +10,8 @@ import { themeParams } from '@/features/ThemeParams/instance.js';
 export function instantiateMainButton() {
   return new MainButton(
     bottomButtonOptions('mainButton', 'main_button_pressed', {
-      bgColor: themeParams.buttonColor() || '#2481cc',
-      textColor: themeParams.buttonTextColor() || '#ffffff',
+      bgColor: computed(() => themeParams.buttonColor() || '#2481cc'),
+      textColor: computed(() => themeParams.buttonTextColor() || '#ffffff'),
     }),
   );
 }
