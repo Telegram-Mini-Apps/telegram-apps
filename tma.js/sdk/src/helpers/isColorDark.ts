fd1d5cdb8a542fbb127716cd3c186572e31521f0
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-17 17:07:57 +0500

    feat(sdk): implement isColorDarkFp

--- tma.js/sdk/src/helpers/isColorDark.ts
+++ tma.js/sdk/src/helpers/isColorDark.ts
@@ -1,20 +1,31 @@
-import { toRGB } from '@tma.js/transformers';
+import { throwifyFpFn } from '@tma.js/toolkit';
+import { toRGBFullFp } from '@tma.js/transformers';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 
 /**
  * @param color - color in any format acceptable by the `toRGB` function.
  * @returns True if the color is recognized as dark.
  * @see toRGB
  */
-export function isColorDark(color: string): boolean {
-  const rgb = toRGB(color);
-
-  // Real formula: hsp = Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b)
-  // See: https://stackoverflow.com/a/596243
-  return Math.sqrt(
-    [0.299, 0.587, 0.114].reduce<number>((acc, modifier, idx) => {
-      // Extract part of #RRGGBB pattern and convert it to DEC.
-      const dec = parseInt(rgb.slice(1 + (idx * 2), 1 + ((idx + 1) * 2)), 16);
-      return acc + (dec * dec * modifier);
-    }, 0),
-  ) < 120;
+export function isColorDarkFp(color: string): E.Either<Error, boolean> {
+  return pipe(
+    toRGBFullFp(color),
+    E.map(rgb => {
+      // Actual formula: hsp = Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b)
+      // See: https://stackoverflow.com/a/596243
+      return Math.sqrt(
+        [0.299, 0.587, 0.114].reduce<number>((acc, modifier, idx) => {
+          // Extract part of #RRGGBB pattern and convert it to DEC.
+          const dec = parseInt(rgb.slice(1 + (idx * 2), 1 + ((idx + 1) * 2)), 16);
+          return acc + (dec * dec * modifier);
+        }, 0),
+      ) < 120;
+    }),
+  );
 }
+
+/**
+ * @see isColorDarkFp
+ */
+export const isColorDark = throwifyFpFn(isColorDarkFp);
