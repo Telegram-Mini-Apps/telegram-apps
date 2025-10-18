6722c4b084aefc88b65ad3272ba8f07b59bca52b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-10-18 16:18:17 +0500

    feat(transformers): make transformQueryToSchema and transformJsonToSchema non-throwable

--- tma.js/transformers/src/transformers.ts
+++ tma.js/transformers/src/transformers.ts
@@ -1,30 +1,43 @@
 import {
   type BaseIssue,
   type BaseSchema,
+  check,
+  type CheckAction,
   type InferOutput,
+  instance,
+  type InstanceSchema,
   parse, pipe,
   type SchemaWithPipe,
   string,
   type StringSchema,
   transform,
   type TransformAction,
+  union,
+  type UnionSchema,
 } from 'valibot';
 
 type RequiredSchema = BaseSchema<any, any, BaseIssue<any>>;
 
 export type TransformJsonToSchemaAction<Schema extends RequiredSchema> = SchemaWithPipe<readonly [
   StringSchema<any>,
+  CheckAction<string, string>,
   TransformAction<string, unknown>,
   Schema,
 ]>;
 
-export type TransformQueryToSchemaAction<Schema extends RequiredSchema> =
-  TransformAction<string | URLSearchParams, InferOutput<Schema>>;
+export type TransformQueryToSchemaAction<Schema extends RequiredSchema> = SchemaWithPipe<readonly [
+  UnionSchema<[
+    StringSchema<undefined>,
+    InstanceSchema<typeof URLSearchParams, undefined>,
+  ], undefined>,
+  CheckAction<string | URLSearchParams, string>,
+  TransformAction<string | URLSearchParams, InferOutput<Schema>>,
+]>;
 
 export function transformQueryToSchema<Schema extends RequiredSchema>(
   schema: Schema,
 ): TransformQueryToSchemaAction<Schema> {
-  return transform(input => {
+  const transformer = (input: string | URLSearchParams) => {
     const result: Record<string, string | string[]> = {};
 
     new URLSearchParams(input).forEach((value, key) => {
@@ -39,7 +52,20 @@ export function transformQueryToSchema<Schema extends RequiredSchema>(
     });
 
     return parse(schema, result);
-  });
+  };
+
+  return pipe(
+    union([string(), instance(URLSearchParams)]),
+    check(input => {
+      try {
+        transformer(input);
+        return true;
+      } catch {
+        return false;
+      }
+    }, 'The value doesn\'t match required schema'),
+    transform(transformer),
+  );
 }
 
 /**
@@ -48,5 +74,17 @@ export function transformQueryToSchema<Schema extends RequiredSchema>(
 export function transformJsonToSchema<Schema extends RequiredSchema>(
   schema: Schema,
 ): TransformJsonToSchemaAction<Schema> {
-  return pipe(string(), transform(JSON.parse), schema);
+  return pipe(
+    string(),
+    check(input => {
+      try {
+        JSON.parse(input);
+        return true;
+      } catch {
+        return false;
+      }
+    }, 'Input is not a valid JSON value'),
+    transform(JSON.parse),
+    schema,
+  );
 }
